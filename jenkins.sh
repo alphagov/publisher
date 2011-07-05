4ac558cbf56b72a08642ac193e18850834e76a9b
Ben Griffiths <bengriffiths@gmail.com>
2011-07-05 07:29:50 +0100

    CI file

--- jenkins.sh
+++ jenkins.sh
@@ -3,4 +3,4 @@ source '/usr/local/lib/rvm'
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
 bundle exec rake db:setup
 bundle exec rake db:migrate
-bundle exec rake spec
+bundle exec rake test
