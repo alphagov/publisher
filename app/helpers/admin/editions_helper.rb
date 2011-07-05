a79d3468d6c4cba73b9df7a29670e3f6e162b848
James Stewart <jys@ketlai.co.uk>
2011-07-05 11:15:22 +0100

    Add a helper that will make sure that we have a given number of associated objects. We use this to build relationships for our nested model forms

--- app/helpers/admin/editions_helper.rb
+++ app/helpers/admin/editions_helper.rb
@@ -1,2 +1,15 @@
 module Admin::EditionsHelper
+  def setup_association(object, associated, opts)
+    associated = object.send(associated.to_s) if associated.is_a? Symbol
+    associated = associated.is_a?(Array) ? associated : [associated] # preserve association proxy if this is one
+
+    opts.symbolize_keys!
+
+    (opts[:new] - associated.select(&:new_record?).length).times  { associated.build } if opts[:new] and object.new_record? == true
+    if opts[:edit] and object.new_record? == false
+      (opts[:edit] - associated.count).times { associated.build }
+    elsif opts[:new_in_edit] and object.new_record? == false
+      opts[:new_in_edit].times { associated.build }
+    end
+  end
 end
