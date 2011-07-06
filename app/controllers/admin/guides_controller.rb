386dd10b1b013e71f16213003bf255e5781c60e8
James Stewart <jys@ketlai.co.uk>
2011-07-06 17:01:34 +0100

    Use scopes to assemble content for various guides tabs

--- app/controllers/admin/guides_controller.rb
+++ app/controllers/admin/guides_controller.rb
@@ -1,6 +1,13 @@
 class Admin::GuidesController < InheritedResources::Base
   defaults :route_prefix => 'admin'
   
+  def index
+    @drafts = Guide.in_draft
+    @published = Guide.published
+    @archive = Guide.archive
+    @review_requested = Guide.review_requested
+  end
+
   def show
     @guide = resource
     @latest_edition = resource.latest_edition
