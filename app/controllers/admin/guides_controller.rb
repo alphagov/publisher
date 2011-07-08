fbb1a25a20aa5912d2fdfc58e0fcd2f45812adf9
James Stewart <jys@ketlai.co.uk>
2011-07-08 14:55:44 +0100

    Basic buttons and action to progress guides

--- app/controllers/admin/guides_controller.rb
+++ app/controllers/admin/guides_controller.rb
@@ -1,6 +1,6 @@
 class Admin::GuidesController < InheritedResources::Base
-  #before_filter :authenticate_user!
-  
+
+  before_filter :authenticate_user!
   defaults :route_prefix => 'admin'
   
   def index
@@ -17,5 +17,23 @@ class Admin::GuidesController < InheritedResources::Base
   
   def update
     update! { admin_guide_url(@guide, :anchor => 'metadata') }
+  end
+  
+  def progress
+    @guide = resource
+    @latest_edition = resource.latest_edition
+    
+    case params[:activity]
+    when 'request review'
+      current_user.request_review(@latest_edition)
+    when 'review'
+      current_user.review(@latest_edition)
+    when 'okay'
+      current_user.okay(@latest_edition)
+    when 'publish'
+      current_user.publish(@latest_edition)
+    end
+    
+    redirect_to admin_guide_path(@guide), :notice => 'Guide updated'
   end
 end
