1ce45878015952b83148cc584843c16d2cee5b56
James Stewart <jys@ketlai.co.uk>
2011-07-12 17:09:10 +0100

    Oops. Don't want http:// on the front of asset host names

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,6 +1,5 @@
 require File.expand_path('production.rb', File.dirname(__FILE__))
 
 Guides::Application.configure do
-  config.action_controller.asset_host = ['http://staging.alphagov.co.uk:8080/']
-  
+  config.action_controller.asset_host = ['staging.alphagov.co.uk:8080/']
 end
