0e2781cfa6c1aa9f850544160778998ad7fcd03a
James Stewart <jys@ketlai.co.uk>
2011-07-12 17:06:06 +0100

    Set asset host in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,6 @@
 require File.expand_path('production.rb', File.dirname(__FILE__))
 
 Guides::Application.configure do
+  config.action_controller.asset_host = ['http://staging.alphagov.co.uk:8080/']
+  
 end
