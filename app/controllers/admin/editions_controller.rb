d61dcbd39b2005b534b725af8d5aa9e6cb256edc
James Stewart <jys@ketlai.co.uk>
2011-07-13 15:54:41 +0100

    Hook in code to create new edition. Display current edition on edit page

--- app/controllers/admin/editions_controller.rb
+++ app/controllers/admin/editions_controller.rb
@@ -4,6 +4,16 @@ class Admin::EditionsController < InheritedResources::Base
   defaults :route_prefix => 'admin'
   belongs_to :guide
  
+  def create
+    guide = Guide.find(params[:guide_id])
+    new_edition = guide.build_edition(guide.latest_edition.title)
+    if new_edition.save
+      redirect_to [:admin, guide], :notice => 'New edition created'
+    else
+      redirect_to [:admin, guide], :notice => 'Failed to create new edition'
+    end
+  end
+
   def update
     update! { admin_guide_path(@guide) }
   end
