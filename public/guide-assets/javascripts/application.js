c3d63300a5e38f870b160603de07f7dae8d84ee8
James Stewart <jys@ketlai.co.uk>
2011-07-14 09:00:33 +0100

    A little javascript to auto-generate slugs for us (which can then be overridden)

--- public/guide-assets/javascripts/application.js
+++ public/guide-assets/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+var GovUKGuideUtils = {
+  convertToSlug: function(title) {
+      return title
+      .toLowerCase()
+      .replace(/[^\w ]+/g,'')
+      .replace(/ +/g,'-');
+  }
+}
