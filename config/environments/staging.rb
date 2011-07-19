8431db5b7de5e223e226755f9ef3513a4aba0f50
James Stewart <jys@ketlai.co.uk>
2011-07-19 12:31:38 +0100

    Turn off SSL when sending an email - fixes the bug which was causing an error page to appear whenever you created a guide/etc.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,4 +2,5 @@ require File.expand_path('production.rb', File.dirname(__FILE__))
 
 Guides::Application.configure do
   config.action_controller.asset_host = 'staging.alphagov.co.uk:8080'
+  config.action_mailer.smtp_settings = {:enable_starttls_auto => false}
 end
