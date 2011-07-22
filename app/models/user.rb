d62f1fdde23d624f9e93bd102c3981e6c9c7a397
James Stewart <jys@ketlai.co.uk>
2011-07-22 15:17:45 +0100

    Show peoples' pictures next to their activity in the history tab
    Add User#gravatar method to support this

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,4 +64,12 @@ class User
   def to_s
     name
   end
+  
+  def gravatar_url(opts = {})
+    opts.symbolize_keys!
+    qs = opts.select { |k, v| k == :s }.collect { |k, v| "#{k}=#{Rack::Utils.escape(v)}" }.join("&")
+    qs = "?" + qs unless qs == ""
+    scheme_and_host = opts[:ssl] ? "https://secure.gravatar.com" : "http://www.gravatar.com"
+    "#{scheme_and_host}/avatar/#{Digest::MD5.hexdigest(email.downcase)}#{qs}"
+  end
 end
