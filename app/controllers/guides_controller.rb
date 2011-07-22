b5441a2674a37a94d5badc0d435fc71346de2800
James Stewart <jys@ketlai.co.uk>
2011-07-22 15:07:50 +0100

    Make selection of API Generator module more explicit

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -4,6 +4,12 @@ class GuidesController < ApplicationController
   def show
     publication = Publication.first(conditions: {slug: params[:id]})
     edition = publication.published_edition
-    render :json => Api::Generator::Guide.edition_to_hash(edition)
+    generator = case publication.class
+      when Transaction then Api::Generator::Transaction
+      when Guide then Api::Generator::Guide
+      when Answer then Api::Generator::Guide
+    end
+            
+    render :json => generator.edition_to_hash(edition)
   end
 end
