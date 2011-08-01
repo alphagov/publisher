c265a6db605a36d94f7def57eaa9fd9b28dce326
Joshua Marshall <joshua@workwithgreat.com>
2011-08-01 13:26:47 +0100

    Make preview work if it's the first thing loaded after the app starts
    If the rest of the app hadn't been loaded, it didn't know where to find the Api::Generator module

--- lib/guides_front_end/preview.rb
+++ lib/guides_front_end/preview.rb
@@ -1,23 +1,24 @@
+require File.expand_path('../../../app/models/api/guide', __FILE__)
 module GuidesFrontEnd
   class Preview < GuidesFrontEnd::Base
     def self.preview_edition_id(env)
       env['action_dispatch.request.path_parameters'][:edition_id]
     end
-    
+
     helpers do
       def guide_path(guide_slug, part_slug)
         "/preview/#{preview_edition_id}#{base_path(guide_slug,part_slug)}"
       end
     end
-    
+
     def preview_edition_id
       self.class.preview_edition_id(request.env)
     end
-    
+
     def get_publication
       @this_publication ||= Publication.where(:slug => params[:slug]).first
     end
-    
+
     def router
       publication = get_publication
       if publication
@@ -26,13 +27,13 @@ module GuidesFrontEnd
         nil
       end
     end
-    
+
     def setup_publication
       publication = get_publication.editions.select { |e| e.version_number.to_i == preview_edition_id.to_i }.first
       hashed_publication = Api::Generator.edition_to_hash(publication)
-      Api::Client.from_hash(hashed_publication)      
+      Api::Client.from_hash(hashed_publication)
     end
-    
+
     def publication
       @publication ||= setup_publication
     end
