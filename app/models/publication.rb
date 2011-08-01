c86085b265b7011940552df97ab031635f39c5c6
Joshua Marshall <joshua@workwithgreat.com>
2011-08-01 11:38:31 +0100

    Improve performance when opening a list of publications
    Only run calculate_statuses in create_first_edition if we actually needed to add an edition

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,31 +1,31 @@
 class Publication
   include Mongoid::Document
   include Mongoid::Timestamps
-  
+
   field :name,            :type => String
   field :slug,            :type => String
   field :tags,            :type => String
   field :audiences,       :type => Array
-  
+
   field :has_drafts,      :type => Boolean
   field :has_published,   :type => Boolean
-  field :has_reviewables, :type => Boolean  
+  field :has_reviewables, :type => Boolean
   field :archived,        :type => Boolean
-  
+
   embeds_many :publishings
-  
+
   scope :in_draft,         where(has_drafts: true)
   scope :published,        where(has_published: true)
   scope :review_requested, where(has_reviewables: true)
   scope :archive,          where(archived: true)
-  
+
   after_initialize :create_first_edition
-  
+
   before_save :calculate_statuses
 
-  validates_presence_of :name  
+  validates_presence_of :name
   validates_presence_of :slug
-  
+
   accepts_nested_attributes_for :editions, :reject_if => proc { |a| a['title'].blank? }
 
    def build_edition(title)
@@ -39,15 +39,15 @@ class Publication
    def create_first_edition
      unless self.persisted? or self.editions.any?
        self.editions << self.class.edition_class.new(:title => self.name)
+       calculate_statuses
      end
-     calculate_statuses
   end
-  
+
   def calculate_statuses
      self.has_published = self.publishings.any? && ! self.archived
 
-     published_versions = Set.new(publishings.map(&:version_number))
-     all_versions = Set.new(editions.map(&:version_number))
+     published_versions = ::Set.new(publishings.map(&:version_number))
+     all_versions = ::Set.new(editions.map(&:version_number))
      drafts = (all_versions - published_versions)
      self.has_drafts = drafts.any?
 
@@ -55,12 +55,12 @@ class Publication
 
      true
   end
-  
+
   def publish(edition,notes)
     self.publishings << Publishing.new(:version_number=>edition.version_number,:change_notes=>notes)
     calculate_statuses
   end
-  
+
   def published_edition
     latest_publishing = self.publishings.first
     if latest_publishing
@@ -85,7 +85,7 @@ class Publication
   def title
     self.name || latest_edition.title
   end
-  
+
   AUDIENCES = [
     "Age-related audiences",
     "Carers",
@@ -123,5 +123,5 @@ class Publication
     "Widows",
     "Young people"
   ]
-  
+
 end
