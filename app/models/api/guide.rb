05420e9463372e3688d5bf2391a4e842ddc285c6
James Stewart <jys@ketlai.co.uk>
2011-08-10 15:52:59 +0100

    Add temporary fields for categories and related items in preparation for user testing

--- app/models/api/guide.rb
+++ app/models/api/guide.rb
@@ -12,7 +12,7 @@ module Api
 
     module Guide
       def self.edition_to_hash(edition)
-        attrs = edition.guide.as_json(:only => [:audiences, :slug, :tags, :updated_at])
+        attrs = edition.guide.as_json(:only => [:audiences, :slug, :tags, :updated_at, :category, :related_items])
         attrs.merge!(edition.as_json(:only => [:title]))
         attrs['parts'] = edition.parts.sort_by(&:order).collect { |p| p.as_json(:only => [:slug, :title, :body]).merge('number' => p.order) }
         attrs['type'] = 'guide'
@@ -22,7 +22,7 @@ module Api
 
     module Answer
       def self.edition_to_hash(edition)
-        attrs = edition.answer.as_json(:only => [:audiences, :slug, :tags, :updated_at])
+        attrs = edition.answer.as_json(:only => [:audiences, :slug, :tags, :updated_at, :category, :related_items])
         attrs['type'] = 'answer'
         attrs.merge!(edition.as_json(:only => [:title,:body]))
       end
@@ -30,7 +30,7 @@ module Api
     
     module Transaction
       def self.edition_to_hash(edition)
-        attrs = edition.transaction.as_json(:only => [:audiences, :slug, :tags, :updated_at])
+        attrs = edition.transaction.as_json(:only => [:audiences, :slug, :tags, :updated_at, :category, :related_items])
         attrs['type'] = 'transaction'
         attrs['expectations'] = edition.expectations.map {|e| e.as_json(:only => [:css_class,:text]) }
         attrs.merge!(edition.as_json(:only => [:title,:introduction,:more_information,:will_continue_on,:link]))
