496344b6d962b062a20bdcd1f6dc32c022a9ab32
James Stewart <jys@ketlai.co.uk>
2011-08-10 16:18:04 +0100

    End API requests after returning a 404

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -3,7 +3,7 @@ require 'api/guide'
 class GuidesController < ApplicationController
   def show
     publication = Publication.first(conditions: {slug: params[:id]})
-    head 404 if publication.nil?
+    head 404 and return if publication.nil?
     
     edition = publication.published_edition
     render :json => Api::Generator.edition_to_hash(edition)
