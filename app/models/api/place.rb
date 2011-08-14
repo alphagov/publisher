67434bf9900910e0242b4c8a30fe008cc09967a7
James Stewart <jys@ketlai.co.uk>
2011-08-14 16:51:47 +0100

    Add expectations to places as well as transactions

--- app/models/api/place.rb
+++ app/models/api/place.rb
@@ -6,6 +6,7 @@ module Api
       def self.edition_to_hash(edition)
         attrs = edition.place.as_json(:only => [:audiences, :slug, :tags, :updated_at, :category, :related_items])
         attrs.merge!(edition.as_json(:only => [:title, :introduction, :more_information, :place_type]))
+        attrs['expectations'] = edition.expectations.map {|e| e.as_json(:only => [:css_class,:text]) }
         attrs['type'] = 'place'
         attrs
       end
--- app/models/place_edition.rb
+++ app/models/place_edition.rb
@@ -1,11 +1,13 @@
 class PlaceEdition < Edition
+  include Expectant
+
   embedded_in :place
 
   field :introduction,      :type => String
   field :more_information,  :type => String
   field :place_type,        :type => String
 
-  @fields_to_clone = [:introduction, :will_continue_on, :link, :more_information, :place_type]
+  @fields_to_clone = [:introduction, :link, :more_information, :place_type, :expectation_ids]
 
   def container
     self.place
