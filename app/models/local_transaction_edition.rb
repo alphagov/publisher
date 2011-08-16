6295403a73a27a4d5029a2983dd2d26dee8c3eeb
James Stewart <jys@ketlai.co.uk>
2011-08-16 10:25:59 +0100

    Simplify local transaction code a bit - remove some duplication
    LocalTransactionEdition should use 'Expectant'

--- app/models/local_transaction_edition.rb
+++ app/models/local_transaction_edition.rb
@@ -1,24 +1,17 @@
 class LocalTransactionEdition < Edition
+  include Expectant
+  
   embedded_in :local_transaction
 
   field :introduction,      type: String
   # field :will_continue_on,  type: String
   # field :link,              type: String
   field :more_information,  type: String
-  field :expectation_ids,   type: Array, default: []
 
   @fields_to_clone = [:introduction, :more_information, :lgsl, :expectation_ids]
 
   def admin_list_title
     "#{title} (LGSL #{local_transaction.lgsl_code}) [#{local_transaction.lgsl.authorities.count}]"
-  end
-
-  def expectations
-    Expectation.criteria.in(_id: self.expectation_ids)
-  end
-
-  def self.expectation_choices
-    Hash[Expectation.all.map {|e| [e.text, e._id.to_s] }]
   end
 
   def container
