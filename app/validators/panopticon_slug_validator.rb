a609774fa262c0c20d821653c07edcf8c4c6756a
James Stewart <jys@ketlai.co.uk>
2011-08-22 14:48:33 +0100

    Allow longer slugs

--- app/validators/panopticon_slug_validator.rb
+++ app/validators/panopticon_slug_validator.rb
@@ -18,8 +18,8 @@ class PanopticonSlugValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     the_slug = record.send(attribute)
 
-    if the_slug.length < 4 or the_slug.length > 32
-      record.errors[attribute] << "must be between 4 and 32 characters"
+    if the_slug.length < 4 or the_slug.length > 60
+      record.errors[attribute] << "must be between 4 and 60 characters"
       return
     end
     
