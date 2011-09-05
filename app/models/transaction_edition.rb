773fbb9bb4c4a1a70dff9be798f0214a445eca66
Gareth Rushgrove <gareth@alphagov.co.uk>
2011-09-05 13:17:20 +0000

    fix bug introduced in previous commit and add test to prevent regression

--- app/models/transaction_edition.rb
+++ app/models/transaction_edition.rb
@@ -1,19 +1,14 @@
 class TransactionEdition < Edition
   embedded_in :transaction
-  
+
   include Expectant
-  
+
   field :introduction,      :type => String
   field :will_continue_on,  :type => String
   field :link,              :type => String
   field :more_information,  :type => String
-  
+
   @fields_to_clone = [:introduction, :will_continue_on, :link, :more_information, :expectation_ids]
- 
-  validates_presence_of :will_continue_on
-  validates_presence_of :introduction
-  validates_presence_of :link # Should also be a valid URL
-  validates_presence_of :more_information
 
   def container
     self.transaction
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -4,7 +4,6 @@ class UserTest < ActiveSupport::TestCase
   test "transaction creation convenience function correctly invokes create_publication" do
     user = User.new
     user.expects(:create_publication).with(Transaction, {attr_name: 'value'}).returns(:instance)
-
     assert_equal :instance, user.create_transaction({attr_name: 'value'})
   end
 
@@ -13,5 +12,13 @@ class UserTest < ActiveSupport::TestCase
     user.expects(:create_publication).with(LocalTransaction, {attr_name: 'value'}).returns(:instance)
 
     assert_equal :instance, user.create_local_transaction({attr_name: 'value'})
+  end
+
+  test "creating a transaction with the initial details creates a valid transaction" do
+    user = User.create(:name => "bob")
+    without_panopticon_validation do
+      trans = user.create_transaction({:name => "test", :slug => "test"})
+      assert trans.valid?
+    end
   end
 end
