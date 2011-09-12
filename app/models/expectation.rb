98d0bac9da920805a875c278e4b79eac874ae855
Gareth Rushgrove <gareth@alphagov.co.uk>
2011-09-12 11:52:28 +0000

    tweaks to expectations as requested

--- app/models/expectation.rb
+++ app/models/expectation.rb
@@ -15,8 +15,12 @@ unless Expectation.all.count > 0
   Expectation.create :css_class=>"need_id",       :text=>"Proof of identification required"
   Expectation.create :css_class=>"need_passport", :text=>"A valid passport required"
   Expectation.create :css_class=>"need_ni",       :text=>"National Insurance Number required"
-  Expectation.create :css_class=>"need_address",  :text=>"Three years of address details required"
-  Expectation.create :css_class=>"over_18",       :text=>"You must be over 18"
+  Expectation.create :css_class=>"need_address",  :text=>"3 years of address details required"
   Expectation.create :css_class=>"offline_steps", :text=>"Includes offline steps"
+  Expectation.create :css_class=>"over_18",       :text=>"You must be over 18"
+  Expectation.create :css_class=>"over_16",       :text=>"You must be over 16"
+  Expectation.create :css_class=>"england_only",  :text=>"Available in England only"
+  Expectation.create :css_class=>"england_wales", :text=>"Available in England and Wales only"
+  Expectation.create :css_class=>"not_ni",        :text=>"Not available in Northern Ireland"
 
 end
