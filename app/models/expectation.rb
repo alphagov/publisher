c2023e1ee35d291c0b1228a68083483e590ea5fb
Gareth Rushgrove <gareth@alphagov.co.uk>
2011-09-12 13:10:39 +0000

    order expectations rather than accepting random

--- app/models/expectation.rb
+++ app/models/expectation.rb
@@ -4,14 +4,13 @@ class Expectation
 
   field :css_class, :type => String
   field :text, :type => String
-
 end
 
-
-#Expectation.destroy_all
 unless Expectation.all.count > 0
 
   Expectation.create :css_class=>"card_payment",  :text=>"Credit card required"
+  Expectation.create :css_class=>"debit_card_payment",  :text=>"Debit card required"
+  Expectation.create :css_class=>"debit_or_credit_card_payment",  :text=>"Debit or Credit card required"
   Expectation.create :css_class=>"need_id",       :text=>"Proof of identification required"
   Expectation.create :css_class=>"need_passport", :text=>"A valid passport required"
   Expectation.create :css_class=>"need_ni",       :text=>"National Insurance Number required"
