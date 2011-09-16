4949257b66f18dd8c7d04072744f15b7ecdc36b2
Craig R Webster <craig@barkingiguana.com>
2011-09-16 15:45:30 +0100

    Make the API serve the same stuff it was serving before Related Items changed to an Array

--- app/models/related_item.rb
+++ app/models/related_item.rb
@@ -1,8 +1,21 @@
+# Eventually my data will live somewhere else and I'll not be just a thin wrapper around Publications
 class RelatedItem
   attr_accessor :item
   private :item=, :item
   def initialize item
     self.item = item
+  end
+
+  def format
+    item.class.name.tableize.singularize
+  end
+
+  def path
+    '/' + id
+  end
+
+  def name
+    item.name
   end
 
   def id
@@ -11,6 +24,10 @@ class RelatedItem
 
   def to_s
     item.name
+  end
+
+  def published?
+    item.has_published
   end
 
   include Comparable
@@ -22,8 +39,13 @@ class RelatedItem
     item.name
   end
 
-  # Eventually I will live somewhere else and not be a thin wrapper around Publications
   def self.all
     Publication.all.map { |p| RelatedItem.new p }.sort
+  end
+
+  def self.find slug
+    publication = Publication.where slug: slug
+    return unless publication.any?
+    RelatedItem.new publication.first
   end
 end
