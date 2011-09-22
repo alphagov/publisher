5747ebe89c300a5c16f1dbd58eb8a224ffb2eef2
Chris Roos & James Adam <pairing+chris.roos+james.adam@gofreerange.com>
2011-09-22 13:55:21 +0100

    Allow setting an alternative panopticon endpoint during development.

--- config/initializers/panopticon_hosts.rb
+++ config/initializers/panopticon_hosts.rb
@@ -1,7 +1,7 @@
 require 'panopticon_api'
 
 PanopticonApi.endpoint = case Rails.env.to_s
-when 'development' then 'http://panopticon.dev.gov.uk'
+when 'development' then ENV['PANOPTICON_URI'] || 'http://panopticon.dev.gov.uk'
 when 'test' then 'http://panopticon.dev.gov.uk'
 when 'staging' then 'http://panopticon.staging.alphagov.co.uk:8080'
 end
