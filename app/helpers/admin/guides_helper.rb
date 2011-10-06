5261a082c82d7a8ed75731f6c43b36d125f4701b
Paul Battley <pbattley@gmail.com>
2011-10-06 10:27:37 +0100

    Tidy formatting.

--- app/helpers/admin/guides_helper.rb
+++ app/helpers/admin/guides_helper.rb
@@ -1,17 +1,18 @@
 require 'external_services'
 
 module Admin::GuidesHelper
+
   def safe_to_preview?(publication)
     return true unless publication.is_a?(Guide)
     return (publication.latest_edition.parts.any? and publication.latest_edition.parts.first.slug.present?)
   end
 
   def publication_front_end_path(publication)
-      "#{ExternalServices.front_end_host}/#{publication.slug}"
+    "#{ExternalServices.front_end_host}/#{publication.slug}"
   end
 
   def preview_edition_path(edition)
-      publication_front_end_path(edition.container)+"?edition=#{edition.version_number}"
+    publication_front_end_path(edition.container)+"?edition=#{edition.version_number}"
   end
 
   def activity_form(name, id, url, html_options = {})
@@ -22,19 +23,19 @@ module Admin::GuidesHelper
     if protect_against_forgery?
       request_token_tag = tag(:input, :type => "hidden", :name => request_forgery_protection_token.to_s, :value => form_authenticity_token)
     end
-    
+
     comment_field =  tag(:input,:name=>'comment',:placeholder=>html_options['placeholder'] || "Please enter a comment")
     cancel_button =  tag(:input,:type=>"submit",
                          :class=>"button_to cancel_button",
                          :style=>"width:auto",
                          :value=>"Cancel")
-    
+
     html_options = convert_options_to_data_attributes({}, html_options)
     html_options.merge!("type" => "submit", "value" => name,:style=>"width:auto")
 
     ("<form id=\"#{id}\" method=\"post\" action=\"#{html_escape(url)}\"
        class=\"button_to also_save_edition\" style=\"display:none\">" + comment_field  + 
-      cancel_button + tag("input", html_options)  + request_token_tag + "</form>").html_safe
+       cancel_button + tag("input", html_options)  + request_token_tag + "</form>").html_safe
   end
 
   def progress_form(opts)
@@ -43,59 +44,59 @@ module Admin::GuidesHelper
     path = send("progress_admin_#{guide.class.to_s.underscore}_path", guide, :activity => activity, :edition_id => edition)
     activity_form title, activity+"_form", path, :disabled => !edition.send(check_method), :placeholder => placeholder
   end
-  
+
   def review_buttons(guide,edition)
     [
-        ["Needs more work","review"],
-        ["OK for publication","okay"]
+      ["Needs more work",    "review"],
+      ["OK for publication", "okay"]
     ].map{ |title,activity|
       check_method = "can_#{activity}?".to_sym
       disabled = edition.send(check_method) ? "" : "disabled" 
-     "<form id=\"#{activity}_toggle\" class=\"button_to review_button\">
-     <input type=\"submit\" value=\"#{title}\"  #{disabled}>
-     </form>"
-   }.join("\n").html_safe
+      "<form id=\"#{activity}_toggle\" class=\"button_to review_button\">
+      <input type=\"submit\" value=\"#{title}\"  #{disabled}>
+      </form>"
+    }.join("\n").html_safe
   end
-  
+
   def review_forms(guide,edition)
-     [
-        ["Needs more work","review"],
-        ["OK for publication","okay"]
-      ].map{ |title,activity|
-        progress_form(:title=>title,:guide=>guide,:activity=>activity,:edition=>edition)
-      }.join("\n").html_safe
+    [
+      ["Needs more work",    "review"],
+      ["OK for publication", "okay"]
+    ].map{ |title,activity|
+      progress_form(:title=>title,:guide=>guide,:activity=>activity,:edition=>edition)
+    }.join("\n").html_safe
   end
-  
+
   def progress_buttons(guide,edition)
     [
-      ["Fact check", "request_fact_check"],
-      ["2nd pair of eyes","request_review"],
-      ["Publish","publish"]
+      ["Fact check",       "request_fact_check"],
+      ["2nd pair of eyes", "request_review"],
+      ["Publish",          "publish"]
     ].map{ |title,activity|
       check_method = "can_#{activity}?".to_sym
       disabled = edition.send(check_method) ? "" : "disabled"
-     "<form id=\"#{activity}_toggle\" class=\"button_to progress_button\">
-     <input type=\"submit\" value=\"#{title}\"  #{disabled}>
-     </form>"
-   }.join("\n").html_safe
+      "<form id=\"#{activity}_toggle\" class=\"button_to progress_button\">
+      <input type=\"submit\" value=\"#{title}\"  #{disabled}>
+      </form>"
+    }.join("\n").html_safe
   end
-  
+
   def progress_forms(guide,edition)
     [
-      ["Fact check", "request_fact_check", "Enter email addresses"],
-      ["2nd pair of eyes","request_review"],
-      ["Publish","publish"]
+      ["Fact check",       "request_fact_check", "Enter email addresses"],
+      ["2nd pair of eyes", "request_review"],
+      ["Publish",          "publish"]
     ].map { |title,activity, placeholder|
       progress_form(:title=>title,:guide=>guide,:activity=>activity,:edition=>edition,:placeholder=>placeholder)
     }.join("\n").html_safe
   end
-  
+
   def preview_button(edition)
     form_tag(preview_edition_path(edition), :method => :get, :class => 'preview button_to also_save_edition') do
       hidden_field_tag('edition', edition.version_number) + submit_tag('Preview')
     end
   end
-  
+
   def friendly_date(date)
     if Time.now - date < 2.days
       time_ago_in_words(date) + " ago."
@@ -103,9 +104,8 @@ module Admin::GuidesHelper
       date.strftime("%d/%m/%Y %R")
     end
   end
-  
+
   def admin_editions_path(publication)
     send("admin_#{publication.class.to_s.underscore}_editions_path", publication)
   end
-  
 end
