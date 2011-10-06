7bafb0bc1a13ea8321de726c7627db25d4781c4f
David Heath <david@davidheath.org>
2011-10-06 15:06:21 +0000

    Make message sending reliable, fix config.

--- lib/messenger.rb
+++ lib/messenger.rb
@@ -4,7 +4,11 @@ class Messenger
   def published(edition)
     container = edition.container
     message = { panopticon_id: container.panopticon_id }
-    client.publish '/queue/need_satisfied', message.to_json
+    Timeout.timeout(5) do
+      client.publish '/queue/need_satisfied', message.to_json
+    end
+  rescue => e
+    Rails.logger.error("Unable to send message due to #{e}")
   end
   
   def client
