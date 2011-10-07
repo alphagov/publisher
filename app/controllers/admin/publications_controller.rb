cf3366eb04f055d5d03381d899fd49617bbcdbe3
Craig R Webster <craig@barkingiguana.com>
2011-10-07 15:36:55 +0100

    Let assignment happen from need-o-tron

--- app/controllers/admin/publications_controller.rb
+++ app/controllers/admin/publications_controller.rb
@@ -1,8 +1,10 @@
-class Admin::PublicationsController < ApplicationController
+class Admin::PublicationsController < Admin::BaseController
   def show
     publication = Publication.where(panopticon_id: params[:id]).first
     publication = import_publication params[:id] unless publication.present?
-    redirect_to '/admin/' + publication.class.name.tableize + '/' + publication.id.to_s
+    destination = '/admin/' + publication.class.name.tableize + '/' + publication.id.to_s
+    destination += '?return_to=' + params[:return_to] if params[:return_to]
+    redirect_to destination
   end
 
   def import_publication panopticon_id
