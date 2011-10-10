2333272490ea0ac09dcbf1174ad27df1fa4e095d
Paul Battley <pbattley@gmail.com>
2011-10-10 12:10:16 +0100

    Fix relative path for static symlinks.

--- jenkins.sh
+++ jenkins.sh
@@ -6,7 +6,7 @@ bundle exec rake stats
 # DELETE STATIC SYMLINKS AND RECONNECT...
 for d in images javascripts templates stylesheets; do
   rm -f public/$d
-  ln -s ../../../Static/$d public/
+  ln -s ../../../Static/workspace/public/$d public/
 done
 
 export DISPLAY=:99
