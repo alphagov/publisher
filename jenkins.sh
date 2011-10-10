a0e7154e0c8d82f9cd73f5e369c1aae9b1118a5b
Paul Battley <pbattley@gmail.com>
2011-10-10 12:07:52 +0100

    javascript*s*

--- jenkins.sh
+++ jenkins.sh
@@ -4,12 +4,10 @@ bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
 bundle exec rake stats
 
 # DELETE STATIC SYMLINKS AND RECONNECT...
-cd public
-for d in images javascript templates stylesheets; do
-  rm -f $d
-  ln -s ../../../Static/$d .
+for d in images javascripts templates stylesheets; do
+  rm -f public/$d
+  ln -s ../../../Static/$d public/
 done
-cd -
 
 export DISPLAY=:99
 bundle exec rake ci:setup:testunit test:units test:functionals test:integration
