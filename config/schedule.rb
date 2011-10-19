77722fc7ef54bf843e4435476f2509f2dfd11844
James Stewart <jys@ketlai.co.uk>
2011-10-19 13:41:28 +0100

    Run fact check email check every 5 minutes rather than every hour

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 set :output, {:error => 'log/cron.error.log', :standard => 'log/cron.log'}
 job_type :run_script,  'cd :path && RAILS_ENV=:environment script/:task'
 
-every 1.hour do
+every 5.minutes do
   run_script "mail_fetcher"
 end
