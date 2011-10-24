34967622412594f28e6022739eed17a047fda097
David Heath <david@davidheath.org>
2011-10-24 13:08:56 +0000

    Allow a guide to be saved with a blank part.

--- test/integration/assignment_test.rb
+++ test/integration/assignment_test.rb
@@ -47,5 +47,32 @@ class GuideAssignmentTest < ActionDispatch::IntegrationTest
       assert_equal charlie, guide.editions.last.assigned_to
     end
   end
+
+  test "can assign a new guide without editing the part" do
+    without_metadata_denormalisation Guide do
+      stub_request(:get, "http://panopticon.test.gov.uk/artefacts/2356.js").
+        to_return(status: 200, body: "{}", headers: {})
+
+      # This isn't right, really need a way to run actions when
+      # logged in as particular users without having Signonotron running.
+      #
+      alice   = FactoryGirl.create(:user)
+
+      bob     = FactoryGirl.create(:user, name: "Bob")
+      charlie = FactoryGirl.create(:user, name: "Charlie")
+
+      guide = FactoryGirl.create(:guide, panopticon_id: 2356)
+
+      visit "/admin/guides/#{guide.to_param}"
+
+      select "Bob", from: "Assigned to"
+      click_on "Save"
+      wait_until { page.has_content? "successfully updated" }
+
+      guide.reload
+      assert_equal bob, guide.editions.last.assigned_to
+    end
+  end
+
 end
 
