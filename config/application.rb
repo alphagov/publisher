ee5cb65a43a4a15f9d77ff5bada3a2f2d1533543
Paul Battley <pbattley@gmail.com>
2011-10-27 16:24:44 +0100

    Use an app/presenters directory for presenter(s)

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Publisher
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += %W(#{config.root}/lib #{config.root}/app/presenters)
 
     config.generators do |g|
         g.orm :mongoid
