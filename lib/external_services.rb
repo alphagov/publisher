bd06ab01e6d4731ecde7673c4483e433d14e0446
Dafydd Vaughan <dafydd@alphagov.co.uk>
2011-11-09 15:04:25 +0000

    changing external services to use plek for environments and urls

--- lib/external_services.rb
+++ lib/external_services.rb
@@ -6,49 +6,53 @@ module ExternalServices
   end
 
   def api_host
-    case local_environment
-    when 'development','test'
-      "publisher.dev.gov.uk"
-    when 'production'
-       "api.alpha.gov.uk"
-    else
-       "guides.#{local_environment}.alphagov.co.uk:8080"
-    end
+    Plek.current.find("publisher")
+    #case local_environment
+    #when 'development','test'
+    #  "publisher.dev.gov.uk"
+    #when 'production'
+    #   "api.alpha.gov.uk"
+    #else
+    #   "guides.#{local_environment}.alphagov.co.uk:8080"
+    #end
   end
 
   def front_end_host
-    case local_environment
-    when 'development','test'
-      "http://www.dev.gov.uk"
-    when 'production'
-      "http://www.production.alphagov.co.uk"
-    when 'staging'
-      "http://demo.alphagov.co.uk"
-    else
-      "http://www..#{local_environment}.alphagov.co.uk"
-    end
+    Plek.current.find("frontend")
+    #case local_environment
+    #when 'development','test'
+    #  "http://www.dev.gov.uk"
+    #when 'production'
+    #  "http://www.production.alphagov.co.uk"
+    #when 'staging'
+    #  "http://demo.alphagov.co.uk"
+    #else
+    #  "http://www..#{local_environment}.alphagov.co.uk"
+    #end
   end
 
   def asset_host
-    case local_environment
-    when 'development','test'
-      ""
-    when 'production'
-      "http://static.production.alphagov.co.uk"
-    else
-      "http://#{local_environment}.alphagov.co.uk:8080"
-    end
+    Plek.current.find("static")
+    #case local_environment
+    #when 'development','test'
+    #  ""
+    #when 'production'
+    #  "http://static.production.alphagov.co.uk"
+    #else
+    #  "http://#{local_environment}.alphagov.co.uk:8080"
+    #end
   end
 
   def imminence_api_host
-    case local_environment
-    when 'development','test'
-      "imminence.dev.gov.uk"
-    when 'production'
-      "imminence.production.alpha.gov.uk"
-    else
-      "imminence.#{local_environment}.alphagov.co.uk:8080"
-    end
+    Plek.current.find("data")
+    #case local_environment
+    #when 'development','test'
+    #  "imminence.dev.gov.uk"
+    #when 'production'
+    #  "imminence.production.alpha.gov.uk"
+    #else
+    #  "imminence.#{local_environment}.alphagov.co.uk:8080"
+    #end
   end
 
   extend self
