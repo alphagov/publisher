5dec238c525d7e7ec28900530e6c2a0aebe965cb
Craig R Webster <craig@barkingiguana.com>
2011-11-14 10:35:25 +0000

    Open a new connection for each worker that Passenger spawns

--- config/initializers/messaging.rb
+++ config/initializers/messaging.rb
@@ -6,8 +6,19 @@
 require 'messenger'
 
 unless Rails.env.test?
-  Messenger.transport = Stomp::Client.new "stomp://support.cluster:61613"
-  Publication.marples_transport = Stomp::Client.new 'stomp://support.cluster:61613'
   Publication.marples_client_name = 'publisher'
   Publication.marples_logger = Rails.logger
+  stomp_url = "stomp://support.cluster:61613"
+
+  if defined?(PhusionPassenger)
+    PhusionPassenger.on_event(:starting_worker_process) do |forked|
+      if forked
+        Messenger.transport = Stomp::Client.new stomp_url
+        Publication.marples_transport = Stomp::Client.new stomp_url
+      end
+    end
+  else
+    Messenger.transport = Stomp::Client.new stomp_url
+    Publication.marples_transport = Stomp::Client.new stomp_url
+  end
 end
