42fda4f0e005d74a0977b3b65abd337d997a0a0e
Dafydd Vaughan <dafydd@alphagov.co.uk>
2011-11-25 10:31:13 +0000

    setting test environment to use preview static host this will change to something more permanent/robust later

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,6 +33,6 @@ Publisher::Application.configure do
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
   
-  config.middleware.use Slimmer::App
+  config.middleware.use Slimmer::App, :asset_host => "http://static.preview.alphagov.co.uk"
   config.action_mailer.default_url_options = { :host => "example.com" }
 end
