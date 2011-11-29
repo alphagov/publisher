0b8ec81d173011296d1d1c58c4794264e03ca0f4
Dafydd Vaughan <dafydd@alphagov.co.uk>
2011-11-29 14:43:23 +0000

    making slimmer work in tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,5 +36,3 @@ Publisher::Application.configure do
   config.middleware.use Slimmer::App, :asset_host => "http://static.preview.alphagov.co.uk"
   config.action_mailer.default_url_options = { :host => "example.com" }
 end
-
-require Rails.root.join('test/support/skip_slimmer')
