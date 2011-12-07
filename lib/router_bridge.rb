14317f2e0090704f788cf03c1d4b97c8153a0b58
Dafydd Vaughan <dafydd@alphagov.co.uk>
2011-12-07 11:12:40 +0000

    register guide/programme parts, video guides, print versions and local transactions as routes

--- lib/router_bridge.rb
+++ lib/router_bridge.rb
@@ -19,22 +19,33 @@ class RouterBridge
     marples.join
   end
 
+  def register_multi_part_publication(default_route_params, publication)
+    register_route(default_route_params.merge(:incoming_path  => "/#{publication['slug']}/print"))
+
+    publication.published_edition.parts.each do |part|
+      register_route(default_route_params.merge(:incoming_path  => "/#{publication['slug']}/#{part['slug']}"))
+    end
+    
+    if publication.has_video?
+      register_route(default_route_params.merge(:incoming_path  => "/#{publication['slug']}/video"))
+    end
+  end
+  
+  def register_local_transaction(default_route_params, publication)
+    register_route(default_route_params.merge(:incoming_path => "/#{publication['slug']}/not_found"))
+  end
+    
   def register_publication publication
-    register_route(
-      :application_id => 'frontend',
-      :incoming_path  => "/#{publication['slug']}",
-      :route_type     => :full
-    )
-    register_route(
-      :application_id => 'frontend',
-      :incoming_path  => "/#{publication['slug']}.json",
-      :route_type     => :full
-    )
-    register_route(
-      :application_id => 'frontend',
-      :incoming_path  => "/#{publication['slug']}.xml",
-      :route_type     => :full
-    )
+    default_route_params = {:application_id => 'frontend', :route_type => :full}
+    register_route(default_route_params.merge(:incoming_path  => "/#{publication['slug']}"))
+    register_route(default_route_params.merge(:incoming_path  => "/#{publication['slug']}.json"))
+    register_route(default_route_params.merge(:incoming_path  => "/#{publication['slug']}.xml"))
+    
+    if publication.is_a?(Guide) or publication.is_a?(Programme)
+      register_multi_part_publication(default_route_params, publication)
+    elsif publication.is_a?(LocalTransaction)
+      register_local_transaction(default_route_params, publication)
+    end
   end
 
   def register_route route
