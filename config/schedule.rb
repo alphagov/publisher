e87fd07d1b1d68fa6b37228d9e2a1e529ed817d0
Jordan Hatch <jordan@jordanh.net>
2011-12-07 16:25:22 +0000

    Added dashboard update to Whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,10 @@ job_type :run_script,  'cd :path && RAILS_ENV=:environment script/:task'
 
 every 5.minutes do
   run_script "mail_fetcher"
+end                          
+
+every :hour do
+  rake "update_publisher_dashboard"
 end
 
 every :hour do
