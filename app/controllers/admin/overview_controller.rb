f2da3938bd9ecceb891a38ceff3bb9be1fb883d2
Jordan Hatch <jordan@jordanh.net>
2011-12-08 15:48:15 +0000

    Added JSON output to overview controllero

--- app/controllers/admin/overview_controller.rb
+++ app/controllers/admin/overview_controller.rb
@@ -8,5 +8,13 @@ class Admin::OverviewController < InheritedResources::Base
       'Section' => OverviewDashboard.where(:dashboard_type => "Section").order_by([['result_group','ASC']]),
       'Writing Department' => OverviewDashboard.where(:dashboard_type => "Writing Department")
     }
+    
+    respond_to do |format|
+      format.html { # render overview.html.erb 
+        }
+      format.json {
+        render :json => @overviews.to_json
+      }
+    end
   end
 end
