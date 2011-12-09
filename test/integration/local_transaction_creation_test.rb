db8c2a3193df03497a1b80841c5ebd8779309e1d
James Stewart <jys@ketlai.co.uk>
2011-12-09 12:44:42 +0000

    Add very basic integration test for local transaction creation

---
+++ test/integration/local_transaction_creation_test.rb
@@ -0,0 +1,17 @@
+require 'integration_test_helper'
+
+class LocalTransactionCreationTest < ActionDispatch::IntegrationTest
+  test "creating a local transaction from panopticon requests an LGSL code" do
+    setup_users
+    
+    panopticon_has_metadata(
+      "id" => 2357,
+      "slug" => "foo-bar",
+      "kind" => "local_transaction",
+      "name" => "Foo bar"
+    )
+    
+    visit "/admin/publications/2357"
+    assert page.has_content? "We need a bit more information to create your local transaction."
+  end
+end
