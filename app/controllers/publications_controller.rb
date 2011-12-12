97a36a30be886eda17fd7540783af0482cde2562
Gareth Rushgrove <gareth@morethanseven.net>
2011-12-12 12:55:28 +0000

    whitespace fixes

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -3,14 +3,14 @@ require 'api/generator'
 class PublicationsController < ApplicationController
   caches_page :index
   respond_to :json, :html
-  
+
   def show
     if section_name = Publication::SECTIONS.detect { |s| s.parameterize.to_s == params[:id] }
       data = show_section(section_name)
     else
       data = show_publication(params[:id], params[:edition], params[:snac])
     end
-    
+
     if data
       respond_with(data)
     else
@@ -20,7 +20,6 @@ class PublicationsController < ApplicationController
 
   def show_section(name)
     publications = Publication.where(section: name).collect(&:published_edition).compact
-      
     publications = publications.to_a.collect do |g|
       {
         :title => g.title,
@@ -29,14 +28,14 @@ class PublicationsController < ApplicationController
         :type => g.container.class.to_s.underscore
       }
     end
-    
+
     return { :name => name, :type => 'section', :publications => publications }
   end
 
   def show_publication(slug, edition, snac)
     edition = Publication.find_and_identify_edition(slug, edition)
     return nil if edition.nil?
-    
+
     options = {}
     allowed_options = [:snac,:all]
     allowed_options.each do |a|
@@ -55,10 +54,10 @@ class PublicationsController < ApplicationController
         :slug => g.container.slug
       }
     end
-    if params[:callback]  
-      render :json=> details.to_json, :callback => params[:callback]
+    if params[:callback]
+      render :json => details.to_json, :callback => params[:callback]
     else
-      respond_with details 
+      respond_with details
     end
   end
 end
--- app/models/guide_edition.rb
+++ app/models/guide_edition.rb
@@ -1,10 +1,10 @@
 class GuideEdition < Edition
   embedded_in :guide
   embeds_many :parts
-  
+
   accepts_nested_attributes_for :parts, :allow_destroy => true,
     :reject_if => proc { |attrs| attrs['title'].blank? and attrs['body'].blank? }
-  
+
   field :video_url,	:type => String
   field :video_summary, :type => String
 
@@ -15,16 +15,16 @@ class GuideEdition < Edition
     new_edition.parts = self.parts.map {|p| p.dup }
     new_edition
   end
-  
+
   def order_parts
     ordered_parts = parts.sort_by { |p| p.order ? p.order : 99999 }
     ordered_parts.each_with_index do |obj, i|
       obj.order = i + 1
     end
   end
-  
+
   def container
     self.guide
   end
-  
+
 end
