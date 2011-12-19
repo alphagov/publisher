c57507558d0d9156ff0879568366582ee3b787b7
James Stewart <jys@ketlai.co.uk>
2011-12-19 18:46:02 +0000

    Update to Dai and Craig's messaging fix

--- config/initializers/messaging.rb
+++ config/initializers/messaging.rb
@@ -3,18 +3,18 @@
 require 'messenger'
 
 ActionDispatch::Reloader.to_prepare do
-  Publication.marples_client_name ||= 'publisher'
-  Publication.marples_logger ||= Rails.logger
+  WholeEdition.marples_client_name ||= 'publisher'
+  WholeEdition.marples_logger ||= Rails.logger
 
   if Rails.env.test? or ENV['NO_MESSENGER'].present?
     Messenger.transport ||= Marples::NullTransport.instance
-    Publication.marples_transport ||= Marples::NullTransport.instance
+    WholeEdition.marples_transport ||= Marples::NullTransport.instance
   else
     # TODO: Check if this is thread/forked process safe under passenger. Possible risk
     # that client connections get copied when passenger forks a process but the mutexes
     # protecting those connections do not.
     stomp_url = "failover://(stomp://support.cluster:61613,stomp://support.cluster:61613)"
     Messenger.transport ||= Stomp::Client.new stomp_url
-    Publication.marples_transport ||= Stomp::Client.new stomp_url
+    WholeEdition.marples_transport ||= Stomp::Client.new stomp_url
   end
 end
