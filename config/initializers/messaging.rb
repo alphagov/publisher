b141bfd2d3a1a35c0eafb61d7f4f90dc598e80fb
Jordan Hatch <jordan@jordanh.net>
2011-12-19 16:34:48 +0000

    pplying Craig/Daii's fix for providing messenger transports

--- config/initializers/messaging.rb
+++ config/initializers/messaging.rb
@@ -1,28 +1,20 @@
 # This configuration is suitable for development, it should be managed by puppet
 # in production.
-# TODO: Check if this is thread/forked process safe under passenger. Possible risk
-# that client connections get copied when passenger forks a process but the mutexes
-# protecting those connections do not.
 require 'messenger'
 
-Publication.marples_client_name = 'publisher'
-Publication.marples_logger = Rails.logger
+ActionDispatch::Reloader.to_prepare do
+  Publication.marples_client_name ||= 'publisher'
+  Publication.marples_logger ||= Rails.logger
 
-if Rails.env.test? or ENV['NO_MESSENGER'].present?
-  Messenger.transport = Marples::NullTransport.instance
-  Publication.marples_transport = Marples::NullTransport.instance
-else
-  stomp_url = "failover://(stomp://support.cluster:61613,stomp://support.cluster:61613)"
-
-  if defined?(PhusionPassenger)
-    PhusionPassenger.on_event(:starting_worker_process) do |forked|
-      if forked
-        Messenger.transport = Stomp::Client.new stomp_url
-        Publication.marples_transport = Stomp::Client.new stomp_url
-      end
-    end
+  if Rails.env.test? or ENV['NO_MESSENGER'].present?
+    Messenger.transport ||= Marples::NullTransport.instance
+    Publication.marples_transport ||= Marples::NullTransport.instance
   else
-    Messenger.transport = Stomp::Client.new stomp_url
-    Publication.marples_transport = Stomp::Client.new stomp_url
+    # TODO: Check if this is thread/forked process safe under passenger. Possible risk
+    # that client connections get copied when passenger forks a process but the mutexes
+    # protecting those connections do not.
+    stomp_url = "failover://(stomp://support.cluster:61613,stomp://support.cluster:61613)"
+    Messenger.transport ||= Stomp::Client.new stomp_url
+    Publication.marples_transport ||= Stomp::Client.new stomp_url
   end
 end
