07134d055bf29bcbca316d93047590d8601d778b
James Stewart <jys@ketlai.co.uk>
2011-12-27 19:39:07 +0000

    DRY up loading of publications

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -2,15 +2,25 @@ require 'api/generator'
 
 class PublicationsController < ApplicationController
   respond_to :json
+  before_filter :find_publication
 
   def show
     Rails.logger.info("pubctrl: enter #{Time.now.to_f}")
     data = compose_publication(params[:id], params[:edition], params[:snac])
+    head 404 and return unless data
 
-    if data
-      respond_with(data)
+    data = compose_publication(params[:id], params[:edition], params[:snac])
+    respond_with(data)
+  end
+
+  def verify_snac
+    head 404 and return unless @edition
+    matching_code = params[:snac_codes].detect { |snac| publication.verify_snac(snac) }
+
+    if matching_code
+      render :json => { snac: matching_code }
     else
-      head 404 and return
+      render :text => '', :status => 422
     end
   end
 
@@ -19,7 +29,7 @@ protected
     local_request?
   end
 
-  def compose_publication(slug, edition_number, snac)
+  def find_publication
     edition_number = nil unless allow_preview?
     edition = WholeEdition.find_and_identify_edition(slug, edition_number)
 
@@ -28,5 +38,11 @@ protected
     options = {:snac => params[:snac], :all => params[:all]}.select { |k, v| v.present? }
     result = Api::Generator.edition_to_hash(edition, options)
     result
+    @edition = WholeEdition.find_and_identify(slug, edition_number)
+  end
+
+  def compose_publication(slug, edition_number, snac)
+    options = {:snac => params[:snac], :all => params[:all] }.select { |k, v| v.present? }
+    Api::Generator.edition_to_hash(@edition, options)
   end
 end
