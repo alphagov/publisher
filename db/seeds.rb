50d86ccff093127ef13dcbf67fde16915e0d9c51
James Stewart <jys@ketlai.co.uk>
2011-12-27 20:02:40 +0000

    The notion of expectations has been around a long time. No need to create them on app launch any more

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+Expectation.create :css_class=>"card_payment",  :text=>"Credit card required"
+Expectation.create :css_class=>"debit_card_payment",  :text=>"Debit card required"
+Expectation.create :css_class=>"debit_or_credit_card_payment",  :text=>"Debit or Credit card required"
+Expectation.create :css_class=>"need_id",       :text=>"Proof of identification required"
+Expectation.create :css_class=>"need_passport", :text=>"A valid passport required"
+Expectation.create :css_class=>"need_ni",       :text=>"National Insurance Number required"
+Expectation.create :css_class=>"need_address",  :text=>"3 years of address details required"
+Expectation.create :css_class=>"offline_steps", :text=>"Includes offline steps"
+Expectation.create :css_class=>"over_18",       :text=>"You must be over 18"
+Expectation.create :css_class=>"over_16",       :text=>"You must be over 16"
+Expectation.create :css_class=>"england_only",  :text=>"Available in England only"
+Expectation.create :css_class=>"england_wales", :text=>"Available in England and Wales only"
+Expectation.create :css_class=>"not_ni",        :text=>"Not available in Northern Ireland"
