13d47f46981f15a4ce8a2cf3c5c85bb8a88d84b0
James Stewart <jys@ketlai.co.uk>
2011-12-27 19:25:55 +0000

    Don't look for the edition twice when creating from panopticon data

--- app/controllers/admin/publications_controller.rb
+++ app/controllers/admin/publications_controller.rb
@@ -1,14 +1,20 @@
 class Admin::PublicationsController < Admin::BaseController
   def show
-    publication = WholeEdition.where(panopticon_id: params[:id]).first
-    render_new_form(publication) and return unless publication.persisted?
-
-    destination = '/admin/' + publication.class.name.tableize + '/' + publication.id.to_s
-    destination += '?return_to=' + params[:return_to] if params[:return_to]
-    redirect_to destination
+    publication = WholeEdition.create_from_panopticon_data(params[:id], current_user)
+    if publication.persisted?
+      render_new_form(publication) and return
+    else
+      redirect_to(return_to_destination(publication)) and return
+    end
   end
 
   protected
+    def return_to_destination(publication)
+      destination = "/admin/#{publication.class.name.tableize}/#{publication.id}"
+      destination += '?return_to=' + params[:return_to] if params[:return_to]
+      destination
+    end
+
     def render_new_form(publication)
       @publication = publication
       setup_view_paths_for(publication)
