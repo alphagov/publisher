5a0ba7c2c369affd0549a30a39177153efd82e82
James Stewart <jys@ketlai.co.uk>
2011-12-30 15:12:47 +0000

    Stop duplicating queries when creating from panopticon data

--- app/controllers/admin/publications_controller.rb
+++ app/controllers/admin/publications_controller.rb
@@ -1,22 +1,15 @@
 class Admin::PublicationsController < Admin::BaseController
   def show
-    publication = Publication.where(panopticon_id: params[:id]).first
+    @publication = Publication.create_from_panopticon_data(params[:id], current_user)
+    render_new_form(publication) and return unless @publication.persisted?
 
-    unless publication.present?
-      publication = Publication.create_from_panopticon_data(params[:id], current_user)
-      unless publication.persisted?
-        render_new_form(publication) and return
-      end
-    end
-
-    destination = '/admin/' + publication.class.name.tableize + '/' + publication.id.to_s
+    destination = '/admin/' + @publication.class.name.tableize + '/' + @publication.id.to_s
     destination += '?return_to=' + params[:return_to] if params[:return_to]
     redirect_to destination
   end
 
   protected
     def render_new_form(publication)
-      @publication = publication
       prepend_view_path "app/views/admin/publication_subclasses"
       prepend_view_path admin_template_folder_for(publication)
       render template: 'new'
