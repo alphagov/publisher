90a223b93cbc2af8f15393b26b70977219b6b92b
Mat Wall (mwall@alphagov.co.uk <mwall@ubuntu-vm-mwall.(none)>
2012-01-12 13:19:12 +0000

    Adding logging in attempt to debug timeout issues

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -4,6 +4,7 @@ class PublicationsController < ApplicationController
   respond_to :json
 
   def show
+    Rails.logger.info("pubctrl: enter #{Time.now.to_f}")
     data = compose_publication(params[:id], params[:edition], params[:snac])
 
     if data
@@ -13,17 +14,23 @@ class PublicationsController < ApplicationController
     end
   end
 
-protected
+  protected
   def allow_preview?
     local_request?
   end
 
   def compose_publication(slug, edition_number, snac)
+    Rails.logger.info("pubctrl: compose #{slug} #{edition_number}")
     edition_number = nil unless allow_preview?
+    Rails.logger.info("pubctrl: finding publication edition #{Time.now.to_f}")
     edition = Publication.find_and_identify_edition(slug, edition_number)
+    Rails.logger.info("pubctrl: found edition #{Time.now.to_f}")
     return nil if edition.nil?
 
-    options = {:snac => params[:snac], :all => params[:all] }.select { |k, v| v.present? }
-    Api::Generator.edition_to_hash(edition, options)
+    options = {:snac => params[:snac], :all => params[:all]}.select { |k, v| v.present? }
+    Rails.logger.info("pubctrl: generating hash #{Time.now.to_f}")
+    result = Api::Generator.edition_to_hash(edition, options)
+    Rails.logger.info("pubctr: exit compose #{Time.now.to_f}")
+    result
   end
 end
