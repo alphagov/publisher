659625a53c2b22242860437f73a075a8e688ffb8
Gareth Rushgrove <gareth@morethanseven.net>
2012-01-23 15:02:09 +0000

    remove gelf logger in favour of graylogtail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,6 @@ Publisher::Application.configure do
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
-  config.logger = GELF::Logger.new("graylog.cluster", "12201", max_size = 'WAN', { :facility => "publisher" })
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
