88ed5225580f418cbdff7bf05099ba5526899312
James Stewart <james@jystewart.net>
2012-03-16 08:49:23 +0000

    Remove extraneous logging code
    This logging code was put in for a particular debugging exercise. It's no longer needed

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -4,7 +4,6 @@ class PublicationsController < ApplicationController
   respond_to :json
 
   def show
-    Rails.logger.info("pubctrl: enter #{Time.now.to_f}")
     data = compose_publication(params[:id], params[:edition], params[:snac])
 
     if data
@@ -20,17 +19,12 @@ class PublicationsController < ApplicationController
   end
 
   def compose_publication(slug, edition_number, snac)
-    Rails.logger.info("pubctrl: compose #{slug} #{edition_number}")
     edition_number = nil unless allow_preview?
-    Rails.logger.info("pubctrl: finding publication edition #{Time.now.to_f}")
     edition = WholeEdition.find_and_identify(slug, edition_number)
-    Rails.logger.info("pubctrl: found edition #{Time.now.to_f}")
     return nil if edition.nil?
 
     options = {:snac => params[:snac], :all => params[:all]}.select { |k, v| v.present? }
-    Rails.logger.info("pubctrl: generating hash #{Time.now.to_f}")
     result = Api::Generator.edition_to_hash(edition, options)
-    Rails.logger.info("pubctr: exit compose #{Time.now.to_f}")
     result
   end
 end
