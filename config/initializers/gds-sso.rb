1df853b57754cb03c20dc70b8f69a0fa10f4892e
David Heath <david.heath@digital.cabinet-office.gov.uk>
2012-03-30 23:57:08 -0700

    Settings to allow us to do real oauth in development.

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -4,3 +4,5 @@ GDS::SSO.config do |config|
   config.oauth_secret = 'secret'
   config.oauth_root_url = Plek.current.find("authentication")
 end
+# Omniauth can't figure out the hostname because it's being proxied through nginx
+OmniAuth.config.full_host = "http://publisher.dev.gov.uk"
