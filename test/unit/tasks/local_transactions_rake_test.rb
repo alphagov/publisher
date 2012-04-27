376447ac06139d33380c4195d1f63ba97a6cadc2
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-04-27 15:42:15 +0100

    Tightened up local_transaction import rake task tests.

--- test/unit/tasks/local_transactions_rake_test.rb
+++ test/unit/tasks/local_transactions_rake_test.rb
@@ -3,35 +3,32 @@ require 'rake'
 
 class LocalTransactionsRakeTest < ActiveSupport::TestCase
 
-  def task_path
-    "lib/tasks/local_transactions"
-  end
-
   setup do
     @rake = Rake::Application.new
     Rake.application = @rake
-    Rake.application.rake_require(task_path, [Rails.root.to_s], loaded_files_excluding_current_rake_file)
+    Rake.application.rake_require("lib/tasks/local_transactions", [Rails.root.to_s], [])
     Rake::Task.define_task(:environment)
-  end
-
-  def loaded_files_excluding_current_rake_file
-    $".reject {|file| file == Rails.root.join("#{task_path}.rake").to_s }
   end
 
   context "local_transactions:fetch" do
     setup do
       @task_name = "local_transactions:fetch"
-      @rake["local_transactions:import"].clear
 
       @saved_file = Rails.root.join('data', 'test_local_interactions.csv')
+
+      LocalServiceImporter.stubs(:new).returns(stub(:run))
+      LocalInteractionImporter.stubs(:new).returns(stub(:run))
     end
 
-    should "download the CSV file and write to disk" do
+    should "download the service details CSV file and write it to disk" do
       stub_request(:get, "http://local.direct.gov.uk/Data/local_authority_service_details.csv").
         to_return(:status => 200, :body => "Example CSV Content")
 
       ENV['FILENAME'] = @saved_file.to_s
-      Rake::Task[@task_name].execute
+
+      silence_stream(STDERR) do
+        @rake[@task_name].invoke
+      end
 
       assert File.exists?(@saved_file)
       assert_equal "Example CSV Content", File.open(@saved_file).read
@@ -39,7 +36,7 @@ class LocalTransactionsRakeTest < ActiveSupport::TestCase
     end
 
     teardown do
-      File.delete(@saved_file)
+      @saved_file.delete if @saved_file.exist?
     end
   end
 
@@ -49,27 +46,32 @@ class LocalTransactionsRakeTest < ActiveSupport::TestCase
       @source_filename = 'test.csv'
     end
 
-    should "call LocalServiceImporter and LocalInteractionImporter with the correct filenames" do
+    should "call LocalServiceImporter and LocalInteractionImporter with the correct filehandles" do
       ENV['SOURCE'] = @source_filename
 
-      LocalServiceImporter.expects(:new).returns(stub(:run))
-      LocalInteractionImporter.expects(:new).returns(stub(:run))
+      services_filehandle = stub()
+      interactions_filehandle = stub()
 
-      File.expects(:open).with('data/local_services.csv', 'r:Windows-1252:UTF-8').returns(stub())
-      File.expects(:open).with(@source_filename, 'r:Windows-1252:UTF-8').returns(stub())
+      File.expects(:open).with('data/local_services.csv', 'r:Windows-1252:UTF-8').returns(services_filehandle)
+      File.expects(:open).with(@source_filename, 'r:Windows-1252:UTF-8').returns(interactions_filehandle)
 
-      Rake::Task[@task_name].execute
+      LocalServiceImporter.expects(:new).with(services_filehandle, anything()).returns(stub(:run))
+      LocalInteractionImporter.expects(:new).with(interactions_filehandle, anything()).returns(stub(:run))
+
+      silence_stream(STDERR) do
+        @rake[@task_name].invoke
+      end
     end
 
     should "exit when no source is provided" do
       ENV['SOURCE'] = nil
 
       assert_raise SystemExit do
-        Rake::Task[@task_name].execute
+        silence_stream(STDOUT) do
+          @rake[@task_name].invoke
+        end
       end
     end
-
   end
-
 
 end
