194a340b4dc025210db281d5a6fff0217d5f60bd
James Stewart <james@jystewart.net>
2012-05-03 13:49:57 +0100

    Fix adding new part
    The javascript for this wasn't being included, so it is now. I've also removed all references
    to the old accordion JS and switched to the new bootstrap approach.

--- app/assets/javascripts/guides.js
+++ app/assets/javascripts/guides.js
@@ -4,7 +4,7 @@ $(function() {
   $(".collapse").collapse();
 
   // simulate a click on the first part to open it
-  $('#parts .accordion-toggle').first().trigger('click')
+  $('#parts .part .accordion-body').first().collapse('show');
 
   $('input.title').
     live('change', function () {
@@ -24,18 +24,11 @@ $(function() {
 
   $('.add-associated').bind('associated-added', function () {
     var active_index = $('#parts div.part').length;
-    var my_opts = accordion_opts;
-    my_opts.active = active_index - 1;
-    $('#parts').sortable('destroy').accordion("destroy").
-      accordion(my_opts).sortable(sortable_opts);
     var new_part = $('#parts .part:last-child');
+    new_part.find('.collapse').attr('id', 'new-part-' + active_index).collapse('show');
+    new_part.find('a.accordion-toggle').attr('href', '#new-part-' + active_index);
+
     new_part.find('input.order').val(active_index);
     new_part.find('.title').focus();
-  });
-
-  $('body').bind('associated-removed', function () {
-    $('#parts').sortable('destroy').accordion("destroy").
-      accordion(accordion_opts).sortable(sortable_opts);
-    return false;
   });
 });
