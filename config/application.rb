f0a7f2a81cb4a656ad386f4e2e78239ca9eae67b
James Stewart <james@jystewart.net>
2012-05-08 11:04:13 +0100

    Tweak assets config.

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module Publisher
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/lib #{config.root}/app/presenters)
 
-    config.assets.precompile += %W(application.css bootstrap-responsive.min.css application.js 
+    config.assets.precompile += %W(application.css bootstrap.min.css bootstrap-responsive.min.css application.js 
         bootstrap.min.js form-tools.js jquery.min.js jquery.mustache.js multi-part.js 
         publications.js glyphicons-halflings-white.png glyphicons-halflings.png 
         header-crown.png icon-answer.png icon-guide.png icon-localtransaction.png 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,9 +56,10 @@ Publisher::Application.configure do
   # config.assets.css_compressor = :yui
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
+
   config.action_mailer.delivery_method = :ses
 end
