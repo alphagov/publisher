bc58dc86650d1aeadf6d9fd20fd52a677def8bad
Paul Battley <pbattley@gmail.com>
2012-05-17 17:51:50 +0100

    Moved tests to models gem.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,25 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "creating a transaction with the initial details creates a valid transaction" do
-    user = User.create(:name => "bob")
-    trans = user.create_whole_edition(:transaction, title: "test", slug: "test", panopticon_id: 1234)
-    assert trans.valid?
-  end
-
   test "it doesn't try to send a fact check email if no addresses were given" do
     user = User.create(:name => "bob")
     NoisyWorkflow.expects(:request_fact_check).never
     trans = user.create_whole_edition(:transaction, title: "test answer", slug: "test", panopticon_id: 123)
     assert ! user.send_fact_check(trans, {comment: "Hello"})
-  end
-
-  test "user can't okay a publication they've sent for review" do
-    user = User.create(:name => "bob")
-
-    trans = user.create_whole_edition(:transaction, title: "test answer", slug: "test", panopticon_id: 123)
-    user.request_review(trans, {comment: "Hello"})
-    assert ! user.approve_review(trans, {comment: "Hello"})
   end
 
   test "when an user publishes a guide, a status message is sent on the message bus" do
@@ -34,17 +20,4 @@ class UserTest < ActiveSupport::TestCase
     Messenger.instance.expects(:published).with(trans).once
     user.publish trans, {comment: "Published because I did"}
   end
-
-  test "Edition becomes assigned to user when user is assigned an edition" do
-    boss_user = User.create(:name => "Mat")
-    worker_user = User.create(:name => "Grunt")
-
-    publication = boss_user.create_whole_edition(:answer, title: "test answer", slug: "test", panopticon_id: 123)
-    boss_user.assign(publication, worker_user)
-    publication.save
-    publication.reload
-
-    assert_equal(worker_user, publication.assigned_to)
-  end
-
 end
