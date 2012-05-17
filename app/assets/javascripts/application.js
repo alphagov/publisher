36f82c22d943cb9e4aa6d952c3a35a8201d2522b
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2012-05-17 17:26:45 +0100

    Revert "Use sprockets directives rather than hardcoding assets in application.rb"
    This reverts commit 57ca5fcb5fe6f6809cf8169368699cbf6f44f6d6.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,13 +1,18 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
-//= require_tree .
+// System wide behaviours
+$(function () {
+  $('.flash-notice').delay(3000).slideUp(300).one('click', function () { $(this).slideUp(300); });
+
+  $('a.preview').attr("target","_blank");
+  $('form.preview').attr("target","_blank");
+
+  $('.confirm form').submit(function(){
+      return confirm('Woah. Scary action, cannot be undone. Continue?');
+  });
+})
+
+// System wide library functions
+var GovUKGuideUtils = {
+  convertToSlug: function(title) {
+    return title.toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'-');
+  }
+}
