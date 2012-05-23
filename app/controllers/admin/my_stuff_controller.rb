71894397616f93cbfe6448e487412e8d586eac29
Kushal Pisavadia <kushi.p@gmail.com>
2012-05-23 06:18:57 -0700

    Show some content for a specific user

--- app/controllers/admin/my_stuff_controller.rb
+++ app/controllers/admin/my_stuff_controller.rb
@@ -1,4 +1,8 @@
 class Admin::MyStuffController < Admin::BaseController
+  respond_to :html, :json
+
   def index
+    @editions = WholeEdition.where(
+      'actions.requester_id' => BSON::ObjectId('4e5e3481e2ba805990000020'))
   end
 end
