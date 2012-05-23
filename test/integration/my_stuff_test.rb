b315b613b6f426c710caf393559212a8428e6aa9
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2012-05-23 16:18:35 +0100

    Add an integration test for the 'my stuff' index.

---
+++ test/integration/my_stuff_test.rb
@@ -0,0 +1,26 @@
+require_relative '../integration_test_helper'
+
+class MyStuffTest < ActionDispatch::IntegrationTest
+
+  def setup
+    alice = FactoryGirl.create(:user, name: "Alice", uid: "alice")
+    GDS::SSO.test_user = alice
+    @user = alice
+  end
+
+  def teardown
+    GDS::SSO.test_user = nil
+  end
+
+  test "filtering by assigned user" do
+    # stub_request(:get, %r{^http://panopticon\.test\.gov\.uk/artefacts/.*\.js$}).
+    #   to_return(status: 200, body: "{}", headers: {})
+
+    @guide = FactoryGirl.create(:guide_edition)
+    @user.record_note @guide, "I like this guide"
+
+    visit "/admin/my_stuff"
+
+    assert page.has_content? @guide.title
+  end
+end
