70829fddc388062860e80cf2b0ab6cd7d5ed7608
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2012-05-25 16:09:16 +0100

    Test viewing publications for another user.

--- test/integration/user_search_test.rb
+++ test/integration/user_search_test.rb
@@ -20,4 +20,22 @@ class UserSearchTest < ActionDispatch::IntegrationTest
 
     assert page.has_content? @guide.title
   end
+
+  test "selecting another user" do
+    @guides = FactoryGirl.build_list(:guide_edition, 2)
+    other_user = FactoryGirl.create(:user, name: "Bob", uid: "bob")
+
+    # Assigning manually so it doesn't show up in Alice's list too
+    @guides[0].assigned_to_id = other_user.id
+    @guides[0].save!
+
+    visit "/admin/user_search"
+    select other_user.name, from: "Filter by user"
+    click_button "Filter"
+    wait_until { page.has_content? "User search" }
+
+    assert page.has_content? @guides[0].title
+    refute page.has_content? @guides[1].title
+  end
+
 end
