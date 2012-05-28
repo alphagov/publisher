d899aaefafa3c45d891e8d127c574809286d990d
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2012-05-28 14:17:55 +0100

    Pull the UserSearchEdition out into a decorator.

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Publisher
     config.assets.prefix = '/assets'
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/lib #{config.root}/app/presenters)
+    config.autoload_paths += %W(#{config.root}/lib #{config.root}/app/presenters #{config.root}/app/decorators)
 
     config.assets.precompile += %W(
         application.css bootstrap.min.css bootstrap-responsive.min.css
