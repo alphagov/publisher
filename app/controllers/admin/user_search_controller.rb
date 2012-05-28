b5c664e307218053ecfcc41546c5c07fabc4fa8a
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2012-05-28 14:52:49 +0100

    Display the user's name in the title.
    Also fixed a bug where a missing or incorrect user ID would cause an
    uncaught exception. Now it 404s.

--- app/controllers/admin/user_search_controller.rb
+++ app/controllers/admin/user_search_controller.rb
@@ -3,21 +3,22 @@ class Admin::UserSearchController < Admin::BaseController
 
   def index
     @user_filter = params[:user_filter] || current_user.uid
-    user = params[:user_filter] ? User.find_by_uid(@user_filter) : current_user
+    @user = params[:user_filter] ? User.find_by_uid(@user_filter) : current_user
+    raise ActionController::RoutingError.new('Not Found') unless @user
 
     # Including recipient_id on actions will include anything that has been
     # assigned to the user we're looking at, but include the check anyway to
     # account for manual assignments
     editions = WholeEdition.any_of(
-      {'assigned_to_id' => user.id},
-      {'actions.requester_id' => user.id},
-      {'actions.recipient_id' => user.id}
+      {'assigned_to_id' => @user.id},
+      {'actions.requester_id' => @user.id},
+      {'actions.recipient_id' => @user.id}
     ).excludes(state: 'archived').order_by(last_updated_at: -1)
 
     # Need separate assignments here because Kaminari won't preserve pagination
     # info across a map, and we don't want to load every edition and paginate
     # the resulting array
     @page_info = editions.page(params[:page]).per(20)
-    @editions = @page_info.map { |e| UserSearchEditionDecorator.new e, user }
+    @editions = @page_info.map { |e| UserSearchEditionDecorator.new e, @user }
   end
 end
