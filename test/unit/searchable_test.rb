03c8b479a7c423662785dcbf59083823261d0b0b
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-06-25 11:33:05 +0100

    Fix link_order for additional_links in search indexing
    Zen Desk: #1735

--- test/unit/searchable_test.rb
+++ test/unit/searchable_test.rb
@@ -8,6 +8,13 @@ class SearchableTest < ActiveSupport::TestCase
     edition
   end
 
+  def create_part(order, name)
+    Part.new(order: order, title: "Title of the #{name} part",
+      body: "this is the content body",
+      slug: "title-of-#{name}-part"
+    )
+  end
+
   test "section name is normalized" do
     edition = template_guide_edition
     edition.section = "Cats and Dogs"
@@ -19,6 +26,21 @@ class SearchableTest < ActiveSupport::TestCase
     edition.section = "Crime and Justice:Prison"
     assert_equal 'crime-and-justice', edition.search_index['section']
     assert_equal 'prison', edition.search_index['subsection']
+  end
+
+  test "should add the correct link order for edition parts" do
+    edition = template_guide_edition
+    edition.parts << create_part(2, "second")
+    edition.parts << create_part(1, "first")
+
+    edition.state = "published"
+    edition.safely.save!
+
+    document = edition.search_index
+
+    assert_equal 2, document['additional_links'].length
+    assert_equal 2, document['additional_links'][0]['link_order']
+    assert_equal 1, document['additional_links'][1]['link_order']
   end
 
 end
