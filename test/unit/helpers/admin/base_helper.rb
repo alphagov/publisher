b902453d22b1123ace69571bd19c7671038382db
Kushal Pisavadia <kushi.p@gmail.com>
2012-06-26 08:03:06 -0700

    Add tests for govspeak_to_text
    There was previously no contract that this helper function had to
    adhere to. This change adds some tests related to markdown and
    govspeak syntax to make sure it does as it says on the tin.

--- test/unit/helpers/admin/base_helper.rb
+++ test/unit/helpers/admin/base_helper.rb
@@ -5,4 +5,22 @@ class Admin::BaseHelperTest < ActionView::TestCase
     edition = FactoryGirl.create(:guide_edition_with_two_govspeak_parts, state: "published")
     assert_equal "Some Part Title! This is some version text. Another Part Title This is link text.", edition.indexable_content_with_parts
   end
+
+  test "should provide same text back" do
+    assert_equal "some dummy text", govspeak_to_text("some dummy text")
+  end
+
+  test "should remove markdown from provided string" do
+    assert_equal "some dummy text", govspeak_to_text("some *dummy* text")
+    assert_equal "some dummy text", govspeak_to_text("some **dummy** text")
+    assert_equal "some dummy text", govspeak_to_text("some\n\n* dummy\ntext")
+    assert_equal "some dummy text", govspeak_to_text("some\n\n1. dummy\ntext")
+  end
+
+  test "should remove govspeak from provided string" do
+    assert_equal "This is a warning callout", govspeak_to_text("%This is a warning callout%")
+    assert_equal "Example : Open the pod bay doors", govspeak_to_text("$E\n**Example**: Open the pod bay doors\n$E")
+    assert_equal "This is a very important message or warning", govspeak_to_text("@This is a very important message or warning@")
+    assert_equal "The VAT rate is 20%", govspeak_to_text("{::highlight-answer}\nThe VAT rate is *20%*\n{:/highlight-answer}")
+  end
 end
