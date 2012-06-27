984fae419caee3e9099509ccf4cf447753ec9390
Jamie Cobbett <jamiecobbett@GDS267.internal.localnet>
2012-06-27 14:40:11 +0100

    Remove obsolete OmniAuth workaround

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -4,5 +4,3 @@ GDS::SSO.config do |config|
   config.oauth_secret = 'secret'
   config.oauth_root_url = Plek.current.find("signon")
 end
-# Omniauth can't figure out the hostname because it's being proxied through nginx
-OmniAuth.config.full_host = "http://publisher.dev.gov.uk"
