c83d75bece240a812015f0d20f1bb87bcd560e18
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-07-19 15:52:29 +0100

    Added output to migrator

--- lib/licence_identifier_migrator.rb
+++ lib/licence_identifier_migrator.rb
@@ -5,13 +5,19 @@ class LicenceIdentifierMigrator
   MAPPING_FILENAME = 'licence_mappings.csv'
   
   def self.update_all
+    counter = 0
     licence_mappings = mappings_as_hash
     LicenceEdition.all.each do |licence_edition|
       licence_identifier = licence_mappings[licence_edition.licence_identifier.to_s]
       if licence_identifier
         licence_edition.update_attribute("licence_identifier", licence_identifier)
+        counter += 1
       end
+      
+      done(counter, "\r")
     end
+    
+    done(counter, "\n")
   end
   
   def self.data_file_path(filename)
@@ -28,6 +34,10 @@ class LicenceIdentifierMigrator
       licence_mappings[row['GDS ID']] = row['Legal_Ref_No']  
     end
     licence_mappings
+  end
+  
+  def self.done(counter, nl)
+    print "Migrated #{counter} LicenceEditions out of #{LicenceEditions.count}.#{nl}"
   end
   
 end
