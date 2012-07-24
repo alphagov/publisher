6d932b00b4096b9542bb05ec1aa413c251f34c4c
Kushal Pisavadia <kushi.p@gmail.com>
2012-07-24 03:51:46 -0700

    Pull out fact checking and publishing into a helper
    Created fact_check_and_publish to simplify steps involving the
    assignment, fact check, and publishing a given set of editions.

--- test/unit/edition_clone_test.rb
+++ test/unit/edition_clone_test.rb
@@ -8,22 +8,25 @@ class EditionCloneTest < ActiveSupport::TestCase
     @artefact = FactoryGirl.create(:artefact, name: "Childcare", slug: "childcare")
   end
 
+  def fact_check_and_publish(edition = nil)
+    @user.start_work(edition)
+    @user.request_review(edition, {comment: "Review this guide please."})
+    @other_user.approve_review(edition, {comment: "I've reviewed it"})
+    @user.send_fact_check(edition, {comment: "Review this guide please.", email_addresses: 'test@test.com'})
+    @user.receive_fact_check(edition, {comment: "No changes needed, this is all correct"})
+    @other_user.approve_fact_check(edition, {comment: "Looks good to me"})
+    @user.publish(edition, {comment: "First edition"})
+  end
+
   test "should clone a published GuideEdition to an AnswerEdition" do
     stub_register_published_content
 
     guide_edition = FactoryGirl.create(:guide_edition, slug: "childcare", title: "One", panopticon_id: @artefact.id)
     guide_edition.save
 
-    @user.start_work(guide_edition)
-    @user.request_review(guide_edition,{comment: "Review this guide please."})
-    @other_user.approve_review(guide_edition, {comment: "I've reviewed it"})
-    @user.send_fact_check(guide_edition,{comment: "Review this guide please.", email_addresses: 'test@test.com'})
-    @user.receive_fact_check(guide_edition, {comment: "No changes needed, this is all correct"})
-    @other_user.approve_fact_check(guide_edition, {comment: "Looks good to me"})
-    @user.publish(guide_edition, {comment: "First edition"})
+    fact_check_and_publish(guide_edition)
 
     answer_edition = guide_edition.build_clone(AnswerEdition)
-    answer_edition.save!
 
     assert_equal GuideEdition, guide_edition.class
     assert_equal AnswerEdition, answer_edition.class
@@ -37,20 +40,51 @@ class EditionCloneTest < ActiveSupport::TestCase
     answer_edition = FactoryGirl.create(:answer_edition, slug: "childcare", title: "One", panopticon_id: @artefact.id)
     answer_edition.save
 
-    @user.start_work(answer_edition)
-    @user.request_review(answer_edition,{comment: "Review this guide please."})
-    @other_user.approve_review(answer_edition, {comment: "I've reviewed it"})
-    @user.send_fact_check(answer_edition,{comment: "Review this guide please.", email_addresses: 'test@test.com'})
-    @user.receive_fact_check(answer_edition, {comment: "No changes needed, this is all correct"})
-    @other_user.approve_fact_check(answer_edition, {comment: "Looks good to me"})
-    @user.publish(answer_edition, {comment: "First edition"})
+    fact_check_and_publish(answer_edition)
 
     guide_edition = answer_edition.build_clone(GuideEdition)
-    guide_edition.save!
 
     assert_equal AnswerEdition, answer_edition.class
     assert_equal GuideEdition, guide_edition.class
 
+    assert_equal guide_edition.title, answer_edition.title
+  end
+
+  test "should convert GuideEdition with parts into an AnswerEdition" do
+    stub_register_published_content
+
+    guide_edition = FactoryGirl.create(:guide_edition, slug: "childcare", title: "One", panopticon_id: @artefact.id)
+    guide_edition.parts.build(title: "Some Part Title!", body: "This is some **version** text.", slug: "part-one")
+    guide_edition.parts.build(title: "Another Part Title", body: "This is [link](http://example.net/) text.", slug: "part-two")
+    guide_edition.save
+
+    fact_check_and_publish(guide_edition)
+
+    answer_edition = guide_edition.build_clone(AnswerEdition)
+
+    assert_equal GuideEdition, guide_edition.class
+    assert_equal AnswerEdition, answer_edition.class
+
+    assert_equal "# Some Part Title!\n\nThis is some **version** text.\n\n# Another Part Title\n\nThis is [link](http://example.net/) text.", guide_edition.whole_body
+    assert_equal guide_edition.whole_body, answer_edition.whole_body
+    assert_equal guide_edition.title, answer_edition.title
+  end
+
+  test "should convert AnswerEdition into a GuideEdition" do
+    stub_register_published_content
+
+    answer_edition = FactoryGirl.create(:answer_edition, slug: "childcare", title: "One", panopticon_id: @artefact.id)
+    answer_edition.body = "Bleep, bloop, blop"
+    answer_edition.save
+
+    fact_check_and_publish(answer_edition)
+
+    guide_edition = answer_edition.build_clone(GuideEdition)
+
+    assert_equal AnswerEdition, answer_edition.class
+    assert_equal GuideEdition, guide_edition.class
+
+    assert_equal "# Part One\n\nBleep, bloop, blop", guide_edition.whole_body
     assert_equal guide_edition.title, answer_edition.title
   end
 end
