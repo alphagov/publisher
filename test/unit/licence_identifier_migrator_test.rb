e977ca4d61b911a4c17febe55e478228d2f98953
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-08-20 12:06:35 +0000

    Switched to using numeric hash keys from mappings

--- test/unit/licence_identifier_migrator_test.rb
+++ test/unit/licence_identifier_migrator_test.rb
@@ -5,10 +5,10 @@ class LicenceIdentifierMigratorTest < ActiveSupport::TestCase
   context "update_all" do
     setup do
       LicenceIdentifierMigrator.stubs(:mappings_as_hash).returns({
-        "1083741393" => "1040001",
-        "1083741799" => "1620001",
-        "1084062157" => "1580003",
-        "1075329002" => "1610001"
+        1083741393 => "1040001",
+        1083741799 => "1620001",
+        1084062157 => "1580003",
+        1075329002 => "1610001"
       })
       @le1 = LicenceEdition.create(licence_identifier: "1083741799", title: "Licence One", panopticon_id: 123)
       @le2 = LicenceEdition.create(licence_identifier: "9999999999", title: "Licence Two", panopticon_id: 321)
