af97da756016bf10b8d52ff0064371338298dbc0
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-09-06 13:15:00 +0000

    Makes test data look more like the real thing

--- test/unit/licence_identifier_migrator_test.rb
+++ test/unit/licence_identifier_migrator_test.rb
@@ -5,10 +5,10 @@ class LicenceIdentifierMigratorTest < ActiveSupport::TestCase
   context "update_all" do
     setup do
       LicenceIdentifierMigrator.stubs(:mappings_as_hash).returns({
-        1083741393 => "1040001",
-        1083741799 => "1620001",
-        1084062157 => "1580003",
-        1075329002 => "1610001"
+        1083741393 => "123-1-1",
+        1083741799 => "146-7-1",
+        1084062157 => "898-1-1",
+        1075329002 => "999-4-1"
       })
       @le1 = LicenceEdition.create(licence_identifier: "1083741799", title: "Licence One", panopticon_id: 123)
       @le2 = LicenceEdition.create(licence_identifier: "9999999999", title: "Licence Two", panopticon_id: 321)
@@ -19,7 +19,7 @@ class LicenceIdentifierMigratorTest < ActiveSupport::TestCase
         LicenceIdentifierMigrator.update_all
       end
       @le1.reload
-      assert_equal "1620001", @le1.licence_identifier
+      assert_equal "146-7-1", @le1.licence_identifier
     end
     
     should "ignore licence editions without a matching licence identifier" do
