e51fb50c453d6a035462a1028aab1e69cfe0c390
Dafydd Vaughan <dafydd.vaughan@digital.cabinet-office.gov.uk>
2012-09-26 16:24:42 +0100

    Stop fact check response import cronjob from running
    This will be returned closer to launch

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,6 @@
 set :output, {:error => 'log/cron.error.log', :standard => 'log/cron.log'}
 job_type :run_script,  'cd :path && RAILS_ENV=:environment script/:task'
 
-every 5.minutes do
-  run_script "mail_fetcher"
-end
-
 every 1.day, :at => '5am' do
   rake "local_transactions:fetch"
 end
