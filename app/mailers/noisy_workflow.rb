b2ed3ea1b7d1207ef4dc9ebb7fd926bd2660dbf6
James Stewart <jys@ketlai.co.uk>
2012-11-20 14:42:21 +0000

    Move email groups for noisy workflow to an initializer
    This is a first step towards making it something that can be set in uploaded
    config, removing the need to switch on environment.

--- app/mailers/noisy_workflow.rb
+++ app/mailers/noisy_workflow.rb
@@ -3,13 +3,6 @@
 class NoisyWorkflow < ActionMailer::Base
   default :from => "Winston (GOV.UK Publisher) <winston@alphagov.co.uk>"
 
-  EMAIL_GROUPS = {
-    :dev => 'govuk-dev@digital.cabinet-office.gov.uk',
-    :franchise_editors => 'freds@alphagov.co.uk',
-    :business => 'publisher-alerts-business@digital.cabinet-office.gov.uk',
-    :citizen => 'publisher-alerts-citizen@digital.cabinet-office.gov.uk'
-  }
-
   def make_noise(action)
     @action = action
 
---
+++ config/initializers/email_groups.rb
@@ -0,0 +1,6 @@
+  EMAIL_GROUPS = {
+    :dev => 'govuk-dev@digital.cabinet-office.gov.uk',
+    :franchise_editors => 'freds@alphagov.co.uk',
+    :business => 'publisher-alerts-business@digital.cabinet-office.gov.uk',
+    :citizen => 'publisher-alerts-citizen@digital.cabinet-office.gov.uk'
+  }
