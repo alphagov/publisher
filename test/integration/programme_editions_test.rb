63aa273faa7789189e564bb5a273384ccc3562a6
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2012-12-10 11:07:42 +0000

    Don't allow slugs to be editable
    For the time being we don't require slugs to be editable.
    
    Also updated the title change in the test to be more indicative behind
    why we're allowing this feature to go through.

--- test/integration/programme_editions_test.rb
+++ test/integration/programme_editions_test.rb
@@ -16,16 +16,16 @@ class ProgrammeEditionsTest < JavascriptIntegrationTest
 
     click_on "Overview"
     within :css, "#overview" do
-      fill_in "Title", :with => "Part One"
+      fill_in "Title", :with => "Imagine this is Welsh"
       fill_in "Body",  :with => "Body text"
-      fill_in "Slug",  :with => "part-one"
+      fill_in "Slug",  :with => "imagine-this-is-welsh"
     end
 
     within(:css, ".workflow_buttons") { click_on "Save" }
 
     assert_include page.body, "Programme edition was successfully updated."
 
-    assert_include page.body, "Part One"
-    assert_include page.body, "part-one"
+    assert_include page.body, "Imagine this is Welsh"
+    assert_not_include page.body, "imagine-this-is-welsh"
   end
 end
