24cad01b8dabc0f97411790d6051386dc7fc67ab
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-01-11 11:32:25 +0000

    Upgrade to new permissions format (Array)
    Also added mongoid migrations.

---
+++ db/migrate/20130111113023_change_permissions_to_array.rb
@@ -0,0 +1,30 @@
+class ChangePermissionsToArray < Mongoid::Migration
+  class User
+    include Mongoid::Document
+    include Mongoid::Timestamps
+
+    field "permissions"
+
+    def self.collection_name
+      "publisher_users"
+    end
+  end
+
+  def self.up
+    User.all.each do |user|
+      if user.permissions.is_a?(Hash)
+        user.permissions = user.permissions["Publisher"]
+        user.save!
+      end
+    end
+  end
+
+  def self.down
+    User.all.each do |user|
+      unless user.permissions.nil?
+        user.permissions = { "Publisher" => user.permissions }
+        user.save!
+      end
+    end
+  end
+end
