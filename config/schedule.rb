9e1cb2462bba17b5d977dec2fa5e3f758c73cdea
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-02-05 15:41:27 +0000

    Note that the file is overwritten on deploy

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,4 @@
+# This file is overwritten on deploy
 set :output, {:error => 'log/cron.error.log', :standard => 'log/cron.log'}
 job_type :run_script,  'cd :path && RAILS_ENV=:environment script/:task'
 
