84123d542f4fe3400e365badf4f2f088ec0058da
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-02-05 15:41:53 +0000

    Make cron jobs runnable
    This brings it in line with:
    https://github.com/alphagov/alphagov-deployment/commit/22a21707cedd054eaf03e52a665d8b82dfc14e6d

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 # This file is overwritten on deploy
 set :output, {:error => 'log/cron.error.log', :standard => 'log/cron.log'}
-job_type :run_script,  'cd :path && RAILS_ENV=:environment script/:task'
+job_type :run_script,  'cd :path && RAILS_ENV=:environment /usr/local/bin/govuk_setenv publisher script/:task'
 
 every 1.day, :at => '5am' do
   rake "local_transactions:fetch"
