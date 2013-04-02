b63c232ca28de2b1bfbed6be3b4a5ac41bed31a2
James Stewart <jys@ketlai.co.uk>
2013-04-02 13:24:43 +0100

    Extract progressing editions into a new class
    Most of the methods in the Admin::EditionsController were to do with
    progressing editions and could more neatly be combined in a command
    class. They probably need further refactoring and this gives us a
    clearer base from which to do that.
    
    Conflicts:
    	app/controllers/admin/editions_controller.rb

---
+++ lib/edition_progressor.rb
@@ -0,0 +1,73 @@
+class EditionProgressor
+  attr_accessor :edition, :actor, :activity, :statsd, :status_message
+
+  # existing_edition: The existing edition to be duplicated
+  # actor:            The WorkflowActor (usually a user) to perform the action
+  def initialize(edition, actor, statsd)
+    self.edition = edition
+    self.actor   = actor
+    self.statsd  = statsd
+  end
+
+  def progress(activity)
+    if invalid_fact_check_email_addresses?(activity)
+      self.status_message = fact_check_error_message(activity)
+      return false
+    elsif actor.progress(edition, activity.dup)
+      collect_edition_status_stats(activity[:request_type])
+      self.status_message = success_message(activity[:request_type])
+      return true
+    else
+      self.status_message = failure_message(activity[:request_type])
+      return false
+    end
+  end
+
+  protected
+    def invalid_fact_check_email_addresses?(activity)
+      fact_check_request?(activity[:request_type]) and invalid_email_addresses?(activity[:email_addresses])
+    end
+
+    def fact_check_request?(request_type)
+      request_type == "send_fact_check"
+    end
+
+    def invalid_email_addresses?(addresses)
+      addresses.split(",").any? do |address|
+        !address.include?("@")
+      end
+    end
+
+    def collect_edition_status_stats(intended_status)
+      statsd.decrement("publisher.edition.#{edition.state}")
+      statsd.increment("publisher.edition.#{intended_status}")
+    end
+
+    def fact_check_error_message(activity)
+      "Couldn't #{activity.to_s.humanize.downcase} for " +
+                "#{description(edition).downcase}. The email addresses " +
+                "you entered appear to be invalid."
+    end
+
+    # TODO: This could probably live in the i18n layer?
+    def failure_message(activity)
+      case activity
+      when 'skip_fact_check' then "Could not skip fact check for this publication."
+      when 'start_work' then "Couldn't start work on #{description(edition).downcase}"
+      else "Couldn't #{activity.to_s.humanize.downcase} for #{description(edition).downcase}"
+      end
+    end
+
+    # TODO: This could probably live in the i18n layer?
+    def success_message(activity)
+      case activity
+      when 'start_work' then "Work started on #{description(edition)}"
+      when 'skip_fact_check' then "The fact check has been skipped for this publication."
+      else "#{description(edition)} updated"
+      end
+    end
+
+    def description(r)
+      r.format.underscore.humanize
+    end
+end
