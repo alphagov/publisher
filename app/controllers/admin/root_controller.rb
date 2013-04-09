95d6caaabda9acc016ac169a738753be1abb9a20
James Stewart <jys@ketlai.co.uk>
2013-04-09 21:51:24 +0100

    Tidy up broken out RootController#index methods
    Broken up a little more, and no longer setting instance variables
    all over the place. I think this is probably as far as we want to
    go without opening up the question of breaking this out into filter
    classes and/or revisiting how the AdminRootPresenter works.

--- app/controllers/admin/root_controller.rb
+++ app/controllers/admin/root_controller.rb
@@ -8,22 +8,18 @@ class Admin::RootController < Admin::BaseController
   STATE_NAME_LISTS = {"draft" => "drafts", "fact_check" => "out_for_fact_check"}
 
   def index
-    @user_filter = params[:user_filter] || session[:user_filter]
-    @list = params[:list].blank? ? 'lined_up' : params[:list]
-
-    session[:user_filter] = @user_filter
-
-    if params[:with] && params[:title_filter]
-      raise "Cannot specify both 'with' and 'title_filter' parameters."
-    end
-    if params[:with] && params[:page]
-      raise "Cannot specify both 'with' and 'page' parameters."
-    end
+    user_filter           = params[:user_filter] || session[:user_filter]
+    session[:user_filter] = user_filter
 
     if params[:with]
-      @presenter = build_with_focus
+      raise "Cannot specify both 'with' and 'title_filter' parameters." if params[:title_filter]
+      raise "Cannot specify both 'with' and 'page' parameters." if params[:page]
+
+      @list = list_parameter_from_state(edition.state)
+      @presenter, @user_filter = build_with_focus(user_filter)
     else
-      @presenter = build_without_focus(params[:page])
+      @list = params[:list].blank? ? 'lined_up' : params[:list]
+      @presenter, @user_filter = build_without_focus(user_filter, params[:page])
     end
 
     # Looking at another class, but the whole approach taken by this method and its
@@ -43,44 +39,44 @@ private
     STATE_NAME_LISTS[state] || state
   end
 
-  def build_without_focus(current_page = nil)
-    @user_filter, user = process_user_filter(@user_filter)
+  def build_without_focus(user_filter, current_page = nil)
+    user_filter, user = process_user_filter(user_filter)
     editions = Edition.order_by([sort_column, sort_direction])
     editions = editions.page(current_page).per(ITEMS_PER_PAGE)
-    AdminRootPresenter.new(editions, user)
+    return AdminRootPresenter.new(editions, user), user_filter
   end
 
-  def build_with_focus
-    begin
-      edition = Edition.find(params[:with])
-    rescue Mongoid::Errors::DocumentNotFound, BSON::InvalidObjectId
-      raise ActionController::RoutingError.new('Not Found')
-    end
+  def edition_of_interest
+    Edition.find(params[:with])
+  rescue Mongoid::Errors::DocumentNotFound, BSON::InvalidObjectId
+    raise ActionController::RoutingError.new('Not Found')
+  end
 
-    @list = list_parameter_from_state edition.state
+  def build_with_focus(user_filter)
+    edition = edition_of_interest
+
     if edition.assigned_to.nil? or edition.assigned_to.uid != @user_filter
-      @user_filter = "all"
+      user_filter = "all"
     end
 
-    @user_filter, user = process_user_filter(@user_filter)
+    user_filter, user = process_user_filter(user_filter)
 
     editions = Edition.order_by([sort_column, sort_direction])
 
     item_index = editions.send(edition.state).to_a.index { |e| e.id == edition.id }
     current_page = (item_index / ITEMS_PER_PAGE) + 1
-
     editions = editions.page(current_page).per(ITEMS_PER_PAGE)
-    AdminRootPresenter.new(editions, user)
+    return AdminRootPresenter.new(editions, user), user_filter
   end
 
   def process_user_filter(user_filter = nil)
     if user_filter.blank?
       user_filter = current_user.uid
       user = current_user
-    elsif %w[ all nobody ].include?(@user_filter)
-      user = @user_filter.to_sym
+    elsif %w[ all nobody ].include?(user_filter)
+      user = user_filter.to_sym
     else
-      user = User.where(uid: @user_filter).first
+      user = User.where(uid: user_filter).first
     end
 
     return user_filter, user
