ccb4ce99a4ff82053410506efab0e344e16a1366
James Stewart <jys@ketlai.co.uk>
2013-04-09 19:28:44 +0100

    Remove duplication of NoisyWorkflow invocations
    We want these two methods to be as similar as possible, so sharing
    more code without duplication is a good thing.

--- app/models/enhancements/user.rb
+++ app/models/enhancements/user.rb
@@ -6,13 +6,17 @@ class User
 
   def record_action(edition, type, options = {})
     action = record_action_without_noise(edition, type, options)
-    NoisyWorkflow.make_noise(action).deliver
-    NoisyWorkflow.request_fact_check(action).deliver if type.to_s == "send_fact_check"
+    make_record_action_noises(action, type)
   end
 
   def record_action_without_validation(edition, type, options={})
     action = record_action_without_validation_without_noise(edition, type, options)
-    NoisyWorkflow.make_noise(action).deliver
-    NoisyWorkflow.request_fact_check(action).deliver if type.to_s == "send_fact_check"
+    make_record_action_noises(action, type)
   end
+
+  private
+    def make_record_action_noises(action, type)
+      NoisyWorkflow.make_noise(action).deliver
+      NoisyWorkflow.request_fact_check(action).deliver if type.to_s == "send_fact_check"
+    end
 end
