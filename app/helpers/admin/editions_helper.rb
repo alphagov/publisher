b34885a2d67cfe9c201b57d583973e5c3a9a9ee2
James Stewart <jys@ketlai.co.uk>
2013-04-25 08:46:28 +0100

    Add helper to set up a few form defaults
    It actually feels like this should live in the controller or the model
    but I've not worked out the best/least intrusive way to add it there
    and this change at least gets the basics working.
    
    There's one failing test in AddingPartsToGuidesTest that needs more
    investigation.

--- app/helpers/admin/editions_helper.rb
+++ app/helpers/admin/editions_helper.rb
@@ -2,4 +2,11 @@ module Admin::EditionsHelper
   def format_content_diff( body )
     ContentDiffFormatter.new(body).to_html
   end
+
+  # All guides should have at least one part
+  # Those parts should be in the correct order
+  def tidy_up_parts_before_editing(resource)
+    resource.parts.build if resource.parts.empty?
+    resource.parts.replace(resource.parts.sort_by(&:order))
+  end
 end
