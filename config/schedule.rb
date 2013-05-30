f6c6b1a52d16470adc293549154b2a49fd086059
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-05-30 15:38:55 +0100

    Make whenever rake tasks run under govuk_setenv
    This will fix the local_transactions import

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,6 @@
 # This file is overwritten on deploy
 set :output, {:error => 'log/cron.error.log', :standard => 'log/cron.log'}
+job_type :rake, 'cd :path && /usr/local/bin/govuk_setenv publisher bundle exec rake :task'
 job_type :run_script,  'cd :path && RAILS_ENV=:environment /usr/local/bin/govuk_setenv publisher script/:task'
 
 every 1.day, :at => '5am' do
