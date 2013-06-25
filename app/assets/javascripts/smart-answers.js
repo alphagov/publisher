046751b067642721b92261115c618333d13d1c8a
Jordan Hatch <jordan@jordanh.net>
2013-06-25 17:55:35 +0100

    Nested options can be added to each node.
    The select field automatically updates to list the
    subsequent nodes available for each option.

--- app/assets/javascripts/smart-answers.js
+++ app/assets/javascripts/smart-answers.js
@@ -8,13 +8,21 @@
   var smartAnswerBuilder = {
     init: function() {
       smartAnswerBuilder.setEventHandlers();
+      smartAnswerBuilder.reloadAllNextNodeLists();
+      smartAnswerBuilder.setupInitialQuestion();
     },
     lastNodeKind: null,
     setEventHandlers: function() {
       smartAnswerBuilder.addQuestionButton().click(smartAnswerBuilder.addQuestion);
       smartAnswerBuilder.addOutcomeButton().click(smartAnswerBuilder.addOutcome);
 
-      $(document).on('nested:fieldAdded', smartAnswerBuilder.initNode);
+      $('.nodes').on("change", ".node input.node-title", smartAnswerBuilder.reloadAllNextNodeLists);
+      $('.nodes').on("change", ".node select.next-node-list", smartAnswerBuilder.updateNextNode);
+
+      $(document).on('nested:fieldAdded:nodes', smartAnswerBuilder.initNode);
+      $(document).on('nested:fieldRemoved:nodes', smartAnswerBuilder.reloadAllNextNodeLists);
+
+      $(document).on('nested:fieldAdded:options', smartAnswerBuilder.initOption);
     },
     container: $('.builder-container'),
     addQuestion: function() {
@@ -29,9 +37,14 @@
     addOutcomeButton: function() {
       return smartAnswerBuilder.container.find('a.add-outcome');
     },
+    setupInitialQuestion: function() {
+      if (smartAnswerBuilder.container.find('.node').length < 1) {
+        smartAnswerBuilder.addQuestionButton().click();
+      }
+    },
     initNode: function(e) {
       var kind = smartAnswerBuilder.lastNodeKind;
-      var node = e.field.find('.node');
+      var node = e.field;
       smartAnswerBuilder.lastNodeKind = null;
 
       var indexOfKind = smartAnswerBuilder.container.find(".node."+ kind).length + 1;
@@ -44,7 +57,11 @@
 
       if (kind != "question") {
         node.find('.options').remove();
+      } else {
+        node.find('.options .add_nested_fields').click();
       }
+
+      smartAnswerBuilder.reloadAllNextNodeLists();
 
       function nodeId(kind) {
         return kind + "-" + indexOfKind;
@@ -54,7 +71,45 @@
         var capitalizedKind = kind.charAt(0).toUpperCase() + kind.slice(1);
         return capitalizedKind + " " + indexOfKind;
       }
-    }
+    },
+    initOption: function(e) {
+      var node = $(e.field).parents(".node").first();
+      smartAnswerBuilder.reloadNextNodeList(node);
+    },
+    reloadAllNextNodeLists: function() {
+      $.each( smartAnswerBuilder.container.find('.node'), function(i,node) {
+        smartAnswerBuilder.reloadNextNodeList( $(node) );
+      });
+    },
+    reloadNextNodeList: function(node) {
+      var nextNodeField = node.find('.next-node-list');
+      nextNodeField.find('option:not(.default)').remove();
+
+      $.each( smartAnswerBuilder.optionsForNode(node), function(i, x) {
+        var optionLabel = x.name;
+        if (x.label != '') {
+          optionLabel = optionLabel + " (" + x.label + ")";
+        }
+        $('<option></option>').text(optionLabel).attr('value', x.id).appendTo(nextNodeField.find('optgroup[class="'+ x.kind +'-list"]'));
+      });
+
+      node.find('.option').each( function(i, option){
+        var nextNodeId = $(option).find('.next-node-id').first().val();
+        $(option).find('.next-node-list').first().val(nextNodeId);
+      });
+    },
+    optionsForNode: function(node) {
+      var nextNodes = node.nextAll(':visible');
+
+      return $.map( nextNodes, function(nodeContainer, i) {
+        node = $(nodeContainer);
+        return { id: node.attr('id'), label: node.find('.node-title').val(), kind: node.find('.node-kind').val(), name: node.find('.node-label').text() };
+      });
+    },
+    updateNextNode: function() {
+      var nextNode = $(this).val();
+      $(this).parent('.option').find('.next-node-id').val(nextNode);
+    },
   }
   root.Publisher.smartAnswerBuilder = smartAnswerBuilder;
 }).call(this);
