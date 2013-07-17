a3fc32516001e33009a5756236618c6ed5946826
pezholio <pezholio@gmail.com>
2013-07-17 15:03:46 +0100

    Load Auth details from ENV

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -1,8 +1,8 @@
 GDS::SSO.config do |config|
   config.user_model   = "User"
-  config.oauth_id     = 'abcdefghjasndjkasndpublisher'
-  config.oauth_secret = 'secret'
+  config.oauth_id     = ENV['PUBLISHER_OAUTH_ID']
+  config.oauth_secret = ENV['PUBLISHER_OAUTH_SECRET']
   config.oauth_root_url = Plek.current.find("signon")
-  config.basic_auth_user = 'api'
-  config.basic_auth_password = 'defined_on_rollout_not'
+  config.basic_auth_user = ENV['PANOPTICON_USER']
+  config.basic_auth_password = ENV['PANOPTICON_PASSWORD']
 end
--- config/initializers/panopticon_api_credentials.rb
+++ config/initializers/panopticon_api_credentials.rb
@@ -1,2 +1,7 @@
 # This file gets overwritten on deploy
-PANOPTICON_API_CREDENTIALS = {}
+PANOPTICON_API_CREDENTIALS = {
+  :basic_auth => {
+    :user     => ENV['PANOPTICON_USER'],
+    :password => ENV['PANOPTICON_PASSWORD']
+  }
+}
