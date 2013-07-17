a8be6bc728997732263bd9a0ab3cf9212d72e2d2
pezholio <pezholio@gmail.com>
2013-07-17 13:44:33 +0100

    Load Oauth and Basic Auth credentials from ENV

--- config/initializers/panopticon_api_credentials.rb
+++ config/initializers/panopticon_api_credentials.rb
@@ -1,2 +1,7 @@
 # This file gets overwritten on deploy
-PANOPTICON_API_CREDENTIALS = {}
+PANOPTICON_API_CREDENTIALS = {
+  :basic_auth => {
+    :user     => ENV['PANOPTICAN_USER'],
+    :password => ENV['PANOPTICAN_PASSWORD']
+  }
+}
