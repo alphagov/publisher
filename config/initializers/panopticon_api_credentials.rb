eeeda18554340751b1b558ddda304abd8b754c7b
pezholio <pezholio@gmail.com>
2013-07-17 15:23:48 +0100

    Panopticon, not Panotican!

--- config/initializers/panopticon_api_credentials.rb
+++ config/initializers/panopticon_api_credentials.rb
@@ -1,7 +1,7 @@
 # This file gets overwritten on deploy
 PANOPTICON_API_CREDENTIALS = {
   :basic_auth => {
-    :user     => ENV['PANOPTICAN_USER'],
-    :password => ENV['PANOPTICAN_PASSWORD']
+    :user     => ENV['PANOPTICON_USER'],
+    :password => ENV['PANOPTICON_PASSWORD']
   }
 }
