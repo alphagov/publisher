alphagov/publisher

commit e675941157734d09d4fc25db1fef5e8a4c2bec37

Add sensible defaults if ENV var isn't present

config/initializers/panopticon_api_credentials.rb
@@ -1,7 +1,7 @@
 # This file gets overwritten on deploy
 PANOPTICON_API_CREDENTIALS = {
   :basic_auth => {
-    :user     => ENV['PANOPTICON_USER'],
-    :password => ENV['PANOPTICON_PASSWORD']
+    :user     => ENV['PANOPTICON_USER'] || "api",
+    :password => ENV['PANOPTICON_PASSWORD'] || "defined_on_rollout_not"
   }
 }

config/initializers/gds-sso.rb
@@ -1,8 +1,8 @@
 GDS::SSO.config do |config|
   config.user_model   = "User"
-  config.oauth_id     = ENV['PUBLISHER_OAUTH_ID']
-  config.oauth_secret = ENV['PUBLISHER_OAUTH_SECRET']
+  config.oauth_id     = ENV['PUBLISHER_OAUTH_ID'] || "abcdefghjasndjkasndpublisher"
+  config.oauth_secret = ENV['PUBLISHER_OAUTH_SECRET'] || "secret"
   config.oauth_root_url = Plek.current.find("signon")
-  config.basic_auth_user = ENV['PANOPTICON_USER']
-  config.basic_auth_password = ENV['PANOPTICON_PASSWORD']
+  config.basic_auth_user = ENV['PANOPTICON_USER'] || "api"
+  config.basic_auth_password = ENV['PANOPTICON_PASSWORD'] || "defined_on_rollout_not"
 end

commit 9cdc2104e2b86f303c62c5d386dbace8747b2ea6

Fix broken tests
Broken by: e675941157734d09d4fc25db1fef5e8a4c2bec37

The tests weren't expecting the credentials to be included in the panopticon
URL. The simplest fix is simply not to use credentials in tests.

config/initializers/panopticon_api_credentials.rb
@@ -1,7 +1,11 @@
 # This file gets overwritten on deploy
-PANOPTICON_API_CREDENTIALS = {
-  :basic_auth => {
-    :user     => ENV['PANOPTICON_USER'] || "api",
-    :password => ENV['PANOPTICON_PASSWORD'] || "defined_on_rollout_not"
+if Rails.env.test?
+  PANOPTICON_API_CREDENTIALS = {}
+else
+  PANOPTICON_API_CREDENTIALS = {
+    :basic_auth => {
+      :user     => ENV['PANOPTICON_USER'] || "api",
+      :password => ENV['PANOPTICON_PASSWORD'] || "defined_on_rollout_not"
+    }
   }
-}
+end