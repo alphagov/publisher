21d30c44011795919cb913c9937e015505bfbe63
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-08-28 08:23:58 +0000

    Data migration to convert help content to new format.
    The help pages have been started as quick answers.  Now that we have a
    dedicated format for them, convert them to use it.

---
+++ db/migrate/20130827122554_migrate_help_pages.rb
@@ -0,0 +1,50 @@
+class MigrateHelpPages < Mongoid::Migration
+  SLUGS = %w(
+    about-govuk
+    cookies
+    browsers
+    privacy-policy
+    accessibility
+    terms-and-conditions
+  )
+
+  def self.up
+    SLUGS.each do |slug|
+      puts "Converting #{slug} to HelpPageEdition"
+      a = Artefact.where(:slug => slug).first
+      unless a
+        puts "  artefact not found"
+        next
+      end
+      eds = Edition.where(:slug => slug)
+      unless eds.count == 1
+        puts "  multiple editions found, skipping..."
+        next
+      end
+      ed = eds.first
+      unless a.state == 'draft'
+        puts "  artefact isn't draft, skipping..."
+        next
+      end
+      if ed.state == "published"
+        puts "  edition is published, skipping..."
+        next
+      end
+
+      a.kind = "help_page"
+      if slug == "terms-and-conditions"
+        a.slug = "help/terms-conditions"
+      else
+        a.slug = "help/#{a.slug}"
+      end
+      a.save!
+
+      ed.slug = a.slug
+      ed._type = "HelpPageEdition"
+      ed.save!
+    end
+  end
+
+  def self.down
+  end
+end
