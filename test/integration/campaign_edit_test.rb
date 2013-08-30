22dc21b80edfea31615632d0045373640be2fc15
Jordan Hatch <jordan@jordanh.net>
2013-08-30 10:35:56 +0100

    Add upload field for single campaign image

--- test/integration/campaign_edit_test.rb
+++ test/integration/campaign_edit_test.rb
@@ -59,4 +59,55 @@ class CampaignEditTest < JavascriptIntegrationTest
 
     assert page.has_field?("Body", :with => "Foo")
   end
+
+  should "manage images for a CampaignEdition" do
+    c = FactoryGirl.create(:campaign_edition,
+                             :panopticon_id => @artefact.id,
+                             :state => 'draft',
+                             :title => "Max Campaign",
+                             :body => "Foo")
+
+    file_one = File.open(Rails.root.join("test","fixtures","uploads","campaign.jpg"))
+    file_two = File.open(Rails.root.join("test","fixtures","uploads","campaign_two.jpg"))
+
+    asset_one = OpenStruct.new(:id => 'http://asset-manager.dev.gov.uk/assets/an_image_id', :file_url => 'http://path/to/campaign.jpg')
+    asset_two = OpenStruct.new(:id => 'http://asset-manager.dev.gov.uk/assets/another_image_id', :file_url => 'http://path/to/campaign_two.jpg')
+
+    GdsApi::AssetManager.any_instance.stubs(:create_asset).returns(asset_one)
+    GdsApi::AssetManager.any_instance.stubs(:asset).with("an_image_id").returns(asset_one)
+
+    visit "/admin/editions/#{c.to_param}"
+
+    assert page.has_field?("Upload a new campaign image", :type => "file")
+    attach_file("Upload a new campaign image", file_one.path)
+    click_on "Save"
+
+    within(:css, ".uploaded-image") do
+      assert page.has_selector?("a[href$='campaign.jpg']")
+    end
+
+    # ensure file is not removed on save
+    click_on "Save"
+
+    within(:css, ".uploaded-image") do
+      assert page.has_selector?("a[href$='campaign.jpg']")
+    end
+
+    # replace file
+    GdsApi::AssetManager.any_instance.stubs(:create_asset).returns(asset_two)
+    GdsApi::AssetManager.any_instance.stubs(:asset).with("another_image_id").returns(asset_two)
+
+    attach_file("Upload a new campaign image", file_two.path)
+    click_on "Save"
+
+    within(:css, ".uploaded-image") do
+      assert page.has_selector?("a[href$='campaign_two.jpg']")
+    end
+
+    # remove file
+    check "Remove campaign image?"
+    click_on "Save"
+
+    refute page.has_selector?(".uploaded-image")
+  end
 end
