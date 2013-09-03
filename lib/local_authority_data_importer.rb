91cb4b3490784b816fe0c45fcb4ecaf433f7ad99
Tom Booth <tombooth@gmail.com>
2013-09-03 16:53:43 +0100

    [#51816839] Add shell out to nagios singal script
    In order to provide a passive nagios check to signal the success of the
    daily import of data I have added exception catching and signalling to
    nagios through the local_transactions:fetch task. If there was an error
    the message or class name will be passed through as the description.
    
    The local_authority_import_check script is provided by the puppet
    configuration such that all nagios configuration can be kept in one
    place. The script takes a code (0 - success, 1 - error) and a
    description of the status.
    
    If an instance of publisher fails to lock then it will signal that it
    was successful. That is to keep all the checks in sync and to remove
    lingering errors from previous runs.
    
    I have wrapped up the http fetching such that it throws an error for any
    status but 200 such that we know whether the request failed through
    nagios.

--- lib/local_authority_data_importer.rb
+++ lib/local_authority_data_importer.rb
@@ -7,17 +7,34 @@ class LocalAuthorityDataImporter
 
   def self.update_all
     redis.lock("publisher:#{Rails.env}:local_authority_data_importer_lock", :life => 2.hours) do
-      LocalServiceImporter.update
-      LocalInteractionImporter.update
-      LocalContactImporter.update
+      begin
+        LocalServiceImporter.update
+        LocalInteractionImporter.update
+        LocalContactImporter.update
+        nagios_check(true, "Import succeeded")
+      rescue Exception => e
+        nagios_check(false, e.to_s)
+
+        # re-raise the exception so that it ends up in the logs
+        # and we allow through SyntaxError, Interrupt and SignalException
+        raise e
+      end
     end
   rescue Redis::Lock::LockNotAcquired => e
     Rails.logger.debug("Failed to get lock for local directgov importing (#{e.message}). Another process probably got there first.")
+
+    # Flag nagios that this servers instance succeeded to stop lingering failures
+    nagios_check(true, "Importer not run on this instance")
   end
 
   def self.redis
     redis_config = YAML.load_file(Rails.root.join("config", "redis.yml"))
     Redis.new(redis_config.symbolize_keys)
+  end
+
+  def self.nagios_check(success, message)
+    code = if success then 0 else 1 end
+    `local_authority_import_check #{code} "#{message}"`
   end
 
   def self.update
@@ -34,7 +51,13 @@ class LocalAuthorityDataImporter
     fh.set_encoding('ascii-8bit')
 
     uri = URI.parse(url)
-    fh.write Net::HTTP.get(uri)
+    response = Net::HTTP.get_response(uri)
+
+    if response.code != "200"
+      raise "HTTP Fetch failed [#{response.code}]: #{url}"
+    end
+
+    fh.write response.body
 
     fh.rewind
     fh.set_encoding('windows-1252', 'UTF-8')
