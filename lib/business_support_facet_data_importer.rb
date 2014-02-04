082f7a8c596b2c6c675018e2bc645dfd83d76020
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2014-02-04 12:15:57 +0000

    De-duplicate BusinessSupportEdtion processing
    Added accounting for inelligible schemes (archived artefacts or no published siblings).
    De duplicated merge with Imminence data.

--- lib/business_support_facet_data_importer.rb
+++ lib/business_support_facet_data_importer.rb
@@ -17,6 +17,8 @@ class BusinessSupportFacetDataImporter
 
     saved = []
     unsaved = []
+    invalid = []
+
     # Only modify unarchived editions
     editions = BusinessSupportEdition.excludes(state: 'archived')
 
@@ -24,28 +26,34 @@ class BusinessSupportFacetDataImporter
 
       # Ignore anything that doesn't have a published edition in the series
       unless bse.artefact.archived? or bse.published_edition.nil?
-
         # Find the corresponding Imminence scheme data
         imminence_scheme = imminence_results.find { |bs|
           bs['business_support_identifier'] == bse.business_support_identifier
         }
 
-        # Apply facet values from Imminence data to this Edition
-        if imminence_scheme
-          import_attrs.each do |attr|
-            attrval = imminence_scheme[attr]
-            bse.send("#{attr}=", attrval) if attrval
+        scheme_details = "slug: #{bse.slug}, id: #{bse.business_support_identifier}, state: #{bse.state}"
+
+        unless (saved + unsaved).include?(scheme_details)
+          # Apply facet values from Imminence data to this Edition
+          if imminence_scheme
+            import_attrs.each do |attr|
+              attrval = imminence_scheme[attr]
+              bse.send("#{attr}=", attrval) if attrval
+            end
+            saved << scheme_details if bse.save!(validate: false)
+          else
+            unsaved << scheme_details
           end
-          saved << bse.slug if bse.save!(validate: false)
-        else
-          unsaved << "(#{bse.business_support_identifier}) #{bse.slug}"
         end
+      else
+        # Having an archived artefact or no published editions.
+        invalid << bse.slug
       end
     end
 
     puts "Processed #{editions.size} BusinessSupportEditions."
-    puts "#{saved.size} successfully updated. #{unsaved.size} didn't match Imminence data."
-    puts "Updated: #{saved.join(', ')}"
-    puts "Skipped: #{unsaved.join(', ')}"
+    puts "#{saved.size} successfully updated. #{unsaved.size} didn't match Imminence data. #{invalid.size} were inelligible for update."
+    puts "Updated: #{saved.sort.join(', ')}"
+    puts "Skipped: #{unsaved.sort.join(', ')}"
   end
 end
