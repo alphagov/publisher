5be8c1d9fb079ff918162e1513bb2658462332a7
Stuart Gale <bishboria@gmail.com>
2014-02-27 15:38:22 +0000

    Migration to remove `lined_up` state and set to `draft`

---
+++ db/migrate/20140225163000_make_draft_the_default_state.rb
@@ -0,0 +1,12 @@
+class MakeDraftTheDefaultState < Mongoid::Migration
+  def self.up
+    Edition.where(:state => "lined_up").each do |edition|
+      edition.state = "draft"
+      edition.save!
+    end
+  end
+
+  def self.down
+    # No down, this is destructive
+  end
+end
