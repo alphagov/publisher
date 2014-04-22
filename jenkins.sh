203f3a2b2479bd5e852b2c3332fcb08fb6845ba6
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-04-22 14:29:11 +0100

    Remove unneeded env variables.
    * `DISPLAY` isn't needed since we switched from capybara-webkit to phantomjs
    * Nothing in the app should depend on `FACTER_*` variables.

--- jenkins.sh
+++ jenkins.sh
@@ -2,9 +2,7 @@
 
 set -e
 
-export FACTER_govuk_platform=test
 export RAILS_ENV=test
-export DISPLAY=":99"
 
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
 bundle exec rake stats
