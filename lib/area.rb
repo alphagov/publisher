36a5dc6cda76e9db2239e76082325b20c47a5273
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2014-06-20 12:17:45 +0000

    Retrieve Areas from Imminence.
    Adds a convenience class for retrieving and memoising Imminence area data.

---
+++ lib/area.rb
@@ -0,0 +1,32 @@
+require 'ostruct'
+require 'gds_api/helpers'
+
+class Area < OpenStruct
+  extend GdsApi::Helpers
+
+  AREA_TYPES = ['EUR', 'CTY', 'DIS', 'LBO']
+
+  def self.all
+    areas
+  end
+
+  def self.areas_for_edition(edition)
+    areas.select { |a| edition.areas.include?(a.id.to_s) }
+  end
+
+  private
+
+    def self.areas
+      @areas ||= all_areas
+    end
+
+    def self.all_areas
+      areas = []
+      AREA_TYPES.each do |type|
+        areas << imminence_api.areas_for_type(type)["results"].map do |area_hash|
+          self.new(area_hash)
+        end
+      end
+      areas.flatten
+    end
+end
