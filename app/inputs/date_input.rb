d01ba8968f141391abf7d6817ae94a87dae53497
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2014-06-30 06:09:11 +0000

    Convert date selects to text fields
    www.agileplannerapp.com/boards/173808/cards/4294
    
    as per GOV.UK elements suggestion, the date fields
    should be text inputs:
    
    http://govuk-elements.herokuapp.com/examples/forms

---
+++ app/inputs/date_input.rb
@@ -0,0 +1,61 @@
+class DateInput
+  include Formtastic::Inputs::Base
+
+  def to_html
+    field_title << date_fields
+  end
+
+  def date_fields
+    day, month, year = extract_date_field_prefill_values
+
+    day_field_html(day) <<
+    month_field_html(month) <<
+    year_field_html(year)
+  end
+
+private
+
+  def day_field_html(value)
+    template.content_tag(:div, class: 'form-group-day') do
+      template.label_tag(method_s + '_day', 'Day') +
+      template.text_field_tag(method_s + '_day', value,
+        { placeholder: 'DD', maxlength: 2, name: format_tag_name(builder, '3i'), class: 'form-control', disabled: options[:disabled] })
+    end
+  end
+
+  def month_field_html(value)
+    template.content_tag(:div, class: 'form-group-month') do
+      template.label_tag(method_s + '_month', "Month") +
+      template.text_field_tag(method_s + '_month', value,
+        { placeholder: 'MM', maxlength: 2, name: format_tag_name(builder, '2i'), class: 'form-control', disabled: options[:disabled] })
+    end
+  end
+
+  def year_field_html(value)
+    template.content_tag(:div, class: 'form-group-year') do
+      template.label_tag(method_s + '_year', "Year") +
+      template.text_field_tag(method_s + '_year', value,
+        { placeholder: 'YYYY', maxlength: 4, name: format_tag_name(builder, '1i'), class: 'form-control', disabled: options[:disabled] })
+    end
+  end
+
+  def format_tag_name(builder, suffix=nil)
+    "#{@object_name}[#{sanitized_method_name}#{"(#{suffix})" if suffix}]"
+  end
+
+  def method_s
+    method.to_s
+  end
+
+  def extract_date_field_prefill_values
+    prefill_value = builder.object.send(method)
+    [prefill_value.day.to_s.rjust(2, '0'),
+      prefill_value.month.to_s.rjust(2, '0'),
+      prefill_value.year] if prefill_value
+  end
+
+  def field_title
+    template.content_tag(:legend, label_text, class: 'date-input-legend')
+  end
+
+end
