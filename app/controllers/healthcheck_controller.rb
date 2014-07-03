888e4a2094f57ec0002f989ce085b1a9ceec5d72
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2014-07-03 15:46:29 +0000

    Add a check for the scheduled publishing queue.
    From time to time, an item will be scheduled for publishing, but the
    code to add it to the Sidekiq queue will fail. We should have monitoring
    for when this happens, so we can give it a nudge.

--- app/controllers/healthcheck_controller.rb
+++ app/controllers/healthcheck_controller.rb
@@ -5,7 +5,25 @@ class HealthcheckController < ActionController::Base
   respond_to :json
 
   def check
-    health_status = {"status" => "ok"}
+    health_status = {"checks" => {}}
+    health_status["checks"]["schedule_queue"] = schedule_queue_result
+    health_status["status"] = health_status["checks"]["schedule_queue"]["status"]
     respond_with health_status
+  end
+
+private
+
+  def schedule_queue_result
+    scheduled_editions = Edition.scheduled_for_publishing.count
+    queue_size = Sidekiq::Stats.new.scheduled_size
+
+    if scheduled_editions == queue_size
+      {"status" => "ok"}
+    else
+      {
+        "status" => "warning",
+        "message" => "#{scheduled_editions} scheduled edition(s); #{queue_size} item(s) queued"
+      }
+    end
   end
 end
--- test/functional/healthcheck_controller_test.rb
+++ test/functional/healthcheck_controller_test.rb
@@ -6,13 +6,64 @@ class HealthcheckControllerTest < ActionController::TestCase
     JSON.parse(response.body)
   end
 
-  test "it returns a 200 response" do
+  should "return a 200 response" do
     get :check, format: :json
     assert_response :success
   end
 
-  test "it returns an overall status field" do
+  should "return an overall status field" do
     get :check, format: :json
-    assert json.has_key? "status"
+    assert_includes json, "status"
+  end
+
+  should "have a field for the checks" do
+    get :check, format: :json
+    assert_includes json, "checks"
+    assert json["checks"].is_a? Hash
+  end
+
+  context "scheduled count matches queue length" do
+    setup do
+      Edition.stubs(:scheduled_for_publishing).returns(stub(count: 0))
+      Sidekiq::Stats.any_instance.stubs(:scheduled_size).returns(0)
+    end
+
+    should "report the check is ok" do
+      get :check, format: :json
+      assert_includes json["checks"], "schedule_queue"
+      assert_equal "ok", json["checks"]["schedule_queue"]["status"]
+    end
+
+    should "report the overall status as ok" do
+      get :check, format: :json
+      assert_equal "ok", json["status"]
+    end
+  end
+
+  context "scheduled count does not match queue length" do
+    setup do
+      Edition.stubs(:scheduled_for_publishing).returns(stub(count: 5))
+      Sidekiq::Stats.any_instance.stubs(:scheduled_size).returns(3)
+    end
+
+    should "report the check as a warning" do
+      get :check, format: :json
+      assert_includes json["checks"], "schedule_queue"
+      assert_equal "warning", json["checks"]["schedule_queue"]["status"]
+    end
+
+    should "include a status message" do
+      get :check, format: :json
+      assert_includes json["checks"], "schedule_queue"
+      assert_equal(
+        "5 scheduled edition(s); 3 item(s) queued",
+        json["checks"]["schedule_queue"]["message"]
+      )
+    end
+
+    should "report the overall status as a warning" do
+      get :check, format: :json
+      assert_equal "warning", json["status"]
+    end
   end
 end
