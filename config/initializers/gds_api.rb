febe8711fce8eb88e08c77d6326c32a355603dd0
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-07-22 09:38:34 +0100

    Clarify separation of bearer tokens in initializer.
    This file is overwritten on deploy, so this change won't affect
    production etc, but it's good to make it clear that the panopticon and
    asset-manager clients use different tokens.

--- config/initializers/gds_api.rb
+++ config/initializers/gds_api.rb
@@ -9,9 +9,10 @@ GdsApi::Base.logger = Logger.new(Rails.root.join("log/#{Rails.env}.api_client.lo
 # This can be loaded from an environment variable, or this file can
 # be overwritten with the correct details on deploy.
 # See README.md for details of how to generate a bearer token.
-options = {
-  bearer_token: ENV['PUBLISHER_API_CLIENT_BEARER_TOKEN'] || 'set_at_deploy_time'
+PANOPTICON_API_CREDENTIALS = {
+  bearer_token: ENV['PUBLISHER_PANOPTICON_CLIENT_BEARER_TOKEN'] || 'set_at_deploy_time'
 }
 
-Attachable.asset_api_client = GdsApi::AssetManager.new(Plek.current.find('asset-manager'), options)
-PANOPTICON_API_CREDENTIALS = options
+Attachable.asset_api_client = GdsApi::AssetManager.new(Plek.current.find('asset-manager'), {
+  bearer_token: ENV['PUBLISHER_ASSET_MANAGER_CLIENT_BEARER_TOKEN'] || 'also_set_at_deploy_time',
+})
