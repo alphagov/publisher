7da2974e05bee5ea897e54892aa3b7bff7b3cb32
Aaron Keogh <aaron.keogh@digital.cabinet-office.gov.uk>
2014-07-30 15:43:37 +0000

    Migration for updating become-a-driving-instructor owning app and slug
    become-a-driving-instructor is currently a smart-answer. It is being replaced with a simple smart-answer. This migration changes the owning app, kind and rendering app before changing the slug.

---
+++ db/migrate/20140730074552_change_owning_app_and_slug_for_driving_instructor.rb
@@ -0,0 +1,58 @@
+class ChangeOwningAppAndSlugForDrivingInstructor < Mongoid::Migration
+  # select the slug to be changed
+  SSA_SLUG = 'become-driving-instructor'
+  SA_SLUG = 'become-a-driving-instructor'
+
+
+  def self.up
+    changeling = Artefact.find_by_slug(SA_SLUG)
+
+    if changeling
+      # change the kind and owning app
+      puts "Updating owning app, kind and rendering app"
+      changeling.update_attributes(kind: "simple_smart_answer", owning_app: "publisher", rendering_app: "frontend")
+
+      # update the slug on all editions
+      puts "Updating the slug on all editions"
+      Edition.where(:slug => SSA_SLUG).each do |e| 
+        e.slug = SA_SLUG
+        e.panopticon_id = changeling._id
+        e.save(:validate => false)
+      end
+
+      # re-register with panopticon to recreate in search
+      puts "Re-registering with panopticon to recreate in search"
+      ed = Edition.published.where(:slug => SA_SLUG).first
+      ed.register_with_panopticon
+    else
+      puts "Can't find artefact"
+    end
+  end
+
+  def self.down
+    reverse_changeling = Artefact.find_by_slug(SA_SLUG)
+
+    if reverse_changeling
+      # change the kind and owning app
+
+      reverse_changeling.update_attributes(kind: "smart-answer", owning_app: "smartanswers", rendering_app: nil)
+      puts "Reverting owning app, kind and rendering app"
+
+      # update the slug on all editions
+      puts "Reverting the slug on all editions"
+      artefact = Artefact.find_by_slug(SSA_SLUG)
+      Edition.where(:slug => SA_SLUG).each do |e|
+        e.slug = SSA_SLUG
+        e.panopticon_id = artefact._id
+        e.save(:validate => false)
+      end
+
+      # re-register with panopticon to recreate in search
+      puts "Re-registering with panopticon to recreate in search"
+      ed = Edition.published.where(:slug => SSA_SLUG).first
+      ed.register_with_panopticon
+    else
+      puts "Can't find artefact"
+    end
+  end
+end
