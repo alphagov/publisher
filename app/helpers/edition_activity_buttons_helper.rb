8368b34c721a5e4c0658685e8e1f5fdffafe3a68
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2014-08-28 08:38:16 +0000

    Split button helpers into a separate file
    www.agileplannerapp.com/boards/173808/cards/5629

---
+++ app/helpers/edition_activity_buttons_helper.rb
@@ -0,0 +1,65 @@
+module EditionActivityButtonsHelper
+
+  def build_review_button(edition, activity, title)
+    check_method = "can_#{activity}?".to_sym
+    disabled = edition.send(check_method) ? "" : "disabled"
+    %{<button data-toggle="modal" href="##{activity}_form" class="btn btn-info add-top-margin" value="#{title}" type="submit" #{disabled}>#{title}</button>}
+  end
+
+  def review_buttons(edition)
+    [
+      ["Needs more work",    "request_amendments"],
+      ["OK for publication", "approve_review"]
+    ].map{ |title, activity|
+      build_review_button(edition, activity, title)
+    }.join("\n").html_safe
+  end
+
+  def fact_check_buttons(edition)
+    [
+      ["Needs major changes",    "request_amendments"],
+      ["Minor or no changes required", "approve_fact_check"]
+    ].map{ |title, activity|
+      build_review_button(edition, activity, title)
+    }.join("\n").html_safe
+  end
+
+  def progress_buttons(edition, options = {})
+    [
+      ["Fact check", "send_fact_check"],
+      ["2nd pair of eyes", "request_review"],
+      *scheduled_publishing_buttons(edition),
+      ["Publish", "publish"],
+    ].map { |title, activity, button_color = 'primary'|
+      enabled = edition.send("can_#{activity}?")
+      show_disabled = options.fetch(:show_disabled, true)
+      next unless show_disabled || enabled
+
+      button_options = {
+        type: :submit,
+        data: { toggle: 'modal'},
+        href: "##{activity}_form",
+        class: "btn btn-large btn-#{button_color}",
+        disabled: ! enabled,
+        value: title,
+      }
+      content_tag(:button, button_options) { title }
+    }.join("\n").html_safe
+  end
+
+  def scheduled_publishing_buttons(edition)
+    buttons = []
+    buttons << ["Schedule", "schedule_for_publishing", 'warning'] if edition.can_schedule_for_publishing?
+    buttons << ["Cancel scheduled publishing", "cancel_scheduled_publishing", 'danger'] if edition.can_cancel_scheduled_publishing?
+    buttons
+  end
+
+  def preview_button(edition)
+    form_tag(preview_edition_path(edition), :method => :get) do
+      hidden_field_tag('cache', Time.zone.now().to_i) +
+      hidden_field_tag('edition', edition.version_number) +
+      submit_tag('Preview', class: 'btn btn-primary btn-large')
+    end
+  end
+
+end
