14cb1a878b42ff254193c3c519083614c091e9b5
Andy Mitchell <themitchell@me.com>
2014-11-12 15:04:38 +0000

    Rake task for updating a mainstream slug
    Provides a rake task for updating a mainstream slug
    by updating all editions, updating the artefact and
    reregistering with panopticon.
    
    See:
    https://github.com/alphagov/wiki/wiki/Changing-GOV.UK-URLs#making-the-change

---
+++ lib/mainstream_slug_updater.rb
@@ -0,0 +1,56 @@
+class MainstreamSlugUpdater
+
+  def initialize(old_slug, new_slug, logger = nil)
+    @old_slug = old_slug.sub(/^\//, '')
+    @new_slug = new_slug.sub(/^\//, '')
+    @logger = logger || Logger.new(nil)
+  end
+
+  def update
+    update_slug_on_all_editions
+    update_artefact_slug
+    reregister_slug_with_panopticon
+  end
+
+  def published_edition
+    @published_edition ||= editions.find { |e| e.published? }
+  end
+
+private
+  attr_reader(
+    :old_slug,
+    :new_slug,
+    :logger
+  )
+
+  def user
+    @user ||= User.find_or_create_by(name: "2nd Line Support")
+  end
+
+  def editions
+    @editions ||= Edition.where(slug: old_slug).to_a
+  end
+
+  def artefact
+    @artefact ||= Artefact.find_by_slug(old_slug)
+  end
+
+  def update_slug_on_all_editions
+    logger.info "Updating the slug on all Editions"
+    editions.each do |e|
+      e.slug = new_slug
+      e.save(validate: false)
+    end
+  end
+
+  def update_artefact_slug
+    logger.info "Updating the slug on the Artefact"
+    artefact.slug = new_slug
+    artefact.save_as(user, validate: false)
+  end
+
+  def reregister_slug_with_panopticon
+    logger.info "Re-registering with panopticon to re-create in search"
+    published_edition.register_with_panopticon
+  end
+end
---
+++ lib/tasks/update_mainstream_slug.rake
@@ -0,0 +1,6 @@
+desc "Update a mainstream slug.\n
+See original documentation @ https://github.com/alphagov/wiki/wiki/Changing-GOV.UK-URLs#making-the-change"
+
+task :update_mainstream_slug, [:old_slug, :new_slug] => :environment do |_task, args|
+  MainstreamSlugUpdater.new(args[:old_slug], args[:new_slug]).update
+end
