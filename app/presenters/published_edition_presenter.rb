3c654ad868d734d4bc83f1516a2d69e01c58e468
Richard Boulton <richard.boulton@digital.cabinet-office.gov.uk>
2014-11-14 07:36:59 +0000

    Support sending republishes to content store
    Update the `render_for_publishing_api` method to accept a parameter
    telling it whether it's a republish event.  If so, it returns an
    `update_type` field of `republish`.  This is understood by the content
    store, and allows us to re-register content with the content store,
    without causing actions like email alerts to happen.

--- app/presenters/published_edition_presenter.rb
+++ app/presenters/published_edition_presenter.rb
@@ -3,7 +3,7 @@ class PublishedEditionPresenter
     @edition = edition
   end
 
-  def render_for_publishing_api
+  def render_for_publishing_api(republish)
     {
       title: @edition.title,
       base_path: base_path,
@@ -17,7 +17,7 @@ class PublishedEditionPresenter
         {path: base_path, type: "exact"}
       ],
       redirects: [],
-      update_type: "major",
+      update_type: republish ? "republish" : "major",
       details: {
         change_note: "",
         tags: {
--- test/unit/published_edition_presenter_test.rb
+++ test/unit/published_edition_presenter_test.rb
@@ -2,39 +2,46 @@ require "test_helper"
 
 class PublishedEditionPresenterTest < ActiveSupport::TestCase
   context ".render_for_publishing_api" do
-    should "create an attributes hash for the publishing api" do
-      edition = FactoryGirl.create(:edition, :published,
-        browse_pages: ["tax/vat", "tax/capital-gains"],
-        primary_topic: "oil-and-gas/wells",
-        additional_topics: ["oil-and-gas/fields", "oil-and-gas/distillation"]
-      )
+    edition = FactoryGirl.create(:edition, :published,
+      browse_pages: ["tax/vat", "tax/capital-gains"],
+      primary_topic: "oil-and-gas/wells",
+      additional_topics: ["oil-and-gas/fields", "oil-and-gas/distillation"]
+    )
 
-      presenter = PublishedEditionPresenter.new(edition)
+    presenter = PublishedEditionPresenter.new(edition)
 
-      expected_attributes_for_publishing_api_hash = {
-        title: edition.title,
-        base_path: "/#{edition.slug}",
-        description: edition.overview,
-        format: "placeholder",
-        need_ids: [],
-        public_updated_at: edition.updated_at,
-        publishing_app: "publisher",
-        rendering_app: "frontend",
-        routes: [ { path: "/#{edition.slug}", type: "exact" }],
-        redirects: [],
-        update_type: "major",
-        details: {
-          change_note: "",
-          tags: {
-            browse_pages: ["tax/vat", "tax/capital-gains"],
-            primary_topic: ["oil-and-gas/wells"],
-            additional_topics: ["oil-and-gas/fields", "oil-and-gas/distillation"],
-            topics: ["oil-and-gas/wells", "oil-and-gas/fields", "oil-and-gas/distillation"],
-          }
+    expected_attributes_for_publishing_api_hash = {
+      title: edition.title,
+      base_path: "/#{edition.slug}",
+      description: edition.overview,
+      format: "placeholder",
+      need_ids: [],
+      public_updated_at: edition.updated_at,
+      publishing_app: "publisher",
+      rendering_app: "frontend",
+      routes: [ { path: "/#{edition.slug}", type: "exact" }],
+      redirects: [],
+      update_type: "major",
+      details: {
+        change_note: "",
+        tags: {
+          browse_pages: ["tax/vat", "tax/capital-gains"],
+          primary_topic: ["oil-and-gas/wells"],
+          additional_topics: ["oil-and-gas/fields", "oil-and-gas/distillation"],
+          topics: ["oil-and-gas/wells", "oil-and-gas/fields", "oil-and-gas/distillation"],
         }
       }
+    }
 
-      assert_equal expected_attributes_for_publishing_api_hash, presenter.render_for_publishing_api
+    should "create an attributes hash for the publishing api" do
+      assert_equal expected_attributes_for_publishing_api_hash, presenter.render_for_publishing_api(false)
+    end
+
+    should "create an attributes hash for the publishing api" do
+      attributes_for_republish = expected_attributes_for_publishing_api_hash.merge({
+        update_type: "republish",
+      })
+      assert_equal attributes_for_republish, presenter.render_for_publishing_api(true)
     end
   end
 end
