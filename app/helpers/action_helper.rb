4979e899cdbac82d41e32ac9f56141944e629520
Paul Hayes <fofr@users.noreply.github.com>
2014-11-17 11:28:14 +0000

    Move html_safe closer to source

--- app/helpers/action_helper.rb
+++ app/helpers/action_helper.rb
@@ -11,16 +11,16 @@ module ActionHelper
     if action.comment.present?
       format_and_auto_link_plain_text(action.comment)
     elsif action.is_fact_check_request? && action.email_addresses.present?
-      "Request sent to #{mail_to action.email_addresses}"
+      "Request sent to #{mail_to action.email_addresses}".html_safe
     elsif action.recipient_id.present?
-      "Assigned to #{mail_to action.recipient.email, action.recipient.name}"
+      "Assigned to #{mail_to action.recipient.email, action.recipient.name}".html_safe
     end
   end
 
   def format_and_auto_link_plain_text(text)
     text = auto_link(escape_once(text), link: :urls, sanitize: false)
     text = auto_link_zendesk_tickets(text)
-    simple_format(text, {}, :sanitize => false)
+    simple_format(text, {}, :sanitize => false).html_safe
   end
 
   def auto_link_zendesk_tickets(text)
