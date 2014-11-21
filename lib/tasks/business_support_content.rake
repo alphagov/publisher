befab7b600e2bfdbb2b07970903f6447e7a7ad5c
steventux <steve.laing@digital.cabinet-office.gov.uk>
2014-11-21 12:03:43 +0000

    Add business support areas data and importer

--- lib/tasks/business_support_content.rake
+++ lib/tasks/business_support_content.rake
@@ -8,4 +8,9 @@ namespace :business_support_content do
   task :migrate_locations_to_areas => :environment do
     BusinessSupportLocationMigrator.run
   end
+
+  desc "Adds regional data to BusinessSupportEditions"
+  task :import_areas, [:data_path] => :environment do |t, args|
+    BusinessSupportAreasImporter.run(args[:data_path])
+  end
 end
---
+++ test/unit/lib/business_support_areas_importer_test.rb
@@ -0,0 +1,57 @@
+require 'test_helper'
+
+class BusinessSupportAreasImporterTest < ActiveSupport::TestCase
+
+  def temp_csv_file(contents)
+    f = Tempfile.new(['bsf_areas_test_example', '.csv'])
+    f.write contents
+    f.rewind
+    f
+  end
+
+  context "run" do
+    setup do
+      file = temp_csv_file(<<-EOT)
+slug,regions
+business-start-up-support,"Trumpton Town Council, Bolton Metropolitan Borough Council, Trafford Metropolitan Borough Council"
+international-economic-development-support,"London, North West, Scotland, West Midlands, Northern Ireland, South East"
+archived,London
+archived-artefact,London
+investment-escalatorzz,London
+EOT
+
+      %w(business-start-up-support international-economic-development-support archived-artefact investment-escalatorzz).each do |slug|
+        FactoryGirl.create(:business_support_edition, slug: slug, state: "published")
+      end
+      FactoryGirl.create(:business_support_edition, slug: "archived", state: "archived")
+      BusinessSupportEdition.where(slug: "archived-artefact").first.artefact.update_attribute(:state, "archived")
+
+      results = OpenStruct.new(results: ["bolton-metropolitan-borough-council", "trafford-metropolitan-borough-council", "london",
+        "north-west", "scotland", "west-midlands", "northern-ireland", "south-east"].map { |s| OpenStruct.new(slug: s) })
+
+      GdsApi::Imminence.any_instance.stubs(:areas_for_type).returns(results)
+
+      silence_stream(STDOUT) do
+        BusinessSupportAreasImporter.run(file.path)
+      end
+    end
+
+    should "ignore business support editions which are not published" do
+      assert_empty BusinessSupportEdition.where(slug: "archived").first.areas
+    end
+
+    should "ignore business support editions with an archived artefact" do
+      assert_empty BusinessSupportEdition.where(slug: "archived-artefact").first.areas
+    end
+
+    should "omit areas which are not present in Imminence" do
+      assert_equal %w(bolton-metropolitan-borough-council trafford-metropolitan-borough-council),
+        BusinessSupportEdition.where(slug: "business-start-up-support").first.areas
+    end
+
+    should "import areas for existing business support editions" do
+      assert_equal %w(london north-west scotland west-midlands northern-ireland south-east),
+        BusinessSupportEdition.where(slug: "international-economic-development-support").first.areas
+    end
+  end
+end
