1437ebb6f8519683eff673499cff2ebd8441cd18
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2014-11-28 11:14:12 +0000

    Fix migration written to fix synced users
    The earlier version was incompatible with mongoid,
    because it expects keys to be symbols or strings.

--- db/migrate/20141128075604_fix_erroneously_synced_users_with_empty_permissions.rb
+++ db/migrate/20141128075604_fix_erroneously_synced_users_with_empty_permissions.rb
@@ -1,6 +1,6 @@
 class FixErroneouslySyncedUsersWithEmptyPermissions < Mongoid::Migration
   def self.up
-    User.delete_all(:permissions => [], :created_at.gte => Time.zone.parse("27-Nov-2014 10:50"))
+    User.where(:permissions => [], :created_at.gte => Time.zone.parse("27-Nov-2014 10:50")).delete_all
   end
 
   def self.down
