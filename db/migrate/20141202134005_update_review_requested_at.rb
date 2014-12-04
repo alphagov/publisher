1f86dcbd428612125499b98adb2f81e278b63d0a
steventux <steve.laing@digital.cabinet-office.gov.uk>
2014-12-04 10:56:32 +0000

    Migrate review_requested_at for editions in review

---
+++ db/migrate/20141202134005_update_review_requested_at.rb
@@ -0,0 +1,16 @@
+class UpdateReviewRequestedAt < Mongoid::Migration
+  def self.up
+    Edition.in_review.each do |edition|
+      request_review_action = edition.actions.where(:request_type => Action::REQUEST_REVIEW).last
+      if request_review_action
+        edition.update_attribute(:review_requested_at, request_review_action.created_at)
+      end
+    end
+  end
+
+  def self.down
+    Edition.in_review.each do |edition|
+      edition.update_attribute(:review_requested_at, nil)
+    end
+  end
+end
