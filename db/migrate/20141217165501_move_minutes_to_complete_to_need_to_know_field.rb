cf734bfef20e4e780e9d37e62c72bde2a6f072d1
Paul Hayes <fofr@users.noreply.github.com>
2014-12-18 10:52:39 +0000

    Use welsh minutes message where appropriate
    Tweak migration to use welsh when the edition is welsh

--- db/migrate/20141217165501_move_minutes_to_complete_to_need_to_know_field.rb
+++ db/migrate/20141217165501_move_minutes_to_complete_to_need_to_know_field.rb
@@ -1,7 +1,13 @@
 class MoveMinutesToCompleteToNeedToKnowField < Mongoid::Migration
   def self.up
-    Edition.where(:minutes_to_complete.ne => "", :minutes_to_complete.exists => true).each do |edition|
-      edition.set(:need_to_know, "- Takes around #{edition.minutes_to_complete} minutes\r\n#{edition.need_to_know}")
+    Edition.where(:minutes_to_complete.nin => ["", nil]).each do |edition|
+      language = edition.artefact.language
+
+      if language == 'cy'
+        edition.set(:need_to_know, "- Mae'n cymryd tua #{edition.minutes_to_complete} munud\r\n#{edition.need_to_know}")
+      else
+        edition.set(:need_to_know, "- Takes around #{edition.minutes_to_complete} minutes\r\n#{edition.need_to_know}")
+      end
     end
 
     Edition.all.each { |edition| edition.unset(:minutes_to_complete) }
