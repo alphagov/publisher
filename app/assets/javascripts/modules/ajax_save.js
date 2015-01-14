eb2e4904d81291e92973125c0b350be5d05ede5d
Paul Hayes <fofr@users.noreply.github.com>
2015-01-14 15:24:23 +0000

    Create an ajax save JS module
    * When clicking on a `js-save` element, post the form using ajax
    * Serialise the values of the form
    * Toggle classes and text on a message element to indicate state
    * Pickup on returned validation errors and display those messages
    alongside the erroring inputs

---
+++ app/assets/javascripts/modules/ajax_save.js
@@ -0,0 +1,81 @@
+(function(Modules) {
+  "use strict";
+  Modules.AjaxSave = function() {
+    this.start = function(element) {
+      var url = element.attr('action') + '.json',
+          message = element.find('.js-status-message'),
+          hideTimeout;
+
+      element.on('click', '.js-save', save);
+
+      function save(evt) {
+        evt.preventDefault();
+        saving();
+
+        $.ajax({
+          url : url,
+          type : 'POST',
+          data : element.serialize(),
+          success : success,
+          error: error
+        });
+      }
+
+      function success(response) {
+        hideErrors();
+        message.addClass('workflow-message-saved').removeClass('workflow-message-saving');
+        message.text('Saved');
+        hideTimeout = setTimeout(hide, 2000);
+      }
+
+      function error(response) {
+        showErrors(response.responseJSON);
+        message.addClass('workflow-message-error').removeClass('workflow-message-saving');
+        message.text('Couldn’t save');
+        hideTimeout = setTimeout(hide, 2000);
+      }
+
+      function showErrors(errors) {
+        var keys = Object.keys(errors);
+
+        for(var i = 0, l = keys.length; i < l; i++) {
+
+          var errorElement = element.find('#edition_' + keys[i] + '_input'),
+              errorMessages = errors[keys[i]],
+              $list = $('<ul class="help-block js-error"></ul>');
+
+          errorElement.addClass('has-error');
+
+          for(var j = 0, m = errorMessages.length; j < m; j++) {
+            $list.append('<li>' + errorMessages[j] + '</li>');
+          }
+
+          errorElement.append($list);
+        }
+      }
+
+      function hideErrors() {
+        element.find('.js-error').remove();
+        element.find('.has-error').removeClass('has-error');
+      }
+
+      function saving() {
+        reset();
+        message.addClass('workflow-message-saving');
+        message.text('Saving…');
+      }
+
+      function hide() {
+        message.addClass('workflow-message-hide');
+      }
+
+      function reset() {
+        hideErrors();
+        clearTimeout(hideTimeout);
+        message.removeClass(function (index, css) {
+          return (css.match(/(^|\s)workflow-message-\S+/g) || []).join(' ');
+        });
+      }
+    }
+  };
+})(window.GOVUKAdmin.Modules);
---
+++ spec/javascripts/spec/ajax_save.spec.js
@@ -0,0 +1,148 @@
+describe('An ajax save module', function() {
+  "use strict";
+
+  var ajaxSave,
+      element;
+
+  beforeEach(function() {
+
+    element = $('<form action="some/url">\
+      <div class="js-status-message"></div>\
+      <div id="edition_test_input">\
+        <input type="text" name="test" value="prefilled value">\
+      </div>\
+      <div id="edition_another_input">\
+        <input type="text" name="another" value="another value">\
+      </div>\
+      <input type="submit" class="js-save" value="Save">\
+    </form>');
+
+    $('body').append(element);
+    ajaxSave = new GOVUKAdmin.Modules.AjaxSave();
+    ajaxSave.start(element);
+  });
+
+  afterEach(function() {
+    element.remove();
+  });
+
+  describe('when clicking a save link', function() {
+    it('indicates the form is saving' , function() {
+      element.find('.js-save').trigger('click');
+
+      var statusMessage = element.find('.js-status-message');
+      expect(statusMessage.text()).toBe('Saving…');
+      expect(statusMessage.is('.workflow-message-saving')).toBe(true);
+    });
+
+    it('posts the form using ajax' , function() {
+      var ajaxOptions;
+      spyOn($, 'ajax').and.callFake(function(options) {
+        ajaxOptions = options;
+      });
+      element.find('.js-save').trigger('click');
+
+      expect($.ajax).toHaveBeenCalled();
+      expect(ajaxOptions.type).toBe('POST');
+      expect(ajaxOptions.url).toBe('some/url.json');
+      expect(ajaxOptions.data).toBe('test=prefilled+value&another=another+value');
+    });
+  });
+
+  describe('when an ajax save is successful', function() {
+    var timeoutTime;
+
+    beforeEach(function() {
+      spyOn($, 'ajax').and.callFake(function(options) {
+        options.success({});
+      });
+      spyOn(window, 'setTimeout').and.callFake(function(fn, time) {
+        timeoutTime = time;
+        fn();
+      });
+      element.find('.js-save').trigger('click');
+    });
+
+    it('says that it has saved' , function() {
+      var statusMessage = element.find('.js-status-message');
+      expect(statusMessage.text()).toBe('Saved');
+      expect(statusMessage.is('.workflow-message-saved')).toBe(true);
+    });
+
+    it('the save message disappears after a short while', function() {
+      expect(timeoutTime).toBe(2000);
+      expect(element.find('.js-status-message').is('.workflow-message-hide'));
+    });
+  });
+
+
+  describe('when an ajax save errors with validation messages', function() {
+    var timeoutTime, ajaxError, ajaxSuccess;
+
+    beforeEach(function() {
+      spyOn($, 'ajax').and.callFake(function(options) {
+        ajaxError = function(errors) {
+          errors = errors || {};
+          options.error({responseJSON: errors});
+        };
+
+        ajaxSuccess = options.success;
+      });
+      spyOn(window, 'setTimeout').and.callFake(function(fn, time) {
+        timeoutTime = time;
+        fn();
+      });
+      element.find('.js-save').trigger('click');
+    });
+
+    it('says that it couldn’t save' , function() {
+      ajaxError();
+      var statusMessage = element.find('.js-status-message');
+      expect(statusMessage.text()).toBe('Couldn’t save');
+      expect(statusMessage.is('.workflow-message-error')).toBe(true);
+    });
+
+    it('the error message disappears after a short while', function() {
+      ajaxError();
+      expect(timeoutTime).toBe(2000);
+      expect(element.find('.js-status-message').is('.workflow-message-hide'));
+    });
+
+    it('shows the error alongside the erroring field', function() {
+      ajaxError({test: ['must be changed']});
+      expect(element.find('#edition_test_input').is('.has-error')).toBe(true);
+      expect(element.find('#edition_test_input ul li').length).toBe(1);
+      expect(element.find('#edition_test_input ul li:first').text()).toBe('must be changed');
+    });
+
+    it('ignores validation messages for fields it does not recognise', function() {
+      ajaxError({not_a_field: ['nonsense']});
+      expect(element.find('.has-error').length).toBe(0);
+      expect(element.find('.js-error').length).toBe(0);
+    });
+
+    it('can show multiple errors', function() {
+      ajaxError({test: ['must be changed', 'must be blue'], another: ['must rhyme']});
+
+      expect(element.find('#edition_test_input').is('.has-error')).toBe(true);
+      expect(element.find('#edition_test_input ul li').length).toBe(2);
+      expect(element.find('#edition_test_input ul li:first').text()).toBe('must be changed');
+      expect(element.find('#edition_test_input ul li:last').text()).toBe('must be blue');
+
+      expect(element.find('#edition_another_input').is('.has-error')).toBe(true);
+      expect(element.find('#edition_another_input ul li').length).toBe(1);
+      expect(element.find('#edition_another_input ul li:first').text()).toBe('must rhyme');
+    });
+
+    describe('when the form is saved again', function() {
+      it('removes all errors', function() {
+        ajaxError({test: ['must be changed', 'must be blue'], another: ['must rhyme']});
+        element.find('.js-save').trigger('click');
+
+        expect(element.find('.has-error').length).toBe(0);
+        expect(element.find('.js-error').length).toBe(0);
+      });
+    });
+
+  });
+});
