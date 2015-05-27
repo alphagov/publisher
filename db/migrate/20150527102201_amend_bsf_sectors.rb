9760bdc131139c9fd76f555a6c1550a93bcc1b6e
Ben Janecke <benjanecke@gmail.com>
2015-05-27 11:43:25 +0100

    Amend BSF Sectors
    * Rename BSF sectors to have more descriptive names 
    * Remove extraneous sectors

---
+++ db/migrate/20150527102201_amend_bsf_sectors.rb
@@ -0,0 +1,26 @@
+class AmendBsfSectors < Mongoid::Migration
+  def self.up
+    # Amend sector names
+    BusinessSupport::Sector.where(slug: "agriculture").update(name: "Agriculture, fishing and forestry")
+    BusinessSupport::Sector.where(slug: "business-and-finance").update(name: "Financial services and business consultancy")
+    BusinessSupport::Sector.where(slug: "construction").update(name: "Construction")
+    BusinessSupport::Sector.where(slug: "education").update(name: "Education")
+    BusinessSupport::Sector.where(slug: "health").update(name: "Medical, mental health, addiction and social work")
+    BusinessSupport::Sector.where(slug: "hospitality-and-catering").update(name: "Accommodation and food services")
+    BusinessSupport::Sector.where(slug: "manufacturing").update(name: "Manufacturing and engineering")
+    BusinessSupport::Sector.where(slug: "mining").update(name: "Mining and quarrying")
+    BusinessSupport::Sector.where(slug: "real-estate").update(name: "Real estate, renting and property development")
+    BusinessSupport::Sector.where(slug: "science-and-technology").update(name: "Research and development")
+    BusinessSupport::Sector.where(slug: "transport-and-distribution").update(name: "Transport, travel, storage and distribution")
+    BusinessSupport::Sector.where(slug: "utilities").update(name: "Electricity, gas and water supply")
+    BusinessSupport::Sector.where(slug: "wholesale-and-retail").update(name: "Wholsale, retail and repairs")
+
+    # Remove sectors
+    BusinessSupport::Sector.where(slug: "information-communication-and-media").destroy
+    BusinessSupport::Sector.where(slug: "service-industries").destroy
+    BusinessSupport::Sector.where(slug: "travel-and-leisure").destroy
+  end
+
+  def self.down
+  end
+end
