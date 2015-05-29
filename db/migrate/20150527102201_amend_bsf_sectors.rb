68d10c1856c22745e73e0e7426a6f6717ad29137
Ben Janecke <benjanecke@gmail.com>
2015-05-29 12:51:52 +0100

    Create new BSF sectors
    These sectors are being added to provide more granular results for the business support finder.

--- db/migrate/20150527102201_amend_bsf_sectors.rb
+++ db/migrate/20150527102201_amend_bsf_sectors.rb
@@ -19,6 +19,15 @@ class AmendBsfSectors < Mongoid::Migration
     BusinessSupport::Sector.where(slug: "information-communication-and-media").destroy
     BusinessSupport::Sector.where(slug: "service-industries").destroy
     BusinessSupport::Sector.where(slug: "travel-and-leisure").destroy
+
+    # Create new sectors
+    BusinessSupport::Sector.create(slug: "post-couriers-telecommunication", name: "Post, couriers and telecommunication")
+    BusinessSupport::Sector.create(slug: "media-advertising-publishing" , name: "Media, advertising and publishing")
+    BusinessSupport::Sector.create(slug: "motor-retail-repair-wholesale", name: "Motor retail, repair and wholesale")
+    BusinessSupport::Sector.create(slug: "arts-entertainment-sport", name: "Arts, entertainment and sport")
+    BusinessSupport::Sector.create(slug: "call-centers-administrative-services", name: "Call centres and administrative services")
+    BusinessSupport::Sector.create(slug: "tradespeople-cleaners-maintenance", name: "Tradespeople, cleaners and maintenance")
+    BusinessSupport::Sector.create(slug: "professional-scientific-technical", name: "Professional, scientific and technical ")
   end
 
   def self.down
