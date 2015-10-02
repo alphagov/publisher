62b9d901bebb81f0853da91344d5be888d4e657a
tijmen <tijmen@gmail.com>
2015-10-02 13:58:24 +0100

    Add rake task to republish content
    This task does the same as `rake reregister:all`, but only sends the
    content to the publishing-api (and not panopticon).

--- app/workers/publishing_api_notifier.rb
+++ app/workers/publishing_api_notifier.rb
@@ -1,20 +1,12 @@
-require "gds_api/publishing_api"
-
 class PublishingAPINotifier
   include Sidekiq::Worker
 
-  def perform(edition_id)
+  def perform(edition_id, update_type = "normal")
     edition = Edition.find(edition_id)
     presenter = PublishedEditionPresenter.new(edition)
-    document_for_publishing_api = presenter.render_for_publishing_api(republish: false)
+    document_for_publishing_api = presenter.render_for_publishing_api(republish: update_type == "republish")
     base_path = document_for_publishing_api[:base_path]
 
-    publishing_api.put_content_item(base_path, document_for_publishing_api)
-  end
-
-private
-
-  def publishing_api
-    @publishing_api ||= GdsApi::PublishingApi.new(Plek.find("publishing-api"))
+    Services.publishing_api.put_content_item(base_path, document_for_publishing_api)
   end
 end
