70df24a7b16294d016295e1930fd8c186d56d2d5
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2016-02-24 10:09:24 +0000

    Update configuration files.
    This will need a change to puppet/deployment scripts to add new env variable
    for SECRET_KEY_BASE.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,6 +7,11 @@ Publisher::Application.configure do
   # and recreated between test runs.  Don't rely on the data there!
   config.cache_classes = true
 
+  # Do not eager load code on boot. This avoids loading your whole application
+  # just for the purpose of running a single test. If you are using a tool that
+  # preloads Rails for running tests, you may have to set it to true.
+  config.eager_load = false
+
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
@@ -34,4 +39,6 @@ Publisher::Application.configure do
   config.active_support.deprecation = :stderr
 
   config.action_mailer.default_url_options = { :host => "example.com" }
+
+  config.active_support.test_order = :sorted
 end
