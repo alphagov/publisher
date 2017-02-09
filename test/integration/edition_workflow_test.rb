d725a17041fa8e051a3d5388a9ddeb03c7b4eb18
Tom Gladhill <whoojemaflip@gmail.com>
2017-02-09 14:05:06 +0000

    Reduce test run time by 50s
    Reduce the number of page loads by relying on object factories to
    initialise test data. These tests still contain the same assertions
    as they used to, but the execution time for the class is down from 77s
    to 26s.

--- test/integration/edition_workflow_test.rb
+++ test/integration/edition_workflow_test.rb
@@ -1,30 +1,268 @@
 require 'integration_test_helper'
 
 class EditionWorkflowTest < JavascriptIntegrationTest
+  attr_reader :alice, :bob, :guide
 
   setup do
     stub_linkables
 
-    %w(Alice Bob Charlie).each do |name|
-      FactoryGirl.create(:user, name: name)
-    end
+    @alice = FactoryGirl.create(:user, name: "Alice")
+    @bob = FactoryGirl.create(:user, name: "Bob")
+
+    @guide = FactoryGirl.create(:guide_edition)
+
+    login_as "Alice"
   end
 
   teardown do
     GDS::SSO.test_user = nil
   end
 
-  # Assign a guide to a user. The user parameter can be a User or a name
-  def assign(guide, user)
-    if user.is_a? User
-      user = user.name
-    end
+  test "should show and update a guide's assigned person" do
+    assert_nil guide.assigned_to
+
+    visit_edition guide
+    select "Bob", from: "Assigned to"
+    save_edition_and_assert_success
+    guide.reload
+
+    assert_equal guide.assigned_to, bob
+  end
+
+  test "doesn't show disabled users in 'Assigned to' select box" do
+    disabled_user = FactoryGirl.create(:disabled_user)
+
     visit_edition guide
 
-    select user, from: "Assigned to"
-    save_edition_and_assert_success
+    refute page.has_xpath?("//select[@id='edition_assigned_to_id']/option[text() = '#{disabled_user.name}']")
+  end
 
+  test "can send guide to fact-check when in ready state" do
+    guide.update_attribute(:state, 'ready')
+    visit_edition guide
+
+    page.find_link('Fact check').trigger('click')
+
+    within "#send_fact_check_form" do
+      fill_in "Customised message", with: "Blah"
+      fill_in "Email address", with: "user@example.com"
+      click_on "Send"
+    end
+
+    assert page.has_css?('.label', text: 'Fact check')
     guide.reload
+    assert guide.fact_check?
+  end
+
+  test "a guide in the ready state can be requested to make more amendments" do
+    guide.update_attribute(:state, 'ready')
+
+    visit_edition guide
+    send_action guide, "Needs more work", "Request amendments", "You need to fix some stuff"
+    filter_for_all_users
+    view_filtered_list "Amends needed"
+
+    assert page.has_content? guide.title
+  end
+
+  test "a guide in the fact check state can be requested to make more amendments" do
+    guide.update_attribute(:state, 'fact_check')
+
+    visit_edition guide
+    send_action guide, "Needs more work", "Request amendments", "You need to fix some stuff"
+    filter_for_all_users
+    view_filtered_list "Amends needed"
+
+    assert page.has_content? guide.title
+  end
+
+  test "can flag guide for review" do
+    guide.assigned_to = bob
+
+    visit_edition guide
+    send_action guide, "2nd pair of eyes", "Send to 2nd pair of eyes", "I think this is done"
+    filter_for_all_users
+    view_filtered_list "In review"
+
+    assert page.has_content? guide.title
+  end
+
+  test "cannot review own guide" do
+    guide.assigned_to = alice
+
+    visit_edition guide
+    send_action guide, "2nd pair of eyes", "Send to 2nd pair of eyes", "I think this is done"
+
+    assert page.has_selector?(".alert-info")
+    refute has_link? "OK for publication"
+  end
+
+  test "cannot be the guide reviewer and assignee" do
+    guide.assigned_to = bob
+    guide.update_attribute(:state, 'in_review')
+
+    visit_edition guide
+    select("Bob", from: "Reviewer")
+    save_edition_and_assert_error
+
+    assert page.has_css?(".form-group.has-error li", text: "can't be the assignee")
+  end
+
+  test "can deselect the guide reviewer" do
+    guide.assigned_to = bob
+
+    visit_edition guide
+    send_action guide, "2nd pair of eyes", "Send to 2nd pair of eyes", "I think this is done"
+
+    select("", from: "Reviewer")
+    save_edition_and_assert_success
+  end
+
+  test "can unassign the guide" do
+    guide.assigned_to = bob
+
+    visit_edition guide
+    select "", from: "Assigned to"
+    save_edition_and_assert_success
+    guide.reload
+
+    assert_nil guide.assignee
+    assert page.has_select?("Assigned to", selected: "")
+  end
+
+  test "can become the guide reviewer" do
+    guide.assigned_to = bob
+
+    send_action guide, "2nd pair of eyes", "Send to 2nd pair of eyes", "I think this is done"
+
+    visit_edition guide
+
+    select("Alice", from: "Reviewer")
+    save_edition_and_assert_success
+  end
+
+
+  test "can review another's guide" do
+    guide.update_attribute(:state, 'in_review')
+    guide.assigned_to = bob
+
+    visit_edition guide
+    assert page.has_selector?(".alert-info")
+    assert has_link? "Needs more work"
+    assert has_link? "OK for publication"
+  end
+
+  test "review failed" do
+    guide.update_attribute(:state, 'in_review')
+    guide.assigned_to = bob
+
+    visit_edition guide
+    send_action guide, "Needs more work", "Request amendments", "You need to fix some stuff"
+    filter_for_all_users
+    view_filtered_list "Amends needed"
+
+    assert page.has_content? guide.title
+  end
+
+  test "review passed" do
+    guide.update_attribute(:state, 'in_review')
+
+    visit_edition guide
+    send_action guide, "OK for publication", "OK for publication", "Yup, looks good"
+    filter_for_all_users
+    view_filtered_list "Ready"
+    assert page.has_content? guide.title
+  end
+
+  test "can skip fact check" do
+    guide.update_attribute(:state, 'fact_check')
+
+    visit_edition guide
+
+    click_on "Admin"
+    click_on "Skip fact check"
+
+    # This information is not quite correct but it is the current behaviour.
+    # Adding this test as an aid to future improvements
+    assert page.has_content? "Fact check was skipped for this edition."
+    filter_for_all_users
+    view_filtered_list "Ready"
+    assert page.has_content? guide.title
+
+    visit_edition guide
+    assert page.has_content? "Request this edition to be amended further."
+    assert page.has_content? "Needs more work"
+  end
+
+  test "can progress from fact check" do
+    guide.update_attribute(:state, 'fact_check_received')
+
+    visit_edition guide
+    send_action guide, "Minor or no changes required", "Approve fact check", "Hurrah!"
+    filter_for_all_users
+    view_filtered_list "Ready"
+
+    assert page.has_content? guide.title
+  end
+
+  test "can go back to fact check from fact check received" do
+    guide.update_attribute(:state, 'fact_check_received')
+
+    visit_edition guide
+    send_for_fact_check guide
+    visit_edition guide
+
+    assert page.has_css?('.label', text: 'Fact check')
+  end
+
+  test "can create a new edition from the listings screens" do
+    guide.update_attribute(:state, 'published')
+
+    visit '/'
+    filter_for_all_users
+    view_filtered_list "Published"
+    click_on "Create new edition"
+
+    assert page.has_content? "New edition created"
+  end
+
+  test "should link to a newer sibling" do
+    artefact = FactoryGirl.create(:artefact)
+    old_edition = FactoryGirl.create(
+      :guide_edition,
+      panopticon_id: artefact.id,
+      state: "published",
+      version_number: 1
+    )
+    new_edition = FactoryGirl.create(
+      :guide_edition,
+      panopticon_id: artefact.id,
+      state: "draft",
+      version_number: 2
+    )
+    visit_edition old_edition
+    assert page.has_link?(
+      "Edit existing newer edition",
+      href: edition_path(new_edition)
+    ), "Page should have edit link"
+  end
+
+  test "should show an alert if another person has created a newer edition" do
+    guide.update_attribute(:state, 'published')
+
+    filter_for_all_users
+    view_filtered_list "Published"
+
+    # Simulate that someone has clicked on 'Create new edition'
+    # while current user has been viewing the list of published editions
+    new_edition = guide.build_clone(GuideEdition)
+    new_edition.save
+
+    # Current user now decides to click the button
+    click_on "Create new edition"
+
+    assert page.has_content?("Another person has created a newer edition")
+    assert page.has_css?('.label', text: 'Published')
   end
 
   def send_for_generic_action(guide, button_text, &block)
@@ -63,14 +301,10 @@ class EditionWorkflowTest < JavascriptIntegrationTest
     end
   end
 
-  def submit_for_review(guide, options={message: "I think this is done"})
-    send_action guide, "2nd pair of eyes", "Send to 2nd pair of eyes", "I think this is done"
-  end
-
-  def filter_for(user)
+  def filter_for_all_users
     visit "/"
     within :css, ".user-filter-form" do
-      select "All", :from => 'user_filter'
+      select "All", from: 'user_filter'
       click_on "Filter publications"
     end
     assert page.has_content?("Publications")
@@ -85,315 +319,5 @@ class EditionWorkflowTest < JavascriptIntegrationTest
     filter_link.click
 
     assert page.has_content?(filter_label)
-  end
-
-  # Given a guide and an owner, take the guide to review stage
-  def get_to_review(guide, owner)
-    login_as owner
-    assign guide, owner
-    fill_in_parts guide
-    submit_for_review guide
-  end
-
-  def get_to_fact_check_received(guide, owner)
-    get_to_fact_check(guide, owner)
-    receive_fact_check(User.new, guide)
-    guide.reload
-  end
-
-  def get_to_fact_check(guide, owner)
-    get_to_review guide, owner
-    login_as "Bob"
-    send_action guide, "OK for publication", "OK for publication", "Yup, looks good"
-    send_for_fact_check guide
-  end
-
-  test "should show and update a guide's assigned person" do
-    guide = FactoryGirl.create(:guide_edition)
-    fill_in_parts guide
-    assert_nil guide.assigned_to
-
-    assign guide, "Bob"
-    assert_equal guide.assigned_to, get_user("Bob")
-
-    assign guide, "Charlie"
-    assert_equal guide.assigned_to, get_user("Charlie")
-  end
-
-  test "can assign a new guide without editing the part" do
-    guide = FactoryGirl.create(:guide_edition)
-
-    assign guide, "Bob"
-    assert_equal guide.assigned_to, get_user("Bob")
-  end
-
-  test "doesn't show disabled users in 'Assigned to' select box" do
-    disabled_user = FactoryGirl.create(:disabled_user)
-    guide = FactoryGirl.create(:guide_edition)
-
-    visit_edition guide
-
-    refute page.has_xpath?("//select[@id='edition_assigned_to_id']/option[text() = '#{disabled_user.name}']")
-  end
-
-  test "a guide is in draft after creation" do
-    guide = FactoryGirl.create(:guide_edition)
-
-    assign guide, "Alice"
-    assert guide.draft?
-
-    visit_edition guide
-    assert page.has_css?('.label', text: 'Draft')
-  end
-
-  test "should update progress of a guide" do
-    guide = FactoryGirl.create(:guide_edition)
-    guide.update_attribute(:state, 'ready')
-    fill_in_parts guide
-
-    page.find_link('Fact check').trigger('click')
-
-    within "#send_fact_check_form" do
-      fill_in "Customised message", with: "Blah"
-      fill_in "Email address", with: "user@example.com"
-      click_on "Send"
-    end
-
-    assert page.has_css?('.label', text: 'Fact check')
-
-    guide.reload
-
-    assert guide.fact_check?
-  end
-
-  test "a guide in the ready state can be requested to make more amendments" do
-    guide = FactoryGirl.create(:guide_edition)
-    guide.update_attribute(:state, 'ready')
-    fill_in_parts guide
-
-    login_as "Bob"
-    send_action guide, "Needs more work", "Request amendments", "You need to fix some stuff"
-    filter_for "All"
-    view_filtered_list "Amends needed"
-    assert page.has_content? guide.title
-  end
-
-  test "a guide in the fact check state can be requested to make more amendments" do
-    guide = FactoryGirl.create(:guide_edition)
-    guide.update_attribute(:state, 'fact_check')
-    fill_in_parts guide
-
-    login_as "Bob"
-    send_action guide, "Needs more work", "Request amendments", "You need to fix some stuff"
-    filter_for "All"
-    view_filtered_list "Amends needed"
-    assert page.has_content? guide.title
-  end
-
-  test "can flag guide for review" do
-    guide = FactoryGirl.create(:guide_edition)
-    login_as "Alice"
-
-    assign guide, "Alice"
-    fill_in_parts guide
-    submit_for_review guide
-
-    login_as "Bob"
-    filter_for "All"
-    view_filtered_list "In review"
-
-    assert page.has_content? guide.title
-  end
-
-  test "cannot review own guide" do
-    guide = FactoryGirl.create(:guide_edition)
-    login_as "Alice"
-
-    assign guide, "Alice"
-    fill_in_parts guide
-    submit_for_review guide
-
-    visit_edition guide
-    assert page.has_selector?(".alert-info")
-    refute has_link? "OK for publication"
-  end
-
-  test "cannot be the guide reviewer and assignee" do
-    guide = FactoryGirl.create(:guide_edition)
-    login_as "Alice"
-
-    assign guide, "Bob"
-    fill_in_parts guide
-    submit_for_review guide
-
-    visit_edition guide
-
-    select("Bob", from: "Reviewer")
-
-    save_edition_and_assert_error
-
-    assert page.has_css?(".form-group.has-error li", text: "can't be the assignee")
-  end
-
-  test "can deselect the guide reviewer" do
-    guide = FactoryGirl.create(:guide_edition)
-    login_as "Alice"
-
-    assign guide, "Bob"
-    fill_in_parts guide
-    submit_for_review guide
-
-    visit_edition guide
-
-    select("", from: "Reviewer")
-    save_edition_and_assert_success
-  end
-
-  test "can unassign the guide" do
-    guide = FactoryGirl.create(:guide_edition)
-    login_as "Alice"
-
-    assign guide, "Bob"
-    fill_in_parts guide
-
-    visit_edition guide
-
-    assign guide, ""
-
-    assert_nil guide.assignee
-    assert page.has_select?("Assigned to", selected: "")
-  end
-
-  test "can become the guide reviewer" do
-    guide = FactoryGirl.create(:guide_edition)
-    login_as "Alice"
-
-    assign guide, "Bob"
-    fill_in_parts guide
-    submit_for_review guide
-
-    visit_edition guide
-
-    select("Charlie", from: "Reviewer")
-    save_edition_and_assert_success
-  end
-
-  test "can review another's guide" do
-    guide = FactoryGirl.create(:guide_edition)
-    get_to_review guide, "Alice"
-
-    login_as "Bob"
-    visit_edition guide
-    assert page.has_selector?(".alert-info")
-    assert has_link? "Needs more work"
-    assert has_link? "OK for publication"
-  end
-
-  test "review failed" do
-    guide = FactoryGirl.create(:guide_edition)
-    get_to_review guide, "Alice"
-
-    login_as "Bob"
-    send_action guide, "Needs more work", "Request amendments", "You need to fix some stuff"
-    filter_for "All"
-    view_filtered_list "Amends needed"
-    assert page.has_content? guide.title
-  end
-
-  test "review passed" do
-    guide = FactoryGirl.create(:guide_edition)
-    get_to_review guide, "Alice"
-
-    login_as "Bob"
-    send_action guide, "OK for publication", "OK for publication", "Yup, looks good"
-    filter_for "All"
-    view_filtered_list "Ready"
-    assert page.has_content? guide.title
-  end
-
-  test "can skip fact check" do
-    guide = FactoryGirl.create(:guide_edition)
-    get_to_fact_check guide, "Alice"
-    visit_edition guide
-
-    click_on "Admin"
-    click_on "Skip fact check"
-
-    # This information is not quite correct but it is the current behaviour.
-    # Adding this test as an aid to future improvements
-    assert page.has_content? "Fact check was skipped for this edition."
-
-    filter_for "All"
-    view_filtered_list "Ready"
-    assert page.has_content? guide.title
-    visit_edition guide
-    assert page.has_content? "Request this edition to be amended further."
-    assert page.has_content? "Needs more work"
-  end
-
-  test "can progress from fact check" do
-    guide = FactoryGirl.create(:guide_edition)
-    get_to_fact_check_received guide, "Alice"
-    visit_edition guide
-    send_action guide, "Minor or no changes required", "Approve fact check", "Hurrah!"
-    filter_for "All"
-    view_filtered_list "Ready"
-    assert page.has_content? guide.title
-  end
-
-  test "can go back to fact check from fact check received" do
-    guide = FactoryGirl.create(:guide_edition)
-    get_to_fact_check_received guide, "Alice"
-
-    send_for_fact_check guide
-
-    visit_edition guide
-    assert page.has_css?('.label', text: 'Fact check')
-  end
-
-  test "can create a new edition from the listings screens" do
-    guide = FactoryGirl.create(:guide_edition, state: 'published')
-    filter_for "All"
-    view_filtered_list "Published"
-    click_on "Create new edition"
-    assert page.has_content? "New edition created"
-  end
-
-  test "should link to a newer sibling" do
-    artefact = FactoryGirl.create(:artefact)
-    old_edition = FactoryGirl.create(
-      :guide_edition,
-      panopticon_id: artefact.id,
-      state: "published",
-      version_number: 1
-    )
-    new_edition = FactoryGirl.create(
-      :guide_edition,
-      panopticon_id: artefact.id,
-      state: "draft",
-      version_number: 2
-    )
-    visit_edition old_edition
-    assert page.has_link?(
-      "Edit existing newer edition",
-      href: edition_path(new_edition)
-    ), "Page should have edit link"
-  end
-
-  test "should show an alert if another person has created a newer edition" do
-    guide = FactoryGirl.create(:guide_edition, state: 'published')
-    filter_for "All"
-    view_filtered_list "Published"
-
-    # Simulate that someone has clicked on 'Create new edition'
-    # while current user has been viewing the list of published editions
-    new_edition = guide.build_clone(GuideEdition)
-    new_edition.save
-
-    # Current user now decides to click the button
-    click_on "Create new edition"
-
-    assert page.has_content?("Another person has created a newer edition")
-    assert page.has_css?('.label', text: 'Published')
   end
 end
