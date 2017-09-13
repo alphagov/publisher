3c0846de50cb5d8f767beb844f09ad37c1d814ad
Dennis Tsiang <dennis.tsiang15@imperial.ac.uk>
2017-09-13 16:05:35 +0100

    Add jwth_auth_secret to development environment
    This allows you to generate the reports in development environment.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,8 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  config.jwt_auth_secret = '123'
+
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = false
