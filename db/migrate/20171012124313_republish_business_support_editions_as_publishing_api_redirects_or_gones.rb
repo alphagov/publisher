c425f0ef80e7ac41f1f6a9c19fd8be3b0d87b506
Murray Steele <muz@h-lame.com>
2017-10-12 16:07:39 +0100

    Deal with live business support artefacts
    Our goal is to use the `UnpublishService` to archive all these artefacts
    and send an unpublish notice with a redirect to the publishing-api.  All
    but 3 of these artefacts have already been redirected via router-data and
    so we can get the redirect url from there.  For the 3 that haven't one we
    need to treat specially, 'jobcentre-plus-vacancy-filling-system-uk', the
    other two we can redirect to the new business finance support finder route
    at '/business-finance-support'.  Dealing with
    'jobcentre-plus-vacancy-filling-system-uk' is more complex because it has
    already been turned into a redirect with a new content_id via
    short-url-manager.  Instead of issuing a redirect we check that the state
    of the world is as we expect (there's a redirect in router, publishing-api
    doesn't think our content_id owns the path, the object that does is a
    redirect) and halt the migration if it's not.  We also halt the migration
    if the state of the world is not as we expect for any of the other live
    artefacts (there's no route in router, publishing-api thinks we own the
    path, and the object in publishing-api is a business_support document).

--- db/migrate/20171012124313_republish_business_support_editions_as_publishing_api_redirects_or_gones.rb
+++ db/migrate/20171012124313_republish_business_support_editions_as_publishing_api_redirects_or_gones.rb
@@ -10,6 +10,52 @@ class RepublishBusinessSupportEditionsAsPublishingApiRedirectsOrGones < Mongoid:
     handle_archived_business_support_artefacts(business_support_by_state['archived'])
   end
 
+  def self.handle_live_business_support_artefacts(live_artefacts)
+    # 1. Exclude 'jobcentre-plus-vacancy-filling-system-uk' as it's already
+    #    been redirected by short-url-manager
+    live_artefacts = handle_jobcentre_plus_vacancy_filling_system_uk(live_artefacts)
+
+    # 2. Set the appropriate redirects from router-data as the redirect_uri on
+    #    the Artefact or set general purpose redirect_uri of
+    #    /business-finance-support if it's not currently a redirect
+    say_with_time "Checking #{live_artefacts.size} live artefacts status in router & publishing-api" do
+      live_artefacts.each do |live_artefact|
+        content_id = publishing_api.lookup_content_id(base_path: "/#{live_artefact.slug}", exclude_unpublishing_types: [], exclude_document_types: [])
+        raise "Didn't expect publishing-api to think some other content_id is the owner of a live artefact's path (#{live_artefact.slug}, #{live_artefact.content_id}, #{content_id})" if live_artefact.content_id != content_id
+        content_item = publishing_api.get_content(content_id).to_hash
+        raise "Didn't expect publishing-api content for live artefact not to be a business_support (#{live_artefact.slug}, #{live_artefact.content_id}, #{content_item['document_type']})" if content_item['document_type'] != 'business_support'
+        current_route = router_response(live_artefact)
+        raise "Didn't exxpect any live artefacts not to have a route in router-api (#{live_artefact.slug}, #{live_artefact.content_id})" if current_route.nil?
+
+        if current_route['handler'] == 'redirect'
+          live_artefact.redirect_url = current_route['redirect_to']
+        else
+          live_artefact.redirect_url = '/business-finance-support'
+        end
+      end
+    end
+
+    # 3. Use UnpublishService to generate a publishing-api redirect
+    say_with_time "Unpublishing #{live_artefacts.size} live artefacts as redirects via publishing-api" do
+      live_artefacts.each do |live_artefact|
+        UnpublishService.call(live_artefact, nil, live_artefact.redirect_url)
+      end
+    end
+  end
+
+  def self.handle_jobcentre_plus_vacancy_filling_system_uk(live_artefacts)
+    jobcentre_plus_vacany_fill_system_uk = live_artefacts.detect { |x| x.slug = 'jobcentre-plus-vacancy-filling-system-uk'}
+    raise "Didn't expect 'jobcentre-plus-vacancy-filling-system-uk' to be missing from live business support artefacts" if jobcentre_plus_vacany_fill_system_uk.nil?
+
+    say_with_time "Checking 'jobcentre-plus-vacancy-filling-system-uk' is already migrated by short-url-manager" do
+      content_id = publishing_api.lookup_content_id(base_path: "/jobcentre-plus-vacancy-filling-system-uk", exclude_unpublishing_types: [], exclude_document_types: [])
+      raise "Didn't expect publishing-api to think artefact is the content_id for /jobcentre-plus-vacancy-filling-system-uk path" if jobcentre_plus_vacany_fill_system_uk.content_id == content_id
+      content_item = publishing_api.get_content(content_id).to_hash
+      raise "Didn't expect publishing-api content for /jobcentre_plus_vacany_fill_system_uk not to be a redirect" if content_item['document_type'] != 'redirect'
+    end
+    live_artefacts.reject { |x| x.slug == jobcentre_plus_vacany_fill_system_uk.slug }
+  end
+
   def self.handle_draft_business_support_artefacts(draft_artefacts)
     raise "Didn't expect any editions for draft artefacts" if draft_artefacts.any? { |a| a.latest_edition.present? }
     raise "Didn't expect any entries in router for draft artefacts" if draft_artefacts.any? { |a| router_response(a).present? }
