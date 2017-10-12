f841d7232ddac96d050f98ed9ac79420d350fb1e
Murray Steele <muz@h-lame.com>
2017-10-12 14:49:23 +0100

    Deal with draft business support artefacts
    In our investigations with the data, none of these has any editions, an
    entry in router for their slug, or an entry in publishing-api for their
    content_id.  If this is truly the case then we can just delete them and
    pretend they never existed.  We guard against the possibility that this
    isn't the case with live data and halt the migration before destroying
    all the artefacts.  Most likely these were created in panopticon but
    never saved in publisher so have been hanging around in a limbo state
    for ages.

--- db/migrate/20171012124313_republish_business_support_editions_as_publishing_api_redirects_or_gones.rb
+++ db/migrate/20171012124313_republish_business_support_editions_as_publishing_api_redirects_or_gones.rb
@@ -10,6 +10,18 @@ class RepublishBusinessSupportEditionsAsPublishingApiRedirectsOrGones < Mongoid:
     handle_archived_business_support_artefacts(business_support_by_state['archived'])
   end
 
+  def self.handle_draft_business_support_artefacts(draft_artefacts)
+    raise "Didn't expect any editions for draft artefacts" if draft_artefacts.any? { |a| a.latest_edition.present? }
+    raise "Didn't expect any entries in router for draft artefacts" if draft_artefacts.any? { |a| router_response(a).present? }
+    raise "Didn't expect any entries in publishing-api for draft artefacts" if draft_artefacts.any? { |a| publishing_api_response(a).present? }
+
+    Artefact.skip_callback(:destroy, :before, :discard_publishing_api_draft)
+    say_with_time "Removing #{draft_artefacts.size} draft artefacts" do
+      draft_artefacts.each { |a| a.destroy }
+    end
+    Artefact.set_callback(:destroy, :before, :discard_publishing_api_draft)
+  end
+
   def self.router
     @router ||= GdsApi::Router.new(Plek.find('router-api'))
   end
