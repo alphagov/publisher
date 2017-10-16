055a6136e80484cc01f562b035c00ce297a1a1c3
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-10-16 18:57:13 +0100

    Correct ASSET_MANAGER_BEARER_TOKEN env var
    This was set to the wrong name:
    https://github.com/alphagov/govuk-puppet/blob/master/modules/govuk/manifests/apps/publisher.pp#L141
    
    Also removed lines about this file being replaced since it won't be
    anymore soon.

--- config/initializers/gds_api.rb
+++ config/initializers/gds_api.rb
@@ -1,6 +1,3 @@
-# This file is overwritten on deploy (see alphagov-deployment), so be careful
-# about relying on this content
-
 require 'gds_api/base'
 require 'gds_api/asset_manager'
 require 'plek'
@@ -12,4 +9,7 @@ GdsApi::Base.logger = Logger.new(Rails.root.join("log/#{Rails.env}.api_client.lo
 # be overwritten with the correct details on deploy.
 # See README.md for details of how to generate a bearer token.
 
-Attachable.asset_api_client = GdsApi::AssetManager.new(Plek.current.find('asset-manager'), bearer_token: ENV['PUBLISHER_ASSET_MANAGER_CLIENT_BEARER_TOKEN'] || 'also_set_at_deploy_time',)
+Attachable.asset_api_client = GdsApi::AssetManager.new(
+  Plek.current.find('asset-manager'),
+  bearer_token: ENV['ASSET_MANAGER_BEARER_TOKEN'] || 'example',
+)
