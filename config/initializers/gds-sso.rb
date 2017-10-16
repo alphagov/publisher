bffaef4da3d0eecfab3114bf1a0d2827a15bdb7e
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-10-16 18:52:44 +0100

    Fix Oauth env vars
    These don't have a PUBLISHER prefix:
    https://github.com/alphagov/govuk-puppet/blob/master/modules/govuk/manifests/apps/publisher.pp#L149-L154

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -1,6 +1,6 @@
 GDS::SSO.config do |config|
   config.user_model   = "User"
-  config.oauth_id     = ENV['PUBLISHER_OAUTH_ID'] || "abcdefghjasndjkasndpublisher"
-  config.oauth_secret = ENV['PUBLISHER_OAUTH_SECRET'] || "secret"
+  config.oauth_id     = ENV['OAUTH_ID'] || "abcdefghjasndjkasndpublisher"
+  config.oauth_secret = ENV['OAUTH_SECRET'] || "secret"
   config.oauth_root_url = Plek.current.find("signon")
 end
