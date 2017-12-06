dd875f03af9608030503e50e0c63da7b4651b4ac
Adrian Clay <adrian@madetech.com>
2017-12-06 15:41:04 +0000

    Allow for mailer and secure_cookie to be disabled in production
    The publishing-e2e-tests are being built in production mode now,
    and as part of that initial work we need to disable secure cookie
    due to not using SSL and sending emails due to no mail server.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,3 +101,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   # config.active_record.dump_schema_after_migration = false
 end
+
+if ENV['DISABLE_EMAIL']
+  ActionMailer::Base.perform_deliveries = false
+end
