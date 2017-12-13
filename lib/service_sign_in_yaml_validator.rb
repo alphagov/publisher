0b19b66a157b47592a0282e9993b993720522f9a
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2017-12-13 16:33:27 +0000

    Log an error if YAML file path is invalid

--- lib/service_sign_in_yaml_validator.rb
+++ lib/service_sign_in_yaml_validator.rb
@@ -1,15 +1,27 @@
 class ServiceSignInYamlValidator
+  attr_reader :yaml_file
+
   def initialize(file_name)
     @file_name = file_name
+    @errors = []
   end
 
   def validate
-    load_yaml_file
+    valid? ? @yaml_file : @errors
   end
 
 private
 
+  def valid?
+    load_yaml_file
+  end
+
   def load_yaml_file
-    YAML.load_file(@file_name)
+    begin
+      @yaml_file = YAML.load_file(@file_name)
+    rescue SystemCallError
+      @errors << "Invalid file path: #{@file_name}"
+      return false
+    end
   end
 end
--- test/unit/lib/service_sign_in_yaml_validator_test.rb
+++ test/unit/lib/service_sign_in_yaml_validator_test.rb
@@ -9,6 +9,10 @@ class ServiceSignInYamlValidatorTest < ActiveSupport::TestCase
     "lib/service_sign_in/example.yaml"
   end
 
+  def invalid_file_path
+    "invalid/file/path.yaml"
+  end
+
   def content
     @file ||= YAML.load_file(valid_yaml_file)
   end
@@ -18,6 +22,13 @@ class ServiceSignInYamlValidatorTest < ActiveSupport::TestCase
       should "return the YAML file as a hash" do
         validator = service_sign_in_yaml_validator(valid_yaml_file)
         assert_equal content, validator.validate
+      end
+    end
+
+    context "when an invalid file path is provided" do
+      should "log an 'Invalid file path' error" do
+        validator = service_sign_in_yaml_validator(invalid_file_path)
+        assert_includes validator.validate, "Invalid file path: #{invalid_file_path}"
       end
     end
   end
