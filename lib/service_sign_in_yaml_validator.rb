88f104f1e854b5a31fa2920be649d3eec5e32773
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2017-12-13 17:25:03 +0000

    Check for presence of required top level field names
    We check for the presence of top level fields that are required in the provided
    YAML file in order to create a `service_sign_in` format and log an error if any
    are absent.

--- lib/service_sign_in_yaml_validator.rb
+++ lib/service_sign_in_yaml_validator.rb
@@ -1,6 +1,9 @@
 class ServiceSignInYamlValidator
   attr_reader :yaml_file
 
+  REQUIRED_TOP_LEVEL_FIELDS =
+    %w(change_note choose_sign_in locale start_page_slug update_type).freeze
+
   def initialize(file_name)
     @file_name = file_name
     @errors = []
@@ -14,6 +17,8 @@ private
 
   def valid?
     load_yaml_file
+    return unless @yaml_file.present?
+    check_for_top_level_required_fields
     @errors.empty?
   end
 
@@ -25,6 +30,12 @@ private
       end
     rescue SystemCallError
       @errors << "Invalid file path: #{@file_name}"
+    end
+  end
+
+  def check_for_top_level_required_fields
+    REQUIRED_TOP_LEVEL_FIELDS.each do |field|
+      @errors << "Missing field: #{field}" unless @yaml_file.has_key?(field)
     end
   end
 end
--- test/unit/lib/service_sign_in_yaml_validator_test.rb
+++ test/unit/lib/service_sign_in_yaml_validator_test.rb
@@ -21,6 +21,14 @@ class ServiceSignInYamlValidatorTest < ActiveSupport::TestCase
     @file ||= YAML.load_file(valid_yaml_file)
   end
 
+  def required_top_level_fields
+    ServiceSignInYamlValidator::REQUIRED_TOP_LEVEL_FIELDS
+  end
+
+  def missing_top_level_fields
+    "test/fixtures/service_sign_in/missing_top_level_fields.yaml"
+  end
+
   context "#validate" do
     context "when a YAML file is valid" do
       should "return the YAML file as a hash" do
@@ -40,6 +48,15 @@ class ServiceSignInYamlValidatorTest < ActiveSupport::TestCase
       should "log an 'Invalid file type' error" do
         validator = service_sign_in_yaml_validator(invalid_file_type)
         assert_includes validator.validate, "Invalid file type"
+      end
+    end
+
+    context "when a required top level field is missing in the YAML file" do
+      should "log a 'Missing field: field_name' error" do
+        validator = service_sign_in_yaml_validator(missing_top_level_fields)
+        required_top_level_fields.each do |field|
+          assert_includes validator.validate, "Missing field: #{field}"
+        end
       end
     end
   end
