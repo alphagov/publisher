d10ea6433f43aff2acd9a6f7032a3ad799caa49f
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2017-12-14 11:26:18 +0000

    Check for missing choose_sign_in fields
    We add a check for missing required `choose_sign_in` fields in the YAML file.
    We also check if `choose_sign_in` is present in the YAML file before doing
    this, and if it is then we check it's a hash.

--- lib/service_sign_in_yaml_validator.rb
+++ lib/service_sign_in_yaml_validator.rb
@@ -3,6 +3,7 @@ class ServiceSignInYamlValidator
 
   REQUIRED_TOP_LEVEL_FIELDS =
     %w(change_note choose_sign_in locale start_page_slug update_type).freeze
+  REQUIRED_CHOOSE_SIGN_IN_FIELDS = %w(options slug title).freeze
 
   def initialize(file_name)
     @file_name = file_name
@@ -19,6 +20,7 @@ private
     load_yaml_file
     return unless @yaml_file.present?
     check_for_top_level_required_fields
+    check_for_choose_sign_in_required_fields if choose_sign_in.present?
     @errors.empty?
   end
 
@@ -37,5 +39,23 @@ private
     REQUIRED_TOP_LEVEL_FIELDS.each do |field|
       @errors << "Missing field: #{field}" unless @yaml_file.has_key?(field)
     end
+  end
+
+  def check_for_choose_sign_in_required_fields
+    error_message = "Missing choose_sign_in field: "
+    unless choose_sign_in.is_a?(Hash)
+      @errors << error_message + REQUIRED_CHOOSE_SIGN_IN_FIELDS.join(", ")
+      return
+    end
+
+    REQUIRED_CHOOSE_SIGN_IN_FIELDS.each do |field|
+      unless choose_sign_in.has_key?(field)
+        @errors << error_message + field
+      end
+    end
+  end
+
+  def choose_sign_in
+    @yaml_file["choose_sign_in"]
   end
 end
--- test/unit/lib/service_sign_in_yaml_validator_test.rb
+++ test/unit/lib/service_sign_in_yaml_validator_test.rb
@@ -25,8 +25,20 @@ class ServiceSignInYamlValidatorTest < ActiveSupport::TestCase
     ServiceSignInYamlValidator::REQUIRED_TOP_LEVEL_FIELDS
   end
 
+  def required_choose_sign_in_fields
+    ServiceSignInYamlValidator::REQUIRED_CHOOSE_SIGN_IN_FIELDS
+  end
+
   def missing_top_level_fields
     "test/fixtures/service_sign_in/missing_top_level_fields.yaml"
+  end
+
+  def missing_choose_sign_in_fields
+    "test/fixtures/service_sign_in/missing_choose_sign_in_fields.yaml"
+  end
+
+  def no_choose_sign_in_fields
+    "test/fixtures/service_sign_in/no_choose_sign_in_fields.yaml"
   end
 
   context "#validate" do
@@ -57,6 +69,23 @@ class ServiceSignInYamlValidatorTest < ActiveSupport::TestCase
         required_top_level_fields.each do |field|
           assert_includes validator.validate, "Missing field: #{field}"
         end
+      end
+    end
+
+    context "when a required choose_sign_in field is missing in the YAML file" do
+      should "log a 'Missing choose_sign_in field: field_name' error" do
+        validator = service_sign_in_yaml_validator(missing_choose_sign_in_fields)
+        required_choose_sign_in_fields.each do |field|
+          assert_includes validator.validate, "Missing choose_sign_in field: #{field}"
+        end
+      end
+    end
+
+    context "when choose_sign_in is not a hash of fields" do
+      should "log a 'Missing choose_sign_in field: title, slug, options' error" do
+        validator = service_sign_in_yaml_validator(no_choose_sign_in_fields)
+        assert_includes validator.validate,
+          "Missing choose_sign_in field: #{required_choose_sign_in_fields.join(', ')}"
       end
     end
   end
