f579a4648002ba2478485103925c6e50da183b1f
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2017-12-21 14:00:45 +0000

    Update Service Sign In Unpublish Service to handle redirects

--- app/services/service_sign_in_unpublish_service.rb
+++ app/services/service_sign_in_unpublish_service.rb
@@ -1,11 +1,46 @@
 class ServiceSignInUnpublishService
   class << self
-    def call(content_id, locale)
+    def call(content_id, locale, redirect_path: nil)
+      @content_id = content_id
+      if redirect_path.present?
+        unpublish_redirect(locale, redirect_path)
+      else
+        unpublish_gone(locale)
+      end
+    end
+
+  private
+
+    def base_path
+      content_item["base_path"]
+    end
+
+    def content_item
+      @content_item ||= Services.publishing_api.get_content(@content_id)
+    end
+
+    def unpublish_gone(locale)
       Services.publishing_api.unpublish(
-        content_id,
+        @content_id,
         locale: locale,
         type: "gone",
         discard_drafts: true,
+      )
+    end
+
+    def unpublish_redirect(locale, redirect_path)
+      Services.publishing_api.unpublish(
+        @content_id,
+        locale: locale,
+        type: "redirect",
+        discard_drafts: true,
+        redirects: [
+          {
+            path: base_path,
+            type: "prefix",
+            destination: redirect_path,
+          },
+        ]
       )
     end
   end
--- test/unit/services/service_sign_in_unpublish_service_test.rb
+++ test/unit/services/service_sign_in_unpublish_service_test.rb
@@ -17,6 +17,28 @@ class ServiceSignInUnpublishServiceTest < ActiveSupport::TestCase
     ServiceSignInUnpublishService.call(content_id, locale)
   end
 
+  should "unpublish a content item with type 'redirect'" do
+    Services.publishing_api.expects(:unpublish).with(
+      content_id,
+      locale: locale,
+      type: "redirect",
+      discard_drafts: true,
+      redirects: [
+        {
+          path: base_path,
+          type: "prefix",
+          destination: redirect_path
+        }
+      ]
+    )
+
+    ServiceSignInUnpublishService.call(
+      content_id,
+      locale,
+      redirect_path: redirect_path
+    )
+  end
+
   def content_id
     "a-content-id"
   end
@@ -25,9 +47,18 @@ class ServiceSignInUnpublishServiceTest < ActiveSupport::TestCase
     "cy"
   end
 
+  def redirect_path
+    "/alternative/path"
+  end
+
+  def base_path
+    "/path/sign-in"
+  end
+
   def content_item
     {
       "locale" => locale,
+      "base_path" => base_path,
     }
   end
 end
