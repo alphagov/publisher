b337df00e0c7befccb49737c42b53a1e5dd6c125
Seb Ashton <seb@sebashton.com>
2018-01-05 11:20:09 +0000

    Add link_checker_api to Services

--- app/lib/services.rb
+++ app/lib/services.rb
@@ -1,15 +1,20 @@
 require "gds_api/publishing_api_v2"
 require "gds_api/calendars"
+require "gds_api/link_checker_api"
 
 module Services
   def self.publishing_api
     @publishing_api ||= GdsApi::PublishingApiV2.new(
-      Plek.new.find('publishing-api'),
-      bearer_token: ENV['PUBLISHING_API_BEARER_TOKEN'] || 'example'
+      Plek.new.find("publishing-api"),
+      bearer_token: ENV["PUBLISHING_API_BEARER_TOKEN"] || "example"
     )
   end
 
   def self.calendars
-    @calendars ||= GdsApi::Calendars.new(Plek.new.find('calendars'))
+    @calendars ||= GdsApi::Calendars.new(Plek.new.find("calendars"))
+  end
+
+  def self.link_checker_api
+    @link_checker_api ||= GdsApi::LinkCheckerApi.new(Plek.new.find("link-checker-api"))
   end
 end
