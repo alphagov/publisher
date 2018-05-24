cec391999d77779288f274c3cb3d579ce5506cd5
Kevin Dew <kevindew@me.com>
2018-05-24 17:50:46 +0100

    Add option to UpdateWorker to also publish
    This allows the UpdateWorker to run an update and then a publish as a
    synchronous pair of actions.
    
    This is intended to replace the process where UpdateWorker and
    PublishWorker are both ran asynchronously as this leads to out of order
    issues where the UpdateWorker runs the UpdateService after the
    PublishWorker has run PublishService. This out of ordering leads to an
    out of document being published and a draft being left in Publishing
    API.

--- app/workers/update_worker.rb
+++ app/workers/update_worker.rb
@@ -1,8 +1,9 @@
 class UpdateWorker
   include Sidekiq::Worker
 
-  def perform(edition_id)
+  def perform(edition_id, publish = false)
     edition = Edition.find(edition_id)
     UpdateService.call(edition)
+    PublishService.call(edition) if publish
   end
 end
--- test/unit/workers/update_worker_test.rb
+++ test/unit/workers/update_worker_test.rb
@@ -5,8 +5,18 @@ class UpdateWorkerTest < ActiveSupport::TestCase
     should "call the UpdateService" do
       edition = FactoryBot.create(:edition)
       UpdateService.expects(:call).with(edition)
+      PublishService.expects(:call).never
 
       UpdateWorker.new.perform(edition.id)
+    end
+
+    context "when publish is true" do
+      should "call the PublishService" do
+        edition = FactoryBot.create(:edition)
+
+        PublishService.expects(:call).with(edition)
+        UpdateWorker.new.perform(edition.id, true)
+      end
     end
   end
 end
