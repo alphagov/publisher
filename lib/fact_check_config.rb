8828e05967e92b9026bc5968fb145d7d6a8a9ef7
Benjamin Eskola <benjamin.eskola@digital.cabinet-office.gov.uk>
2020-05-14 09:36:57 +0100

    Write the regex more directly/clearly

--- lib/fact_check_config.rb
+++ lib/fact_check_config.rb
@@ -9,7 +9,7 @@ class FactCheckConfig
     @reply_to_id = reply_to_id
 
     @subject_prefix = subject_prefix.present? ? subject_prefix + "-" : ""
-    subject_format = "\\[#{@subject_prefix}(?<id>[0-9a-f]+)\\]"
+    @subject_pattern = /\[#{@subject_prefix}(?<id>[0-9a-f]+)\]/
 
     @address_prefix, @address_suffix = address_format.split("{id}")
     @address_pattern = Regexp.new(
@@ -18,10 +18,6 @@ class FactCheckConfig
       "(.+?)" +
       Regexp.escape(@address_suffix) +
       '\Z',
-    )
-
-    @subject_pattern = Regexp.new(
-      subject_format,
     )
   end
 
