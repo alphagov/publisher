df08dbeea077f8e37acc48399c57dbe19e6be4e5
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2020-07-07 17:35:54 +0100

    Remove db:migrations from publisher
    We don't need these data migrations anymore, and prefer developers
    to use temporary rake tasks when dealing with schemaless mongodb
    databases.

--- db/migrate/20131216171639_import_imminence_facet_data.rb
+++
@@ -1,5 +0,0 @@
-class ImportImminenceFacetData < Mongoid::Migration
-  def self.up
-    Rake::Task["business_support_content:import_facet_data"].invoke
-  end
-end
--- db/migrate/20141217161732_remove_uses_government_gateway_field.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUsesGovernmentGatewayField < Mongoid::Migration
-  def self.up
-    Edition.all.each { |edition| edition.unset(:uses_government_gateway) }
-  end
-
-  def self.down
-    # No down as this is a destructive action.
-  end
-end
