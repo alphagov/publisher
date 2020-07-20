8a22b366c3963bbb771231b710665d6c9f799f10
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2020-07-20 15:00:49 +0100

    Use GdsApi.imminence
    Avoids needing to maintain a `Services.imminence` method.

--- app/lib/services.rb
+++ app/lib/services.rb
@@ -1,6 +1,5 @@
 require "gds_api/publishing_api"
 require "gds_api/link_checker_api"
-require "gds_api/imminence"
 
 module Services
   def self.publishing_api
@@ -14,12 +13,6 @@ module Services
     @link_checker_api ||= GdsApi::LinkCheckerApi.new(
       Plek.new.find("link-checker-api"),
       bearer_token: ENV["LINK_CHECKER_API_BEARER_TOKEN"],
-    )
-  end
-
-  def self.imminence
-    @imminence ||= GdsApi::Imminence.new(
-      Plek.new.find("imminence"),
     )
   end
 end
