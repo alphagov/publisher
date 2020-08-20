f1e8782661eabff1fb3e64b00543052a2c1ad92a
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2020-08-20 13:32:39 +0100

    Allow serving assets in production environment
    Usually Nginx will serve static assets, bypassing rails. However, to test out
    this in ECS we'll let rails serve the static assets.
    
    This is a hack. In future we'll use something like AssetSync to push assets
    to S3 and serve assets from there.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
