1ad364cdd614fe36923ac8d447bea45c92fe9f87
Benjamin Eskola <benjamin.eskola@digital.cabinet-office.gov.uk>
2020-09-25 14:34:11 +0100

    Test FactCheckEmailHandler#process_message with the ID in the body not the subject

--- test/integration/fact_check_email_test.rb
+++ test/integration/fact_check_email_test.rb
@@ -157,6 +157,17 @@ class FactCheckEmailTest < ActionDispatch::IntegrationTest
     assert message.is_marked_for_delete?
   end
 
+  test "should look for fact-check body field" do
+    edition = FactoryBot.create(:answer_edition, state: "fact_check")
+    message = fact_check_mail_for(edition, subject: "Fact Checked", body: "[#{edition.id}]")
+
+    Mail.stubs(:all).yields(message)
+
+    handler = FactCheckEmailHandler.new(fact_check_config)
+
+    assert handler.process_message(message)
+  end
+
   test "should invoke the supplied block after each message" do
     answer1 = FactoryBot.create(:answer_edition, state: "fact_check")
     answer2 = FactoryBot.create(
