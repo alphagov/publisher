c7314a6488e6ae8808dfca812012bd154d5df4c0
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2020-10-01 16:38:33 +0100

    Require govuk_editor for skipping fact checks

--- app/lib/govuk_content_models/action_processors/skip_fact_check_processor.rb
+++ app/lib/govuk_content_models/action_processors/skip_fact_check_processor.rb
@@ -1,6 +1,9 @@
 module GovukContentModels
   module ActionProcessors
     class SkipFactCheckProcessor < BaseProcessor
+      def process?
+        actor.govuk_editor?
+      end
     end
   end
 end
