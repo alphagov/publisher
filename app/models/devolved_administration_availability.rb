0af687ce92be652a464dbffce1dc4b1518448ab4
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2021-05-27 15:30:22 +0100

    Model validation for DevolvedAdministrationAvailability
    Adding error messaging to display to the user will be done as part of a following PR.

--- app/models/devolved_administration_availability.rb
+++ app/models/devolved_administration_availability.rb
@@ -4,4 +4,26 @@ class DevolvedAdministrationAvailability
   embedded_in :local_transaction_edition
   field :type, type: String, default: "local_authority_service"
   field :alternative_url, type: String
+
+  validates :type, inclusion: { in: %w[local_authority_service devolved_administration_service unavailable] }
+  validates :alternative_url, presence: true, if: -> { devolved_administration_service? }
+  validate :alternative_url_format
+
+  def alternative_url_format
+    return if alternative_url.blank?
+
+    uri = begin
+      URI.parse(alternative_url)
+    rescue URI::Error
+      nil
+    end
+
+    unless uri.is_a?(URI::HTTPS)
+      errors.add(:alternative_url, "must be a full URL, starting with https://")
+    end
+  end
+
+  def devolved_administration_service?
+    type == "devolved_administration_service"
+  end
 end
