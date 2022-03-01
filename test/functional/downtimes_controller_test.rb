cdec4086ada9998963f7c5f94c056df71caa03c1
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2022-03-01 12:09:40 +0000

    Ensure the publications index page to be accessible
    This does the following:
    
    - Moves the H2 into a caption
    - Uses a label and hint text instead of a placeholder for the
    table fitler
    - Enforces sequential headers

--- test/functional/downtimes_controller_test.rb
+++ test/functional/downtimes_controller_test.rb
@@ -13,9 +13,9 @@ class DowntimesControllerTest < ActionController::TestCase
       get :index
 
       assert_response :ok
-      assert_select "h4.publication-table-title", count: 0, text: unpublished_transaction_edition.title
+      assert_select "h3.publication-table-title", count: 0, text: unpublished_transaction_edition.title
       transaction_editions.each do |edition|
-        assert_select "h4.publication-table-title", text: edition.title
+        assert_select "h3.publication-table-title", text: edition.title
       end
     end
 
