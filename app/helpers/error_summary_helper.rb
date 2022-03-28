0ba70b0996278ac72074dc33fdcb6a16552ea014
Becka Lelew <becka.lelew@digital.cabinet-office.gov.uk>
2022-03-28 17:25:53 +0100

    Renamings according to PR comments [to be squashed]

--- app/helpers/error_summary_helper.rb
+++ app/helpers/error_summary_helper.rb
@@ -2,34 +2,35 @@ module ErrorSummaryHelper
   def errors_to_display(edition)
     errors_with_hrefs = case edition
                         when SimpleSmartAnswerEdition
-                          return_smart_answer_errors_and_hrefs(edition)
+                          smart_answer_errors(edition)
                         when GuideEdition
-                          return_guide_errors_and_hrefs(edition)
+                          guide_errors(edition)
                         else
-                          return_top_level_errors_and_hrefs(edition)
+                          edition_errors(edition)
                         end
     errors_with_hrefs.map { |error, href| [error.message, href] }
   end
 
 private
 
-  def return_smart_answer_errors_and_hrefs(smart_answer)
-    all_errors_and_hrefs = return_top_level_errors_and_hrefs(smart_answer) +
+  def smart_answer_errors(smart_answer)
+    all_errors_and_hrefs = edition_errors(smart_answer) +
       get_node_errors_and_hrefs(smart_answer.nodes) +
-      get_option_errors_and_href(smart_answer.nodes.flat_map(&:options))
+      get_option_errors(smart_answer.nodes.flat_map(&:options))
 
-    # Here we reject any errors with a type of 'invalid' as these will be because of invalid nodes or options and the useful error message will exist at that level
-    all_errors_and_hrefs.reject { |error, _| error.type == :invalid }
+    # Reject errors with an attribute of 'nodes' or 'options' as these are because of the nested structure of guides and will be duplicated so not helpful
+    # Errors with an attribute of 'slug' will be slugs which are derived, rather than being input explicitly, so are not useful error messages
+    all_errors_and_hrefs.reject { |error, _| %i[nodes options slug].include?(error.attribute) }
   end
 
-  def return_guide_errors_and_hrefs(guide)
-    all_errors_and_hrefs = return_top_level_errors_and_hrefs(guide) + get_part_errors_and_href(guide.parts)
+  def guide_errors(guide)
+    all_errors_and_hrefs = edition_errors(guide) + get_part_errors(guide.parts)
 
     # Reject errors with an attribute of 'parts' as these are because of the nested structure of guides and will be duplicated so not helpful
     all_errors_and_hrefs.reject { |error, _| error.attribute == :parts }
   end
 
-  def return_top_level_errors_and_hrefs(edition)
+  def edition_errors(edition)
     edition.errors.map { |error| [error, "#edition_#{error.attribute}"] }
   end
 
@@ -37,26 +38,26 @@ private
     href_constructor = proc do |node, attribute|
       "#edition_nodes_attributes_#{node.order - 1}_#{attribute}"
     end
-    get_errors_and_hrefs_from_nested_objects(nodes, &href_constructor)
+    errors_from_nested_objects(nodes, &href_constructor)
   end
 
-  def get_option_errors_and_href(options)
+  def get_option_errors(options)
     href_constructor = proc do |option, attribute|
       node = option.node
       attr = attribute == :next_node ? "node" : attribute
       "#edition_nodes_attributes_#{node.order - 1}_options_attributes_#{node.options.find_index(option)}_#{attr}"
     end
-    get_errors_and_hrefs_from_nested_objects(options, &href_constructor)
+    errors_from_nested_objects(options, &href_constructor)
   end
 
-  def get_part_errors_and_href(parts)
+  def get_part_errors(parts)
     href_constructor = proc do |part, attribute|
       "#edition_parts_attributes_#{part.guide_edition.parts.find_index(part)}_#{attribute}"
     end
-    get_errors_and_hrefs_from_nested_objects(parts, &href_constructor)
+    errors_from_nested_objects(parts, &href_constructor)
   end
 
-  def get_errors_and_hrefs_from_nested_objects(nested_objects, &href_constructor)
+  def errors_from_nested_objects(nested_objects, &href_constructor)
     nested_objects
       .select { |object| object.errors.present? }
       .flat_map { |object| object.errors.map { |error| [error, href_constructor.call(object, error.attribute)] } }
--- test/unit/helpers/error_summary_helper_test.rb
+++ test/unit/helpers/error_summary_helper_test.rb
@@ -7,25 +7,26 @@ class ErrorSummaryHelperTest < ActionView::TestCase
     GuideEdition.new(title: title, parts: parts, panopticon_id: "Some_id")
   end
 
-  # rubocop:disable Rails/SaveBang
-  test "errors_to_display_hash returns useful error messages and correct hrefs for invalid fields for a guide" do
+  test "For a Guide, errors_to_display returns useful error messages and correct hrefs for invalid fields" do
     valid_part = Part.new(title: "some part", slug: "another-slug")
     invalid_part_1 = Part.new(title: "", slug: "invalid slug with spaces")
     invalid_part_2 = Part.new(title: "valid title", slug: "another invalid slug")
 
     guide_with_invalid_data = guide_with_title_and_parts("", [valid_part, invalid_part_1, invalid_part_2])
 
-    guide_with_invalid_data.save
+    guide_with_invalid_data.valid?
 
-    assert_equal errors_to_display(guide_with_invalid_data), [
+    expected_errors = [
       ["Enter a title", "#edition_title"],
       ["Enter a title for Part 2", "#edition_parts_attributes_1_title"],
       ["Slug can only consist of lower case characters, numbers and hyphens", "#edition_parts_attributes_1_slug"],
       ["Slug can only consist of lower case characters, numbers and hyphens", "#edition_parts_attributes_2_slug"],
     ]
+
+    assert_equal expected_errors, errors_to_display(guide_with_invalid_data)
   end
 
-  test "errors_to_display_hash returns useful error messages and correct hrefs for invalid fields for a SimpleSmartAnswer" do
+  test "For a SimpleSmartAnswer, errors_to_display returns useful error messages and correct hrefs for invalid fields" do
     valid_outcome_node = SimpleSmartAnswerEdition::Node.new(kind: "outcome", title: "Node 1", slug: "node-1", order: 1)
     outcome_node_without_title = SimpleSmartAnswerEdition::Node.new(kind: "outcome", title: "", slug: "node-2", order: 2)
 
@@ -36,7 +37,7 @@ class ErrorSummaryHelperTest < ActionView::TestCase
 
     simple_smart_answer = SimpleSmartAnswerEdition.new(title: "", panopticon_id: "Some_id", nodes: [valid_outcome_node, outcome_node_without_title, question_node_with_valid_and_invalid_options])
 
-    simple_smart_answer.save
+    simple_smart_answer.valid?
 
     expected_errors = [
       ["Enter a title", "#edition_title"],
@@ -45,13 +46,13 @@ class ErrorSummaryHelperTest < ActionView::TestCase
       ["Select a node for Node 3, Option 3", "#edition_nodes_attributes_2_options_attributes_2_node"],
     ]
 
-    assert_equal errors_to_display(simple_smart_answer), expected_errors
+    assert_equal expected_errors, errors_to_display(simple_smart_answer)
   end
 
-  test "errors_to_display_hash returns useful error messages and correct hrefs for invalid fields for an edition type without nested fields" do
+  test "For an Edition without nested fields, errors_to_display returns useful error messages and correct hrefs for invalid fields" do
     invalid_edition = LocalTransactionEdition.new(title: "", panopticon_id: "Some_id", lgil_code: 1.11)
 
-    invalid_edition.save
+    invalid_edition.valid?
 
     expected_errors = [
       ["Enter a title", "#edition_title"],
@@ -59,7 +60,6 @@ class ErrorSummaryHelperTest < ActionView::TestCase
       ["LGIL code can only be a whole number between 0 and 999", "#edition_lgil_code"],
     ]
 
-    assert_equal errors_to_display(invalid_edition), expected_errors
+    assert_equal expected_errors, errors_to_display(invalid_edition)
   end
-  # rubocop:enable Rails/SaveBang
 end
