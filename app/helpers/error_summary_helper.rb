e76c5267a2e8b67930594784af3beccb6ec49fe7
Becka Lelew <becka.lelew@digital.cabinet-office.gov.uk>
2022-03-29 09:49:36 +0100

    Ensure errors order is preserved

--- app/helpers/error_summary_helper.rb
+++ app/helpers/error_summary_helper.rb
@@ -1,65 +1,71 @@
 module ErrorSummaryHelper
   def errors_to_display(edition)
-    errors_with_hrefs = case edition
-                        when SimpleSmartAnswerEdition
-                          smart_answer_errors(edition)
-                        when GuideEdition
-                          guide_errors(edition)
-                        else
-                          edition_errors(edition)
-                        end
-    errors_with_hrefs.map { |error, href| [error.message, href] }
+    case edition
+    when SimpleSmartAnswerEdition
+      smart_answer_errors(edition)
+    when GuideEdition
+      guide_errors(edition)
+    else
+      edition_errors(edition).map { |error, href| [error.message, href] }
+    end
   end
 
 private
-
-  def smart_answer_errors(smart_answer)
-    all_errors_and_hrefs = edition_errors(smart_answer) +
-      get_node_errors_and_hrefs(smart_answer.nodes) +
-      get_option_errors(smart_answer.nodes.flat_map(&:options))
-
-    # Reject errors with an attribute of 'nodes' or 'options' as these are because of the nested structure of guides and will be duplicated so not helpful
-    # Errors with an attribute of 'slug' will be slugs which are derived, rather than being input explicitly, so are not useful error messages
-    all_errors_and_hrefs.reject { |error, _| %i[nodes options slug].include?(error.attribute) }
-  end
-
-  def guide_errors(guide)
-    all_errors_and_hrefs = edition_errors(guide) + get_part_errors(guide.parts)
-
-    # Reject errors with an attribute of 'parts' as these are because of the nested structure of guides and will be duplicated so not helpful
-    all_errors_and_hrefs.reject { |error, _| error.attribute == :parts }
-  end
 
   def edition_errors(edition)
     edition.errors.map { |error| [error, "#edition_#{error.attribute}"] }
   end
 
-  def get_node_errors_and_hrefs(nodes)
-    href_constructor = proc do |node, attribute|
-      "#edition_nodes_attributes_#{node.order - 1}_#{attribute}"
+  def smart_answer_errors(smart_answer)
+    edition_errors = edition_errors(smart_answer)
+
+    nested_errors = []
+
+    smart_answer.nodes.each do |node|
+      node.errors.each do |error|
+        nested_errors << [error, href_for_node(node, error.attribute)]
+      end
+
+      node.options.each do |option|
+        option.errors.each do |error|
+          nested_errors << [error, href_for_option(option, node, error.attribute)]
+        end
+      end
     end
-    errors_from_nested_objects(nodes, &href_constructor)
+
+    # Reject errors with an attribute of 'nodes' or 'options' as these are because of the nested structure of guides and will be duplicated so not helpful
+    # Errors with an attribute of 'slug' will be slugs which are derived, rather than being input explicitly, so are not useful error messages
+    (edition_errors + nested_errors)
+      .reject { |error, _| %i[nodes options slug].include?(error.attribute) } # Errors with attributes of options or nodes will be an error for a option / node as a whole (rather than the individual field) and not helpful
+      .map { |error, href| [error.message, href] }
   end
 
-  def get_option_errors(options)
-    href_constructor = proc do |option, attribute|
-      node = option.node
-      attr = attribute == :next_node ? "node" : attribute
-      "#edition_nodes_attributes_#{node.order - 1}_options_attributes_#{node.options.find_index(option)}_#{attr}"
+  def guide_errors(guide)
+    edition_errors = edition_errors(guide)
+
+    parts_errors = []
+
+    guide.parts.each do |part|
+      part.errors.each do |error|
+        parts_errors << [error, href_for_part(part, error.attribute)]
+      end
     end
-    errors_from_nested_objects(options, &href_constructor)
+
+    (edition_errors + parts_errors)
+      .reject { |error, _| error.attribute == :parts } # Errors with attributes of parts will be an error for a part as a whole (rather than the individual field) and not helpful
+      .map { |error, href| [error.message, href] }
   end
 
-  def get_part_errors(parts)
-    href_constructor = proc do |part, attribute|
-      "#edition_parts_attributes_#{part.guide_edition.parts.find_index(part)}_#{attribute}"
-    end
-    errors_from_nested_objects(parts, &href_constructor)
+  def href_for_node(node, attribute)
+    "#edition_nodes_attributes_#{node.order - 1}_#{attribute}"
   end
 
-  def errors_from_nested_objects(nested_objects, &href_constructor)
-    nested_objects
-      .select { |object| object.errors.present? }
-      .flat_map { |object| object.errors.map { |error| [error, href_constructor.call(object, error.attribute)] } }
+  def href_for_option(option, node, attribute)
+    attr = attribute == :next_node ? "node" : attribute
+    "#edition_nodes_attributes_#{node.order - 1}_options_attributes_#{node.options.find_index(option)}_#{attr}"
+  end
+
+  def href_for_part(part, attribute)
+    "#edition_parts_attributes_#{part.guide_edition.parts.find_index(part)}_#{attribute}"
   end
 end
--- test/unit/helpers/error_summary_helper_test.rb
+++ test/unit/helpers/error_summary_helper_test.rb
@@ -28,22 +28,24 @@ class ErrorSummaryHelperTest < ActionView::TestCase
 
   test "For a SimpleSmartAnswer, errors_to_display returns useful error messages and correct hrefs for invalid fields" do
     valid_outcome_node = SimpleSmartAnswerEdition::Node.new(kind: "outcome", title: "Node 1", slug: "node-1", order: 1)
-    outcome_node_without_title = SimpleSmartAnswerEdition::Node.new(kind: "outcome", title: "", slug: "node-2", order: 2)
 
     valid_option = SimpleSmartAnswerEdition::Node::Option.new(next_node: "Node 1", label: "Some label")
     invalid_option_1 = SimpleSmartAnswerEdition::Node::Option.new(next_node: "Node 1", label: "")
     invalid_option_2 = SimpleSmartAnswerEdition::Node::Option.new(next_node: "", label: "Another label")
-    question_node_with_valid_and_invalid_options = SimpleSmartAnswerEdition::Node.new(kind: "question", title: "Node 3", slug: "node-3", order: 3, options: [valid_option, invalid_option_1, invalid_option_2])
+    question_node_with_valid_and_invalid_options = SimpleSmartAnswerEdition::Node.new(kind: "question", title: "", slug: "node-2", order: 2, options: [valid_option, invalid_option_1, invalid_option_2])
 
-    simple_smart_answer = SimpleSmartAnswerEdition.new(title: "", panopticon_id: "Some_id", nodes: [valid_outcome_node, outcome_node_without_title, question_node_with_valid_and_invalid_options])
+    outcome_node_without_title = SimpleSmartAnswerEdition::Node.new(kind: "outcome", title: "", slug: "node-3", order: 3)
+
+    simple_smart_answer = SimpleSmartAnswerEdition.new(title: "", panopticon_id: "Some_id", nodes: [valid_outcome_node, question_node_with_valid_and_invalid_options, outcome_node_without_title])
 
     simple_smart_answer.valid?
 
     expected_errors = [
       ["Enter a title", "#edition_title"],
       ["Enter a title for Node 2", "#edition_nodes_attributes_1_title"],
-      ["Enter a label for Node 3, Option 2", "#edition_nodes_attributes_2_options_attributes_1_label"],
-      ["Select a node for Node 3, Option 3", "#edition_nodes_attributes_2_options_attributes_2_node"],
+      ["Enter a label for Node 2, Option 2", "#edition_nodes_attributes_1_options_attributes_1_label"],
+      ["Select a node for Node 2, Option 3", "#edition_nodes_attributes_1_options_attributes_2_node"],
+      ["Enter a title for Node 3", "#edition_nodes_attributes_2_title"],
     ]
 
     assert_equal expected_errors, errors_to_display(simple_smart_answer)
