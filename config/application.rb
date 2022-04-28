6bf3337261b8a64eee284e30cfabcf911139554e
Rebecca Pearce <rebecca.pearce@digital.cabinet-office.gov.uk>
2022-04-28 14:30:23 +0100

    Enable new Rails 7 configuration

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ Bundler.require(*Rails.groups)
 module Publisher
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Configuration object for the fact check email fetch script
     # See `script/mail_fetcher`
