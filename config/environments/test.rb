f10240b55b663bb104a3bc3f947efbd67e874018
Kevin Dew <kevindew@me.com>
2022-09-20 20:45:21 +0100

    Set a default "from" for ActionMailer in test
    This is needed to run the tests as the :test mode for ActionMailer
    treats mail requests as if they were for SMTP.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -43,6 +43,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_options = { from: "winston@dev.gov.uk" }
 
   config.action_mailer.default_url_options = { host: "example.com" }
 
