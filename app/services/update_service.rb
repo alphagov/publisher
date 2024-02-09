0d27c2243dda8886cb360599e401a017773e8694
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2024-02-09 15:30:49 +0000

    Add logging to update service

--- app/services/update_service.rb
+++ app/services/update_service.rb
@@ -1,7 +1,9 @@
 class UpdateService
   def self.call(edition, republish: false)
+    Rails.logger.info("Preparing to send draft edition to publishing-api with title: #{edition.title}")
     presenter = EditionPresenterFactory.get_presenter(edition)
     payload = presenter.render_for_publishing_api(republish:)
     Services.publishing_api.put_content(edition.content_id, payload)
+    Rails.logger.info("Draft edition sent to publishing-api with title: #{edition.title}")
   end
 end
