80e2518a30a973ef598beacbce97a5cfa1d4cc87
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2024-02-16 16:43:50 +0000

    Add downtime integration tests
    Update the "Add downtime" integration tests to work with the new page
     that uses GOV.UK Design System components.

--- test/integration/downtime_with_invalid_dates_test.rb
+++ test/integration/downtime_with_invalid_dates_test.rb
@@ -17,6 +17,7 @@ class DowntimeWithInvalidDates < ActionDispatch::IntegrationTest
 
     test_strategy = Flipflop::FeatureSet.current.test!
     test_strategy.switch!(:design_system_downtime_index_page, true)
+    test_strategy.switch!(:design_system_downtime_new, true)
   end
 
   test "Scheduling new downtime with invalid dates" do
@@ -26,32 +27,28 @@ class DowntimeWithInvalidDates < ActionDispatch::IntegrationTest
     click_link "Downtime"
     click_link "Add downtime"
 
-    enter_start_time 1.day.ago
-    enter_end_time 1.day.ago - 1.day
+    enter_from_date_and_time 1.day.ago
+    enter_to_date_and_time 1.day.ago - 1.day
 
-    click_button "Schedule downtime message"
+    click_button "Save"
 
     assert page.has_link?("End time must be in the future", href: "#downtime_end_time")
     assert page.has_link?("Start time must be earlier than end time", href: "#downtime_start_time")
   end
 
-  def enter_start_time(start_time)
-    complete_date_inputs("downtime_start_time", start_time)
+  def enter_from_date_and_time(start_time)
+    enter_date_and_time("start", start_time)
   end
 
-  def enter_end_time(end_time)
-    complete_date_inputs("downtime_end_time", end_time)
+  def enter_to_date_and_time(end_time)
+    enter_date_and_time("end", end_time)
   end
 
-  def complete_date_inputs(input_id, time)
-    select time.year.to_s, from: "#{input_id}_1i"
-    select time.strftime("%B"), from: "#{input_id}_2i"
-    select time.day.to_s, from: "#{input_id}_3i"
-    select pad_digit_to_two_digits(time.hour.to_s), from: "#{input_id}_4i"
-    select time.strftime("%M"), from: "#{input_id}_5i"
-  end
-
-  def pad_digit_to_two_digits(hour_string)
-    hour_string.length == 1 ? "0#{hour_string}" : hour_string
+  def enter_date_and_time(prefix, time)
+    fill_in "downtime[#{prefix}_time(3i)]", with: time.day.to_s
+    fill_in "downtime[#{prefix}_time(2i)]", with: time.month.to_s
+    fill_in "downtime[#{prefix}_time(1i)]", with: time.year.to_s
+    fill_in "downtime[#{prefix}_time(4i)]", with: time.hour.to_s
+    fill_in "downtime[#{prefix}_time(5i)]", with: time.min.to_s
   end
 end
