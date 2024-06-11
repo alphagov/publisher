ecbfbbe36c0b597a19d13a31431d6a251be0c0c5
davidtrussler <mail@davidtrussler.net>
2024-06-11 13:29:34 +0100

    Add `toggleDetails` method

--- spec/javascripts/spec/components/mainstream_table.spec.js
+++ spec/javascripts/spec/components/mainstream_table.spec.js
@@ -15,6 +15,35 @@ describe('Table component', function () {
             <th>Status</th>
           </tr>
         </thead>
+        <tbody>
+          <tr>
+            <td>
+              <p>The National Minimum Wage and Living Wage</p>
+              <details class="govuk-details"></details>
+            </td>
+            <td>18 April 2024</td>
+            <td>David Trussler</td>
+            <td>Draft</td>
+          </tr>
+          <tr>
+            <td>
+              <p>Find software for filing company changes and registrations</p>
+              <details class="govuk-details"></details>
+            </td>
+            <td>21 May 2024</td>
+            <td>Jane Regan</td>
+            <td>Awaiting 2i</td>
+          </tr>
+          <tr>
+            <td>
+              <p>Appeal an Illegal Migration Act decision</p>
+              <details class="govuk-details"></details>
+            </td>
+            <td>22 May 2024</td>
+            <td>Peter Wilkinson</td>
+            <td>Amends needed</td>
+          </tr>
+        </tbody>
       </table>`
 
     module = document.createElement('div')
@@ -35,6 +64,22 @@ describe('Table component', function () {
 
       expect(heading.querySelector('a')).not.toBeNull()
       expect(heading.querySelector('a').textContent).toBe('Expand all')
+    })
+  })
+
+  describe('When the Expand/Contract All link is clicked', function () {
+    it('should open or close all the details sections', function() {
+      // TODO: check text of link is correct
+      // TODO: check right number of links are open when some opened, some closed
+
+      var click = new Event('click');
+      var expandLink = module.querySelector('.mainstream-table__heading a')
+
+      expandLink.dispatchEvent(click)
+      expect(mainstreamTable.openDetails).toBe(3)
+
+      expandLink.dispatchEvent(click)
+      expect(mainstreamTable.openDetails).toBe(0)
     })
   })
 })
