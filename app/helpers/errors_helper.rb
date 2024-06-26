5b0c1776ff45c3d3b71d44d58a422e1d6930fe65
Ana Botto <ana.botto@digital.cabinet-office.gov.uk>
2024-06-26 15:57:25 +0100

    Add error items to flag the form elements on error

--- app/helpers/errors_helper.rb
+++ app/helpers/errors_helper.rb
@@ -1,14 +1,14 @@
 module ErrorsHelper
-  def errors_for(errors, attribute)
+  def errors_for(errors, attribute, use_full_message: true)
     return nil if errors.blank?
 
     errors.filter_map { |error|
       if error.attribute == attribute
         {
-          text: error.full_message,
+          text: use_full_message ? error.full_message : error.message,
         }
       end
     }
-    .presence
+          .presence
   end
 end
