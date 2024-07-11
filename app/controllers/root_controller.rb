1bf87a4e0a84bd8d1e25031b79ad2f04d8547a98
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2024-07-11 14:00:12 +0100

    Add a new RootController
    Create a new `RootController` class and conditionally route to it or the
     `LegacyRootController` based on whether the new
     "design_system_publications_filter" feature is enabled (it defaults to
     disabled).
    
    In this commit `RootController` is an exact duplicate of
     `LegacyRootController`, but future commits will update this so that it
     uses the new, GOV.UK Design System-based, design.

---
+++ app/controllers/root_controller.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+class RootController < ApplicationController
+  respond_to :html, :json
+
+  include ColumnSortable
+
+  ITEMS_PER_PAGE = 20
+
+  STATE_NAME_LISTS = { "draft" => "drafts", "fact_check" => "out_for_fact_check" }.freeze
+
+  def index
+    user_filter           = params[:user_filter] || session[:user_filter]
+    session[:user_filter] = user_filter
+
+    @list = params[:list].presence || "drafts"
+    @presenter, @user_filter = build_presenter(user_filter, params[:page])
+
+    # Looking at another class, but the whole approach taken by this method and its
+    # associated presenter needs revisiting.
+    unless @presenter.acceptable_list?(@list)
+      render body: { "raw" => "Not Found" }, status: :not_found
+      return
+    end
+
+    if params[:string_filter].present?
+      clean_string_filter = params[:string_filter]
+        .strip
+        .gsub(/\s+/, " ")
+      @presenter.filter_by_substring(clean_string_filter)
+    end
+  end
+
+private
+
+  def format_filter
+    Artefact::FORMATS_BY_DEFAULT_OWNING_APP["publisher"].include?(params[:format_filter]) ? params[:format_filter] : "edition"
+  end
+
+  def filtered_editions
+    return Edition if format_filter == "edition"
+
+    Edition.where(_type: "#{format_filter.camelcase}Edition")
+  end
+
+  def list_parameter_from_state(state)
+    STATE_NAME_LISTS[state] || state
+  end
+
+  def build_presenter(user_filter, current_page = nil)
+    user_filter, user = process_user_filter(user_filter)
+
+    editions = filtered_editions.order_by([sort_column, sort_direction])
+    editions = editions.page(current_page).per(ITEMS_PER_PAGE)
+    editions = editions.where.not(_type: "PopularLinksEdition")
+
+    [PrimaryListingPresenter.new(editions, user), user_filter]
+  end
+
+  def process_user_filter(user_filter = nil)
+    if user_filter.blank?
+      user_filter = current_user.uid
+      user = current_user
+    elsif %w[all nobody].include?(user_filter)
+      user = user_filter.to_sym
+    else
+      user = User.where(uid: user_filter).first
+    end
+
+    [user_filter, user]
+  end
+end
