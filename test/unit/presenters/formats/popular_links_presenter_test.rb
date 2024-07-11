0c5e4479a7b0387fe714613407ee109dfae2f5fb
syed-ali-tw <syed.ali1@digital.cabinet-office.gov.uk>
2024-07-11 15:49:44 +0100

    Remove redundant code

--- test/unit/presenters/formats/popular_links_presenter_test.rb
+++ test/unit/presenters/formats/popular_links_presenter_test.rb
@@ -2,8 +2,7 @@ require "test_helper"
 
 class PopularLinksPresenterTest < ActiveSupport::TestCase
   def subject
-    updated_at_time = Time.parse("Thu, 11 Jul 2024 10:47:27.397934771 UTC")
-    Formats::PopularLinksPresenter.new(FactoryBot.create(:popular_links, updated_at: updated_at_time))
+    Formats::PopularLinksPresenter.new(FactoryBot.create(:popular_links))
   end
 
   context "#render_for_publishing_api" do
