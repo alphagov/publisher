a19e5464e9121bc42d89a2c87c9b6049066f42ba
davidtrussler <mail@davidtrussler.net>
2024-07-16 17:04:02 +0100

    Update JS for table

--- app/assets/javascripts/modules/publisher-table.js
+++ app/assets/javascripts/modules/publisher-table.js
@@ -7,25 +7,11 @@
   }
 
   PublisherTable.prototype.init = function () {
-    console.log('init!')
-
     // Add Expand/Contract link to DOM
     this.addExpandlink()
 
     // Add Event listener for Expand All button
     this.$module.querySelector('.publisher-table__heading a').addEventListener('click', this.toggleDetails.bind(this))
-
-    // Add Event listener for Details sections
-    // this.$module.addEventListener('click', function(e) {
-    //   if (e.target.classList.contains('govuk-details__summary')) {
-    //     this.detailsStatus(e.target.parentNode)
-    //   } else if (e.target.parentNode.classList.contains('govuk-details__summary')) {
-    //     this.detailsStatus(e.target.parentNode.parentNode)
-    //   }
-    // }.bind(this))
-
-    // Add Event listeners for sections buttons
-    // this.setUpSections()
   }
 
   // Add Expand/Contract link to DOM
@@ -38,8 +24,6 @@
 
   // Toggles the "More details" sections
   PublisherTable.prototype.toggleDetails = function (e) {
-    console.log('toggleDetails!')
-
     if (this.openDetails < this.numDetails) {
       this.details.forEach(function(section) {
         section.setAttribute('open', true)
@@ -56,95 +40,6 @@
       e.target.textContent = 'Expand all'
     }
   }
-
-  // Records status of Details sections
-  // PublisherTable.prototype.detailsStatus = function (section) {
-  //   var expandLink = this.$module.querySelector('.govuk-table__header--expand').querySelector('a')
-
-  //   if (section.open == true) {
-  //     this.openDetails--
-  //   } else {
-  //     this.openDetails++
-  //   }
-
-  //   if(this.openDetails == this.numDetails) {
-  //     expandLink.textContent = 'Collapse all'
-  //   } else {
-  //     expandLink.textContent = 'Expand all'
-  //   }
-  // }
-
-  // Adds event listeners to set state of the "Show/Hide all" button when section buttons are clicked
-  // PublisherTable.prototype.setUpSections = function () {
-  //   this.sections.forEach(function (section) {
-  //     section.querySelector('.govuk-accordion__section-heading').addEventListener('click', function () {
-  //       if (section.classList.contains('govuk-accordion__section--expanded')) {
-  //         this.openSections--
-
-  //         if (this.openSections === this.numSections - 1) {
-  //           this.toggleShowAllControls()
-  //         }
-  //       } else {
-  //         this.openSections++
-
-  //         if (this.openSections === this.numSections) {
-  //           this.toggleShowAllControls()
-  //         }
-  //       }
-  //     }.bind(this))
-  //   }.bind(this))
-  // }
-
-  // Toggles the "Show/Hide all" button
-  // PublisherTable.prototype.toggleShowAllControls = function () {
-  //   var showAllControls = this.$module.querySelector('.govuk-accordion__show-all')
-
-  //   if (showAllControls.getAttribute('aria-expanded') === 'true') {
-  //     showAllControls.querySelector('.govuk-accordion__show-all-text').textContent = 'Show all'
-  //     showAllControls.querySelector('.govuk-accordion-nav__chevron').classList.remove('govuk-accordion-nav__chevron--up')
-  //     showAllControls.querySelector('.govuk-accordion-nav__chevron').classList.add('govuk-accordion-nav__chevron--down')
-  //     showAllControls.setAttribute('aria-expanded', 'false')
-  //   } else {
-  //     showAllControls.querySelector('.govuk-accordion__show-all-text').textContent = 'Hide all'
-  //     showAllControls.querySelector('.govuk-accordion-nav__chevron').classList.remove('govuk-accordion-nav__chevron--down')
-  //     showAllControls.querySelector('.govuk-accordion-nav__chevron').classList.add('govuk-accordion-nav__chevron--up')
-  //     showAllControls.setAttribute('aria-expanded', 'true')
-  //   }
-  // }
-
-  // PublisherTable.prototype.toggleAllSections = function () {
-  //   var controlAllState = this.$module.querySelector('.govuk-accordion__show-all').getAttribute('aria-expanded')
-  //   var sections = this.$module.querySelectorAll('.' + this.sectionClass)
-
-  //   // Open and close all sections
-  //   sections.forEach(function (section) {
-  //     var button = section.querySelector('button')
-
-  //     if (section.classList.contains(this.sectionExpandedClass)) {
-  //       if (controlAllState === 'true') {
-  //         section.classList.remove(this.sectionExpandedClass)
-  //         section.querySelector('.' + this.sectionInnerContentClass).hidden = 'until-found'
-  //         button.querySelector('.govuk-accordion-nav__chevron').classList.add('govuk-accordion-nav__chevron--down')
-  //         button.querySelector('.govuk-accordion-nav__chevron').classList.remove('govuk-accordion-nav__chevron--up')
-  //         button.querySelector('.govuk-accordion__section-toggle-text').textContent = 'Show'
-  //         button.setAttribute('aria-expanded', 'false')
-  //         this.openSections = 0
-  //       }
-  //     } else {
-  //       if (controlAllState === 'false') {
-  //         section.classList.add(this.sectionExpandedClass)
-  //         section.querySelector('.' + this.sectionInnerContentClass).hidden = ''
-  //         button.querySelector('.govuk-accordion-nav__chevron').classList.remove('govuk-accordion-nav__chevron--down')
-  //         button.querySelector('.govuk-accordion-nav__chevron').classList.add('govuk-accordion-nav__chevron--up')
-  //         button.querySelector('.govuk-accordion__section-toggle-text').textContent = 'Hide'
-  //         button.setAttribute('aria-expanded', 'true')
-  //         this.openSections = this.numSections
-  //       }
-  //     }
-  //   }.bind(this))
-
-  //   this.toggleShowAllControls()
-  // }
 
   Modules.PublisherTable = PublisherTable
 })(window.GOVUK.Modules)
--- spec/javascripts/spec/publisher_table.spec.js
+++ spec/javascripts/spec/publisher_table.spec.js
@@ -1,38 +1,157 @@
 describe('Table component', function () {
   'use strict'
 
-  var table, publisherTable
+  var module, publisherTable
 
   beforeEach(function () {
-    var tableHtml =
-      `<thead>
-        <tr>
-          <th>Title</th>
-          <th>Assigned to</th>
-          <th>Status</th>
-          <th class="govuk-table__header--controls"></th>
-        </tr>
-      </thead>`
+    var moduleHtml =
+      `<p class="publisher-table__heading">20 documents</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Updated</th>
+            <th>Assigned to</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <p><a>Get an even faster decision on your visa or settlement application</a></p>
+              <details class="govuk-details">
+                <summary><span>More details</span></summary>
+              </details>
+            </td>
+            <td><span>12 Dec 2023</span></td>
+            <td>Keir Starmer</td>
+            <td><span>Draft</span></td>
+          </tr>
+          <tr>
+            <td>
+              <p><a>Test Smart Answer</a></p>
+              <details class="govuk-details">
+                <summary><span>More details</span></summary>
+              </details>
+            </td>
+            <td><span>11 Jul 2024</span></td>
+            <td>Rishi Sunak</td>
+            <td><span>Fact check</span></td>
+          </tr>
+          <tr>
+            <td>
+              <p><a>Gwneud cais i bleidleisio drwy’r post</a></p>
+              <details class="govuk-details">
+                <summary><span>More details</span></summary>
+              </details>
+            </td>
+            <td><span>10 Jul 2024</span></td>
+            <td>Liz Truss</td>
+            <td><span>Amends needed</span></td>
+          </tr>
+        </tbody>
+      </table>`
 
-    table = document.createElement('table')
-    table.innerHTML = tableHtml
-    document.body.appendChild(table)
+    module = document.createElement('div')
+    module.innerHTML = moduleHtml
+    document.body.appendChild(module)
 
-    publisherTable = new GOVUK.Modules.PublisherTable(table)
+    publisherTable = new GOVUK.Modules.PublisherTable(module)
     publisherTable.init()
   })
 
   afterEach(function () {
-    document.body.removeChild(table)
+    document.body.removeChild(module)
   })
 
   describe('When initialised', function () {
-    it('should have a "Show/Hide All" button', function () {
-      var headerControls = table.querySelector('th.govuk-table__header--controls')
+    it('should have a "Expand/Collapse All" button', function () {
+      var heading = module.querySelector('p.publisher-table__heading')
 
-      expect(headerControls.querySelector('button')).not.toBeNull()
-      expect(headerControls.querySelector('button').classList).toContain('govuk-accordion__show-all')
-      expect(headerControls.querySelector('button').textContent).toBe('Show all')
+      expect(heading.querySelector('a')).not.toBeNull()
+      expect(heading.querySelector('a').classList).toContain('publisher-table--expand-link')
+      expect(heading.querySelector('a').textContent).toBe('Expand all')
+    })
+  })
+
+  describe('The Expand/Contract all link', function () {
+    var expandContractLink, detailsSections, event
+
+    beforeEach(function () {
+      expandContractLink = module.querySelector('p.publisher-table__heading a')
+      detailsSections = module.querySelectorAll('details')
+      event = new Event('click')
+    })
+
+    it('should be labelled "Expand all" when all of the "More details" sections are closed', function () {
+      detailsSections[0].open = false
+      detailsSections[1].open = false
+      detailsSections[2].open = false
+      publisherTable.openDetails = 0
+
+      expect(expandContractLink.textContent).toBe('Expand all')
+    })
+
+    it('should be labelled "Expand all" when some of the "More details" sections are closed', function () {
+      detailsSections[0].open = false
+      detailsSections[1].open = true
+      detailsSections[2].open = true
+      publisherTable.openDetails = 2
+
+      expect(expandContractLink.textContent).toBe('Expand all')
+    })
+
+    it('should open all the "More details" sections when all of them are closed', function () {
+      detailsSections[0].open = false
+      detailsSections[1].open = false
+      detailsSections[2].open = false
+      publisherTable.openDetails = 0
+
+      expandContractLink.dispatchEvent(event)
+
+      expect(publisherTable.openDetails).toBe(3)
+      expect(detailsSections[0].open).toBe(true)
+      expect(detailsSections[1].open).toBe(true)
+      expect(detailsSections[2].open).toBe(true)
+    })
+
+    it('should open all the "More details" sections when some of them are closed', function () {
+      detailsSections[0].open = false
+      detailsSections[1].open = true
+      detailsSections[2].open = true
+      publisherTable.openDetails = 2
+
+      expandContractLink.dispatchEvent(event)
+
+      expect(publisherTable.openDetails).toBe(3)
+      expect(detailsSections[0].open).toBe(true)
+      expect(detailsSections[1].open).toBe(true)
+      expect(detailsSections[2].open).toBe(true)
+    })
+
+    it('should be labelled "Collapse all" when all of the "More details" sections are open', function () {
+      detailsSections[0].open = false
+      detailsSections[1].open = false
+      detailsSections[2].open = false
+      publisherTable.openDetails = 0
+
+      expandContractLink.dispatchEvent(event)
+
+      expect(expandContractLink.textContent).toBe('Collapse all')
+    })
+
+    it('should close all the "More details" sections when they are all open', function () {
+      detailsSections[0].open = true
+      detailsSections[1].open = true
+      detailsSections[2].open = true
+      publisherTable.openDetails = 3
+
+      expandContractLink.dispatchEvent(event)
+
+      expect(publisherTable.openDetails).toBe(0)
+      expect(detailsSections[0].open).toBe(false)
+      expect(detailsSections[1].open).toBe(false)
+      expect(detailsSections[2].open).toBe(false)
     })
   })
 })
