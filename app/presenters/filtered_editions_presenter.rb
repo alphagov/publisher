2cb2710b122f16cdd699283e67c443d7e6c67baf
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2024-07-25 09:08:31 +0100

    Enable filtering publications by assigned user
    Update the backend for the new publications page to be able to filter
     publications by assigned user.
    
    This change is only at the backend end—the view still needs updating to
     post the assigned user to filter by.
    
    Also provides an `#available_users` method on the presenter for
     populating a list on the UI of users to choose from.

--- app/presenters/filtered_editions_presenter.rb
+++ app/presenters/filtered_editions_presenter.rb
@@ -1,17 +1,44 @@
 # frozen_string_literal: true
 
 class FilteredEditionsPresenter
-  def initialize(states_filter)
+  def initialize(states_filter, assigned_to_filter)
     @states_filter = states_filter || []
+    @assigned_to_filter = assigned_to_filter
+  end
+
+  def available_users
+    User.enabled.alphabetized
   end
 
   def editions
     result = Edition.all
-    result = result.in_states(states_filter) unless states_filter.empty?
-    result
+    result = apply_states_filter(result)
+    apply_assigned_to_filter(result)
   end
 
 private
 
-  attr_reader :states_filter
+  def apply_states_filter(editions)
+    return editions if states_filter.empty?
+
+    editions.in_states(states_filter)
+  end
+
+  def apply_assigned_to_filter(editions)
+    return editions unless assigned_to_filter
+
+    if assigned_to_filter == "nobody"
+      editions = editions.assigned_to(nil)
+    else
+      begin
+        assigned_user = User.find(assigned_to_filter)
+        editions = editions.assigned_to(assigned_user) if assigned_user
+      rescue Mongoid::Errors::DocumentNotFound
+        Rails.logger.warn "An attempt was made to filter by an unknown user ID: '#{assigned_to_filter}'"
+      end
+    end
+    editions
+  end
+
+  attr_reader :states_filter, :assigned_to_filter
 end
--- test/unit/presenters/filtered_editions_presenter_test.rb
+++ test/unit/presenters/filtered_editions_presenter_test.rb
@@ -8,7 +8,7 @@ class FilteredEditionsPresenterTest < ActiveSupport::TestCase
       draft_guide = FactoryBot.create(:guide_edition, state: "draft")
       published_guide = FactoryBot.create(:guide_edition, state: "published")
 
-      filtered_editions = FilteredEditionsPresenter.new(nil).editions
+      filtered_editions = FilteredEditionsPresenter.new(nil, nil).editions
 
       assert_equal(2, filtered_editions.count)
       assert_equal(draft_guide, filtered_editions[0])
@@ -19,10 +19,62 @@ class FilteredEditionsPresenterTest < ActiveSupport::TestCase
       draft_guide = FactoryBot.create(:guide_edition, state: "draft")
       FactoryBot.create(:guide_edition, state: "published")
 
-      filtered_editions = FilteredEditionsPresenter.new(%w[draft]).editions
+      filtered_editions = FilteredEditionsPresenter.new(%w[draft], nil).editions
 
       assert_equal(1, filtered_editions.count)
       assert_equal(draft_guide, filtered_editions[0])
+    end
+
+    should "filter by 'assigned to'" do
+      anna = FactoryBot.create(:user, name: "Anna")
+      assigned_to_anna = FactoryBot.create(:guide_edition, assigned_to: anna.id)
+      FactoryBot.create(:guide_edition)
+
+      filtered_editions = FilteredEditionsPresenter.new(nil, anna.id).editions.to_a
+
+      assert_equal([assigned_to_anna], filtered_editions)
+    end
+
+    should "filter by 'not assigned'" do
+      anna = FactoryBot.create(:user, name: "Anna")
+      FactoryBot.create(:guide_edition, assigned_to: anna.id)
+      not_assigned = FactoryBot.create(:guide_edition)
+
+      filtered_editions = FilteredEditionsPresenter.new(nil, "nobody").editions.to_a
+
+      assert_equal([not_assigned], filtered_editions)
+    end
+
+    should "ignore invalid 'assigned to'" do
+      anna = FactoryBot.create(:user, name: "Anna")
+      FactoryBot.create(:guide_edition, assigned_to: anna.id)
+      FactoryBot.create(:guide_edition)
+
+      filtered_editions =
+        FilteredEditionsPresenter.new(nil, "not a valid user id").editions
+
+      assert_equal(2, filtered_editions.count)
+    end
+  end
+
+  context "#available_users" do
+    should "return users in alphabetical order" do
+      bob = FactoryBot.create(:user, name: "Bob")
+      charlie = FactoryBot.create(:user, name: "Charlie")
+      anna = FactoryBot.create(:user, name: "Anna")
+
+      users = FilteredEditionsPresenter.new(nil, nil).available_users.to_a
+
+      assert_equal([anna, bob, charlie], users)
+    end
+
+    should "not include disabled users" do
+      enabled_user = FactoryBot.create(:user, name: "enabled user")
+      FactoryBot.create(:user, name: "disabled user", disabled: true)
+
+      users = FilteredEditionsPresenter.new(nil, nil).available_users.to_a
+
+      assert_equal(users, [enabled_user])
     end
   end
 end
