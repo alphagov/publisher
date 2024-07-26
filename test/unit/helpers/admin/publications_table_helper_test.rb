9d3cf493fc322c6312474be726c8b54830aac13a
davidtrussler <mail@davidtrussler.net>
2024-07-26 12:11:49 +0100

    - Updates test to   - define @publication_draft in setup   - start testing for each method

--- test/unit/helpers/admin/publications_table_helper_test.rb
+++ test/unit/helpers/admin/publications_table_helper_test.rb
@@ -1,37 +1,43 @@
 require "test_helper"
 
 class PublicationsTableHelperTest < ActionView::TestCase
-  test "it returns the correct value for the `edition_number` method" do
+  include PublicationsTableHelper
+
+  def setup
     # Draft 2
-    publication_draft = stub(
+    @publication_draft = stub(
       # _id: 666bfe6945dba900014b72a3,
       # created_at: 2024-06-14 08:25:13.916 UTC,
       # updated_at: 2024-06-26 10:26:42.484 UTC,
       state: "draft",
-      assigned_to_id: nil,
-      panopticon_id: nil,
+      # assigned_to_id: nil,
+      # panopticon_id: nil,
       version_number: 1,
-      sibling_in_progress: nil,
-      title: "popular links",
-      in_beta: false,
-      publish_at: nil,
-      overview: nil,
-      slug: nil,
-      rejected_count: 0,
-      assignee: nil,
-      reviewer: nil,
-      creator: nil,
-      publisher: nil,
-      archiver: nil,
-      major_change: false,
-      change_note: nil,
-      review_requested_at: nil,
-      auth_bypass_id: "db95b296-a6ff-4d1f-9e67-42c2b536ddc5",
-      _type: "PopularLinksEdition"
+      # sibling_in_progress: nil,
+      # title: "popular links",
+      # in_beta: false,
+      # publish_at: nil,
+      # overview: nil,
+      # slug: nil,
+      # rejected_count: 0,
+      # assignee: nil,
+      # reviewer: nil,
+      # creator: nil,
+      # publisher: nil,
+      # archiver: nil,
+      # major_change: false,
+      # change_note: nil,
+      # review_requested_at: nil,
+      # auth_bypass_id: "db95b296-a6ff-4d1f-9e67-42c2b536ddc5",
+      # _type: "PopularLinksEdition"
       # link_items: [{"title"=>"title1", "url"=>"https://bbc.com"}, {"title"=>"title2", "url"=>"https://bbc.com"}, {"title"=>"title3", "url"=>"https://bbc.com"}, {"title"=>"title4", "url"=>"https://bbc.com"}, {"title"=>"title5", "url"=>"https://bbc.com"}, {"title"=>"title6", "url"=>"https://bbc.com"}]
     )
+  end
 
-    assert_equal edition_number(publication_draft), "1"
+  context "edition_number" do
+    should "return the correct values for supplied publications" do
+      assert_equal edition_number(@publication_draft), "1"
+    end
   end
 end
 
@@ -60,31 +66,3 @@ end
 # _type: "SimpleSmartAnswerEdition"
 # body: "The body of the thing"
 # start_button_text: "Start now">
-
-# Draft 2
-# <PopularLinksEdition
-# _id: 666bfe6945dba900014b72a3,
-# created_at: 2024-06-14 08:25:13.916 UTC,
-# updated_at: 2024-06-26 10:26:42.484 UTC,
-# state: "draft",
-# assigned_to_id: nil,
-# panopticon_id: nil,
-# version_number: 1,
-# sibling_in_progress: nil,
-# title: "popular links",
-# in_beta: false,
-# publish_at: nil,
-# overview: nil,
-# slug: nil,
-# rejected_count: 0,
-# assignee: nil,
-# reviewer: nil,
-# creator: nil,
-# publisher: nil,
-# archiver: nil,
-# major_change: false,
-# change_note: nil,
-# review_requested_at: nil,
-# auth_bypass_id: "db95b296-a6ff-4d1f-9e67-42c2b536ddc5",
-# _type: "PopularLinksEdition",
-# link_items: [{"title"=>"title1", "url"=>"https://bbc.com"}, {"title"=>"title2", "url"=>"https://bbc.com"}, {"title"=>"title3", "url"=>"https://bbc.com"}, {"title"=>"title4", "url"=>"https://bbc.com"}, {"title"=>"title5", "url"=>"https://bbc.com"}, {"title"=>"title6", "url"=>"https://bbc.com"}]
