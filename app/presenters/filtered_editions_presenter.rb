bd49e6298e2be0a92c21a6cab9cb9c0d69976def
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2024-07-26 15:33:41 +0100

    Enable filtering publications by state
    Update the backend for the new publications page to be able to filter
     publications by state.
    
    This change is only at the controller end—the view still needs updating
     to post the expected list of states to filter by.

--- app/presenters/filtered_editions_presenter.rb
+++ app/presenters/filtered_editions_presenter.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 class FilteredEditionsPresenter
-  def editions
-    Edition.all
+  def initialize(states_filter)
+    @states_filter = states_filter || []
   end
+
+  def editions
+    result = Edition.all
+    result = result.in_states(states_filter) unless states_filter.empty?
+    result
+  end
+
+private
+
+  attr_reader :states_filter
 end
