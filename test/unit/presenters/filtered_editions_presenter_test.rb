06dfb8cb12df0b9cb7d97743971add7f60434477
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2024-09-06 11:03:22 +0100

    Memoize FilteredEditionsPresenter.editions
    The view calls `#editions` three times, which means it is querying the
     database three times; memoize the result so that it is only querying
     the database once.

--- test/unit/presenters/filtered_editions_presenter_test.rb
+++ test/unit/presenters/filtered_editions_presenter_test.rb
@@ -121,6 +121,14 @@ class FilteredEditionsPresenterTest < ActiveSupport::TestCase
       assert_equal(middle, filtered_editions[1])
       assert_equal(oldest, filtered_editions[2])
     end
+
+    should "only query the database once, regardless of how many times it is called" do
+      presenter = FilteredEditionsPresenter.new
+      presenter.expects(:query_editions).once.returns([])
+
+      presenter.editions
+      presenter.editions
+    end
   end
 
   context "#available_users" do
