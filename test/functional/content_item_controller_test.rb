460a565b6ff87973879bbb24fc86f9a4a26d5893
syed-ali-tw <syed.ali1@digital.cabinet-office.gov.uk>
2024-10-30 11:34:52 +0000

    Redirect to root controller or legacy root controller based design_system_publications_filter switch

--- test/functional/content_item_controller_test.rb
+++ test/functional/content_item_controller_test.rb
@@ -4,19 +4,45 @@ class ContentItemControllerTest < ActionController::TestCase
   def setup
     login_as_stub_user
     @edition = FactoryBot.create(:edition)
+    @test_strategy = Flipflop::FeatureSet.current.test!
   end
 
-  should "redirect to content by content_id" do
-    get :by_content_id, params: { content_id: @edition.content_id }
+  context "design_system_publications_filter switch is enabled" do
+    setup do
+      @test_strategy.switch!(:design_system_publications_filter, true)
+    end
 
-    assert_redirected_to edition_path(@edition.artefact.latest_edition)
+    should "redirect to new design system publications page" do
+      get :by_content_id, params: { content_id: @edition.content_id }
+
+      assert_routing("/", controller: "root", action: "index")
+    end
+
+    should "redirect to root with error message if content_id is not found" do
+      get :by_content_id, params: { content_id: "#{@edition.artefact.content_id}wrong-id" }
+
+      assert_redirected_to root_path
+      assert_equal "The requested content was not found", flash[:danger]
+    end
   end
 
-  should "redirect to root with error message if content_id is not found" do
-    get :by_content_id, params: { content_id: "#{@edition.artefact.content_id}wrong-id" }
+  context "design_system_publications_filter switch is disabled" do
+    setup do
+      @test_strategy.switch!(:design_system_publications_filter, false)
+    end
 
-    assert_redirected_to root_path
-    assert_equal "The requested content was not found", flash[:danger]
+    should "redirect to old bootstrap ui publications page" do
+      get :by_content_id, params: { content_id: @edition.content_id }
+
+      assert_routing("/", controller: "legacy_root", action: "index")
+    end
+
+    should "redirect to root with error message if content_id is not found" do
+      get :by_content_id, params: { content_id: "#{@edition.artefact.content_id}wrong-id" }
+
+      assert_redirected_to root_path
+      assert_equal "The requested content was not found", flash[:danger]
+    end
   end
 
   should "redirect to root with error message if any error" do
