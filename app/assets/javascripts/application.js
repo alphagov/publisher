4e8e91507e6c82f245906307c80c893b5a66318b
davidtrussler <mail@davidtrussler.net>
2024-12-03 13:11:36 +0000

    Add the "Related external lnks" tab to the edit page - Create view/partials to render the UI - Update `editions_controller` to add a `update_related_external_links` action - Update routes for `update_related_external_links` action - Add references to the JS/CSS for the "Add another" component - Update controller test for `update_related_external_links` method - Update `edition_edit` test for `related external links` tab - Add new `edition_external_links_test` integration test for JS scenarios

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,5 +2,6 @@
 
 //= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/lib
+//= require govuk_publishing_components/components/add-another
 //= require govuk_publishing_components/components/govspeak
 //= require govuk_publishing_components/components/table
