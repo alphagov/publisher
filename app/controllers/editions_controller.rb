36bb519e5fffadd8e2c26a120fc36ede6efdbd63
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2025-02-06 08:44:59 +0000

    Order controller's alias methods alphabetically

--- app/controllers/editions_controller.rb
+++ app/controllers/editions_controller.rb
@@ -33,10 +33,10 @@ class EditionsController < InheritedResources::Base
     render action: "show"
   end
 
+  alias_method :admin, :show
   alias_method :metadata, :show
   alias_method :tagging, :show
   alias_method :unpublish, :show
-  alias_method :admin, :show
 
   def duplicate
     command = EditionDuplicator.new(@resource, current_user)
