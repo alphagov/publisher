ef6a5004c70d02a532cda99bee5793ccd6073d7e
Helen Pickavance <helen.pickavance@digital.cabinet-office.gov.uk>
2025-02-07 15:51:16 +0000

    Attempt to get bank holiday data from publishing API

--- app/controllers/bank_holidays_controller.rb
+++ app/controllers/bank_holidays_controller.rb
@@ -1,16 +1,32 @@
 # frozen_string_literal: true
 
+# TODO: Remove this after testing
+require "gds_api/publishing_api"
+
 class BankHolidaysController < ApplicationController
   # Needed?
   include GDS::SSO::ControllerMethods
   layout "design_system"
 
-  def create
-    @latest_bank_holidays = @latest_bank_holidays.create_draft_bank_holidays_from_last_record
-    render "calendars/bank_holidays/show"
-  end
+  # def create
+    # @latest_bank_holidays = @latest_bank_holidays.create_draft_bank_holidays_from_last_record
+    # render "calendars/bank_holidays/show"
+  #  puts "Here"
+  #     @latest_bank_holidays  = Services.publishing_api.get_content(BankHolidaysEdition.content_id, {})
+  #     if @latest_bank_holidays.nil?
+  #       puts "No bank holidays found"
+  #     end
+  # end
 
   def show
+   # @editions_presenter = EditionsPresenter.new(BankHolidaysEdition.content_id)
+   puts "Here"
+   @latest_bank_holidays = BankHolidaysEdition.create_draft_bank_holidays_from_last_record
+   puts "Here again"
+  rescue Net::HTTPNotFound => e
+    puts "Error #{e.class} #{e.message}"
+  rescue StandardError => e
+    puts "Error #{e.class} #{e.message}"
     render "calendars/bank_holidays/show"
   end
 end
