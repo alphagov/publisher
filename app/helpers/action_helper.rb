d0bdce73b07d89a3d7726fb8b6e9693611da7146
davidtrussler <mail@davidtrussler.net>
2025-02-24 14:26:06 +0000

    Update action_helper for new email formatting methods

--- app/helpers/action_helper.rb
+++ app/helpers/action_helper.rb
@@ -12,6 +12,32 @@ module ActionHelper
     action.comment.present? || action.is_fact_check_request? || action.request_type == "assign"
   end
 
+  def legacy_action_note(action)
+    notes = []
+
+    if action.comment.present?
+      case action.request_type
+      when Action::RECEIVE_FACT_CHECK
+        formatted_email_parts = legacy_format_email_text(action.comment)
+        notes.concat(formatted_email_parts)
+      when HostContentUpdateEvent::Action::CONTENT_BLOCK_UPDATE
+        notes << content_block_update_comment(action)
+      else
+        notes << format_and_auto_link_plain_text(action.comment)
+      end
+    end
+
+    if action.is_fact_check_request? && action.email_addresses.present?
+      notes << tag.p("Request sent to #{mail_to action.email_addresses.gsub(/\s/, ''), action.email_addresses}".html_safe)
+    end
+
+    if action.recipient_id.present?
+      notes << tag.p("Assigned to #{mail_to action.recipient.email, action.recipient.name}".html_safe)
+    end
+
+    notes.join.html_safe
+  end
+
   def action_note(action)
     notes = []
 
@@ -61,7 +87,7 @@ module ActionHelper
     text.html_safe
   end
 
-  def format_email_text(text)
+  def legacy_format_email_text(text)
     email_parts = split_email_at_reply(text)
     formatted_email_parts = [format_and_auto_link_plain_text(email_parts.shift)]
 
@@ -75,6 +101,21 @@ module ActionHelper
       formatted_email_parts << tag.div(
         format_and_auto_link_plain_text(email_parts.join("")),
         class: "original-message if-js-hide js-toggle-target",
+      )
+    end
+
+    formatted_email_parts
+  end
+
+  def format_email_text(text)
+    email_parts = split_email_at_reply(text)
+    formatted_email_parts = [format_and_auto_link_plain_text(email_parts.shift)]
+
+    # if a reply was found
+    unless email_parts.empty?
+      formatted_email_parts << tag.div(
+        format_and_auto_link_plain_text(email_parts.join("")),
+        class: "js-hidden",
       )
     end
 
