585c3010a64764869134dd80133dfdb709862948
syed-ali-tw <syed.ali1@digital.cabinet-office.gov.uk>
2025-02-27 13:17:15 +0000

    Move Artefact and ArtefactAction to postgres

--- app/models/artefact_external_link.rb
+++ app/models/artefact_external_link.rb
@@ -1,12 +1,7 @@
-class ArtefactExternalLink
-  include Mongoid::Document
-
+class ArtefactExternalLink < ApplicationRecord
   strip_attributes only: :url
 
-  field "title", type: String
-  field "url", type: String
-
-  embedded_in :artefact
+  belongs_to :artefact
 
   validates :title, presence: true
   validates :url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]) }
--- test/functional/editions_controller_test.rb
+++ test/functional/editions_controller_test.rb
@@ -2275,7 +2275,7 @@ class EditionsControllerTest < ActionController::TestCase
         },
       }
 
-      assert_equal "External links is invalid", flash[:danger]
+      assert_equal "External links title can't be blank", flash[:danger]
     end
 
     should "display an error message when the url is blank" do
@@ -2286,7 +2286,7 @@ class EditionsControllerTest < ActionController::TestCase
         },
       }
 
-      assert_equal "External links is invalid", flash[:danger]
+      assert flash[:danger].include? "External links URL can't be blank"
     end
 
     should "display an error message when the url is invalid" do
@@ -2297,7 +2297,7 @@ class EditionsControllerTest < ActionController::TestCase
         },
       }
 
-      assert_equal "External links is invalid", flash[:danger]
+      assert_equal "External links URL is invalid", flash[:danger]
     end
 
     should "update related external links and display a success message when successfully saved" do
