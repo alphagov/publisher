3872f1292305fb6913d8141eb818a71e0caeac55
davidtrussler <mail@davidtrussler.net>
2025-03-06 17:19:44 +0000

    Update action_helper

--- app/helpers/action_helper.rb
+++ app/helpers/action_helper.rb
@@ -12,6 +12,32 @@ module ActionHelper
     action.comment.present? || action.is_fact_check_request? || action.request_type == "assign"
   end
 
+  def action_note(action)
+    notes = []
+
+    if action.comment.present?
+      case action.request_type
+      when Action::RECEIVE_FACT_CHECK
+        formatted_email_parts = format_email_text(action.comment)
+        notes.concat(formatted_email_parts)
+      when HostContentUpdateEvent::Action::CONTENT_BLOCK_UPDATE
+        notes << content_block_update_comment(action)
+      else
+        notes << format_and_auto_link_plain_text(action.comment)
+      end
+    end
+
+    if action.is_fact_check_request? && action.email_addresses.present?
+      notes << tag.p("Request sent to #{mail_to action.email_addresses.gsub(/\s/, ''), action.email_addresses}".html_safe)
+    end
+
+    if action.recipient_id.present?
+      notes << tag.p("Assigned to #{mail_to action.recipient.email, action.recipient.name}".html_safe)
+    end
+
+    notes.join.html_safe
+  end
+
   def legacy_action_note(action)
     notes = []
 
@@ -39,11 +65,13 @@ module ActionHelper
   end
 
   def action_items(edition, update_events)
-    items = edition_actions(edition, update_events).map do |action|
+    items = []
+
+    edition_actions(edition, update_events).map do |action|
       requester = action.requester ? action.requester.name : "GOV.UK Bot"
 
       item = [
-        sanitize("<div class='heading'>
+        sanitize("<div class='history__action--#{action.request_type}__heading'>
           <time class='govuk-body' datetime='#{action.created_at}'>#{action.created_at.to_fs(:govuk_date)}</time>
           <p class='govuk-body govuk-!-font-weight-bold'>#{action.to_s} by #{requester}</p>
         </div>")
@@ -51,59 +79,27 @@ module ActionHelper
 
       if action_note?(action)
         warning = if action.comment_sanitized
-          render "govuk_publishing_components/components/warning_text", {
-            text: "We found some potentially harmful content in this email which has been automatically removed. Please check the content of the message in case any text has been deleted as well."
-          }
+          sanitize("<div class='history__action--#{action.request_type}__warning'>#{render 'govuk_publishing_components/components/warning_text', {
+            text: 'We found some potentially harmful content in this email which has been automatically removed. Please check the content of the message in case any text has been deleted as well.'
+          }}</div>")
         end
 
         comment = render "govuk_publishing_components/components/inset_text", {
           text: action_note(action)
         }
 
-        if warning
-          item << sanitize(
-            "<div class='content'>
-              #{warning}
-              <div class='#{action.request_type}'>#{comment}</div>
-            </div>"
-          )
-        else
-          item << sanitize(
-            "<div class='content'>
-              <div class='#{action.request_type}'>#{comment}</div>
-            </div>"
-          )
-        end
+        item << sanitize(
+          "<div class='history__action--#{action.request_type}__content'>
+            #{warning if warning}
+            <div class='history__action--#{action.request_type}__commment'>#{comment}</div>
+          </div>"
+        )
       end
 
-      item.join.html_safe
-    end
-  end
-
-  def action_note(action)
-    notes = []
-
-    if action.comment.present?
-      case action.request_type
-      when Action::RECEIVE_FACT_CHECK
-        formatted_email_parts = format_email_text(action.comment)
-        notes.concat(formatted_email_parts)
-      when HostContentUpdateEvent::Action::CONTENT_BLOCK_UPDATE
-        notes << content_block_update_comment(action)
-      else
-        notes << format_and_auto_link_plain_text(action.comment)
-      end
+      items << item.join.html_safe
     end
 
-    if action.is_fact_check_request? && action.email_addresses.present?
-      notes << tag.p("Request sent to #{mail_to action.email_addresses.gsub(/\s/, ''), action.email_addresses}".html_safe)
-    end
-
-    if action.recipient_id.present?
-      notes << tag.p("Assigned to #{mail_to action.recipient.email, action.recipient.name}".html_safe)
-    end
-
-    notes.join.html_safe
+    items
   end
 
   def content_block_update_comment(action)
