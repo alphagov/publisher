ea545159b20ccc3dfb4a561edbc4442d5718aa4d
davidtrussler <mail@davidtrussler.net>
2025-03-13 10:32:22 +0000

    Improve accessibility: - Render toggle as button - Update JS module to add IDs and aria attributes

--- app/assets/javascripts/modules/history_and_notes.js
+++ app/assets/javascripts/modules/history_and_notes.js
@@ -14,16 +14,20 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
 
   HistoryAndNotes.prototype.setUp = function () {
     const actions = this.$module.querySelectorAll('li')
+    let i = 1
 
     actions.forEach(element => {
       const commentEarlier = element.querySelector('.action--receive_fact_check--earlier') || null
 
       if (commentEarlier) {
-        const toggle = document.createElement('a')
+        const toggle = document.createElement('button')
 
-        toggle.className = 'govuk-link action--receive_fact_check--toggle govuk-!-font-weight-bold'
+        commentEarlier.id = 'toggledContent_' + i
+        toggle.className = 'history__action--receive_fact_check--toggle govuk-body govuk-link govuk-!-font-weight-bold'
         toggle.textContent = this.toggleShowText
         toggle.href = '#'
+        toggle.setAttribute('aria-controls', commentEarlier.id)
+        toggle.setAttribute('aria-expanded', false)
 
         commentEarlier.classList.add('govuk-!-display-none')
         commentEarlier.parentNode.insertBefore(toggle, commentEarlier)
@@ -38,11 +42,15 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
           if (earlierSection.classList.contains('govuk-!-display-none')) {
             earlierSection.classList.remove(hiddenClass)
             toggle.text = this.toggleHideText
+            toggle.setAttribute('aria-expanded', true)
           } else {
             earlierSection.classList.add(hiddenClass)
             toggle.text = this.toggleShowText
+            toggle.setAttribute('aria-expanded', false)
           }
         })
+
+        i++
       }
     })
   }
