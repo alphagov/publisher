6f35b4a60f88dd9317e46304f29318b017a837b7
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2025-03-14 15:24:24 +0000

    Another way to configure chrome driver

--- test/support/govuk_test.rb
+++ test/support/govuk_test.rb
@@ -1,1 +1,15 @@
 GovukTest.configure
+
+Capybara.register_driver :headless_chrome do |app|
+  chrome_options = GovukTest.headless_chrome_selenium_options
+  chrome_options.add_argument("--disable-web-security")
+  chrome_options.add_argument("--window-size=1400,1400")
+  chrome_options.add_argument('--incognito') # Use incognito mode to avoid user data directory conflicts
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    # capabilities: { acceptInsecureCerts: true },
+    options: chrome_options,
+    )
+end
