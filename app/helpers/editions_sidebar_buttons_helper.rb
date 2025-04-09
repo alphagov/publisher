29e0359ee02321f56ef4dc958c04d634cfc05ce3
Helen Pickavance <helen.pickavance@digital.cabinet-office.gov.uk>
2025-04-09 10:38:11 +0100

    Refactor Send to 2i button condition to use existing method

--- app/helpers/editions_sidebar_buttons_helper.rb
+++ app/helpers/editions_sidebar_buttons_helper.rb
@@ -85,7 +85,7 @@ module EditionsSidebarButtonsHelper
       })
     end
     buttons << link_to("Preview (opens in new tab)", preview_edition_path(edition), target: "_blank", rel: "noopener", class: "govuk-link")
-    if current_user.has_editor_permissions?(edition) && %w[draft amends_needed].include?(edition.state)
+    if current_user.has_editor_permissions?(edition) && edition.can_request_review?
       buttons << link_to("Send to 2i", send_to_2i_page_edition_path(edition), class: "govuk-link")
     end
 
