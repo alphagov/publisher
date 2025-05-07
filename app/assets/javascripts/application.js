b97e6edc8ab2f8a7e52ae0adf37ea2c75edeacbd
davidtrussler <mail@davidtrussler.net>
2025-05-07 14:36:40 +0100

    Add more stuff from Whitehall

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,11 @@
 //= require_directory ./modules
 
 //= require govuk_publishing_components/dependencies
+//= require govuk_publishing_components/analytics-ga4
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/add-another
 //= require govuk_publishing_components/components/govspeak
 //= require govuk_publishing_components/components/table
+
+window.GOVUK.approveAllCookieTypes()
+window.GOVUK.cookie('cookies_preferences_set', 'true', { days: 365 })
