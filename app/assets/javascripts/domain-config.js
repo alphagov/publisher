45ad00a5fa373727c06b3302178428151f92827b
davidtrussler <mail@davidtrussler.net>
2025-05-08 15:25:10 +0100

    Update values for publisher

--- app/assets/javascripts/domain-config.js
+++ app/assets/javascripts/domain-config.js
@@ -4,19 +4,19 @@ window.GOVUK.vars = window.GOVUK.vars || {}
 window.GOVUK.vars.extraDomains = [
   {
     name: 'production',
-    domains: ['whitehall-admin.publishing.service.gov.uk'],
+    domains: ['publisher.publishing.service.gov.uk/'],
     initialiseGA4: true,
     id: 'GTM-P93SHJ4Z',
     gaProperty: 'UA-26179049-6'
   },
   {
     name: 'staging',
-    domains: ['whitehall-admin.staging.publishing.service.gov.uk'],
+    domains: ['publisher.staging.publishing.service.gov.uk/'],
     initialiseGA4: false
   },
   {
     name: 'integration',
-    domains: ['whitehall-admin.integration.publishing.service.gov.uk'],
+    domains: ['publisher.integration.publishing.service.gov.uk/'],
     initialiseGA4: true,
     id: 'GTM-P93SHJ4Z',
     auth: '8jHx-VNEguw67iX9TBC6_g',
