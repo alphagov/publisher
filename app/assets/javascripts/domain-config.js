81588b6c9e6f9fc4a47e556ab7b7bcd64c5cdf07
davidtrussler <mail@davidtrussler.net>
2025-05-09 14:48:46 +0100

    Update domain-config for possible problems with domain names

--- app/assets/javascripts/domain-config.js
+++ app/assets/javascripts/domain-config.js
@@ -4,19 +4,19 @@ window.GOVUK.vars = window.GOVUK.vars || {}
 window.GOVUK.vars.extraDomains = [
   {
     name: 'production',
-    domains: ['publisher.publishing.service.gov.uk/'],
+    domains: ['publisher.publishing.service.gov.uk'],
     initialiseGA4: true,
     id: 'GTM-P93SHJ4Z',
     gaProperty: 'UA-26179049-6'
   },
   {
     name: 'staging',
-    domains: ['publisher.staging.publishing.service.gov.uk/'],
+    domains: ['publisher.staging.publishing.service.gov.uk'],
     initialiseGA4: false
   },
   {
     name: 'integration',
-    domains: ['publisher.integration.publishing.service.gov.uk/'],
+    domains: ['publisher.integration.publishing.service.gov.uk'],
     initialiseGA4: true,
     id: 'GTM-P93SHJ4Z',
     auth: '8jHx-VNEguw67iX9TBC6_g',
