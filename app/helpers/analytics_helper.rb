5db70539e257d5f06f6114c9719e7ea2250e32f7
davidtrussler <mail@davidtrussler.net>
2025-05-13 11:54:17 +0100

    Update helper for `document_type` parameter

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -1,11 +1,11 @@
 module AnalyticsHelper
-  def track_analytics_data_on_load(title)
+  def track_analytics_data_on_load(action_name, controller_name)
     {
       event: "page_view",
       page_view: {
+        document_type: "#{action_name}-#{controller_name}",
         publishing_app: "publisher",
         user_created_at: current_user&.created_at&.to_date,
-        document_type: title,
       },
     }.to_json
   end
