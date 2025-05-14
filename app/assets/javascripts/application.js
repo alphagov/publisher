3ac6e2dd51d5afbe59a83f115398e7db6d229beb
davidtrussler <mail@davidtrussler.net>
2025-05-14 12:34:10 +0100

    Remove secondary module generating `page_view` events

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,7 +5,6 @@
 //= require govuk_publishing_components/components/add-another
 //= require govuk_publishing_components/components/govspeak
 //= require govuk_publishing_components/components/table
-//= require analytics-modules/ga4-page-view-tracking.js
 
 window.GOVUK.approveAllCookieTypes()
 window.GOVUK.cookie('cookies_preferences_set', 'true', { days: 365 })
