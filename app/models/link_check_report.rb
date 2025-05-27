50fc7d9173ed4abe1a8365adf0fd2d07c0f77fa1
syed-ali-tw <syed.ali1@digital.cabinet-office.gov.uk>
2025-05-27 16:42:54 +0100

    WIP

--- app/models/link_check_report.rb
+++ app/models/link_check_report.rb
@@ -1,15 +1,8 @@
-class LinkCheckReport
-  include Mongoid::Document
-  include Mongoid::Timestamps
-
-  embedded_in :edition
-  embeds_many :links
+class LinkCheckReport < ApplicationRecord
+  belongs_to :edition, inverse_of: :link_check_reports
+  has_many :links, inverse_of: :link_check_report
 
   accepts_nested_attributes_for :links
-
-  field :batch_id, type: Integer
-  field :status, type: String
-  field :completed_at, type: DateTime
 
   validates :batch_id, presence: true, uniqueness: true
   validates :status, presence: true
