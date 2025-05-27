bcd181c265037b0b33e9b7db70814c0410f09c0a
syed-ali-tw <syed.ali1@digital.cabinet-office.gov.uk>
2025-05-27 15:18:15 +0100

    Move PlaceEdition to postgres

--- app/models/place_edition.rb
+++ app/models/place_edition.rb
@@ -1,10 +1,5 @@
-require "edition"
-
-class PlaceEdition < Edition
-  field :introduction, type: String
-  field :more_information, type: String
-  field :need_to_know, type: String
-  field :place_type, type: String
+class PlaceEdition < ApplicationRecord
+  include Editionable
 
   GOVSPEAK_FIELDS = %i[introduction more_information need_to_know].freeze
 
