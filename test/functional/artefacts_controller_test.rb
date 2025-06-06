5e5054526e52404e35ff028e59e79fa2221a4b7c
Helen Pickavance <helen.pickavance@digital.cabinet-office.gov.uk>
2025-06-06 12:27:40 +0100

    WIP

--- test/functional/artefacts_controller_test.rb
+++ test/functional/artefacts_controller_test.rb
@@ -20,4 +20,27 @@ class ArtefactsControllerTest < ActionController::TestCase
       assert_includes flash[:danger], "do not have permission"
     end
   end
+
+  # TODO: Context '#update'
+  # Replicate above?
+  # Need test to ensure a non-govuk-editor cannot create an artefact
+  context "#update" do
+    should "allow update if govuk_editor" do
+      skip
+    end
+
+    should "not allow update if no editor permissions" do
+      user = FactoryBot.create(:user, name: "Non-editor")
+      edition = FactoryBot.create(:edition)
+      login_as(user)
+
+      # artefact_path(edition.id)
+      patch :update, params: { id: edition.artefact.id }
+
+      # TODO: this needs to flash but stay on the page
+      assert_response :redirect
+      assert_redirected_to edition_path(edition.id)
+      assert_includes flash[:danger], "You do not have correct editor permissions for this action."
+    end
+  end
 end
