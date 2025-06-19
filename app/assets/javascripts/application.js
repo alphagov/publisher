714ddac09e9634a51a4804ab53735ba3f315c94f
davidtrussler <mail@davidtrussler.net>
2025-06-19 17:52:05 +0100

    set up GA4 tracking for "Filter" section (WIP)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,5 +6,7 @@
 //= require govuk_publishing_components/components/govspeak
 //= require govuk_publishing_components/components/table
 
+//= require analytics_modules/ga4-finder-setup.js
+
 window.GOVUK.approveAllCookieTypes()
 window.GOVUK.cookie('cookies_preferences_set', 'true', { days: 365 })
