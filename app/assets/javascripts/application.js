6c4a6953bedead13dd4c12c9b564794ff2806ce0
davidtrussler <mail@davidtrussler.net>
2025-06-24 12:02:40 +0100

    Add GA4 for button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,6 +6,7 @@
 //= require govuk_publishing_components/components/govspeak
 //= require govuk_publishing_components/components/table
 
+//= require analytics_modules/ga4-button-setup.js
 //= require analytics_modules/ga4-finder-setup.js
 //= require analytics_modules/ga4-index-section-setup.js
 //= require analytics_modules/ga4-paste-tracker.js
