965414d5f0fcb8951ea8a7b13793b1fee9869306
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-07-28 17:00:16 +0100

    WIP

--- db/migrate/20250728100451_add_new_fields_to_local_transactions.rb
+++
@@ -1,7 +0,0 @@
-class AddNewFieldsToLocalTransactions < ActiveRecord::Migration[7.1]
-  def change
-    add_column :local_transaction_editions, :cta_text, :string
-    add_column :local_transaction_editions, :before_results, :string
-    add_column :local_transaction_editions, :after_results, :string
-  end
-end
