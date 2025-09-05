a743561df0dd783c0581883c0538d4138bd3fd99
Alex Sheppard <alexander.sheppard@digital.cabinet-office.gov.uk>
2025-09-05 14:21:49 +0100

    Adds TransactionEdition to NewDesignSystemConstraint.
    ** Also refactors to extract out the content types into a variable, as the matches? line was getting too long.

--- app/constraints/new_design_system_constraint.rb
+++ app/constraints/new_design_system_constraint.rb
@@ -1,5 +1,9 @@
 class NewDesignSystemConstraint
+  DESIGN_SYSTEM_CONTENT_TYPES = [AnswerEdition,
+                                 HelpPageEdition,
+                                 PlaceEdition,
+                                 TransactionEdition].freeze
   def matches?(request)
-    AllowedContentTypesConstraint.new([AnswerEdition, HelpPageEdition, PlaceEdition]).matches?(request) && FeatureConstraint.new("design_system_edit").matches?(request)
+    AllowedContentTypesConstraint.new(DESIGN_SYSTEM_CONTENT_TYPES).matches?(request) && FeatureConstraint.new("design_system_edit").matches?(request)
   end
 end
