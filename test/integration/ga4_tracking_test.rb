790511c545b8e9f6a4d027c9610969daf2430733
davidtrussler <mail@davidtrussler.net>
2025-10-03 17:43:18 +0100

    - start adding tests for JS-added parameters

--- test/integration/ga4_tracking_test.rb
+++ test/integration/ga4_tracking_test.rb
@@ -17,11 +17,29 @@ class Ga4TrackingTest < JavascriptIntegrationTest
   end
 
   context "Publications page" do
+    setup do
+      @edition_1 = FactoryBot.create(:edition, title: "The first document")
+      @edition_2 = FactoryBot.create(:edition, title: "The second document")
+    end
+
     should "render the correct ga4 data-attributes" do
       visit "/"
 
       assert page.has_css?(".govuk-width-container[data-ga4-filter-type='Publications']")
       assert page.has_css?(".govuk-width-container[data-module='ga4-event-tracker ga4-index-section-setup ga4-paste-tracker ga4-link-tracker ga4-button-setup']")
+    end
+
+    should "add the correct search-results parameters" do
+      visit "/"
+      filter_by_user("All")
+
+      # within :css, ".publications-table" do
+        within all(".govuk-table__row")[2] do
+          assert page.has_css?("a[data-ga4-ecommerce-content-id='#{@edition_1.id}']")
+          # assert page.has_css?("a[data-ga4-ecommerce-path='yyy']")
+          # assert page.has_css?("a[data-ga4-ecommerce-index='1']")
+        end
+      # end
     end
   end
 end
