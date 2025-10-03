a1afbcbb8d8e8483dc28f262570b6b27b12551f8
davidtrussler <mail@davidtrussler.net>
2025-10-03 16:56:11 +0100

    Update integration test to split out generic attributes from Pubications page

--- test/integration/ga4_tracking_test.rb
+++ test/integration/ga4_tracking_test.rb
@@ -8,27 +8,20 @@ class Ga4TrackingTest < JavascriptIntegrationTest
     test_strategy.switch!(:design_system_publications_filter, true)
   end
 
-  context "header" do
-    should "render the correct ga4 data-attributes in the header section" do
-      visit "/"
+  should "render the correct ga4 data-attributes on page load]" do
+    visit "/"
 
-      assert page.has_css?("header[data-ga4-no-copy='true']")
-    end
+    assert page.has_css?("header[data-ga4-no-copy='true']")
+    assert page.has_css?("footer[data-ga4-no-copy='true']")
+    assert page.has_css?(".govuk-width-container[data-ga4-no-copy='true']")
   end
 
-  context "main" do
-    should "render the correct ga4 data-attributes in the main content section" do
+  context "Publications page" do
+    should "render the correct ga4 data-attributes" do
       visit "/"
 
-      assert page.has_css?(".govuk-width-container[data-ga4-no-copy='true']")
-    end
-  end
-
-  context "footer" do
-    should "render the correct ga4 data-attributes in the footer section" do
-      visit "/"
-
-      assert page.has_css?("footer[data-ga4-no-copy='true']")
+      assert page.has_css?(".govuk-width-container[data-ga4-filter-type='Publications']")
+      assert page.has_css?(".govuk-width-container[data-module='ga4-event-tracker ga4-index-section-setup ga4-paste-tracker ga4-link-tracker ga4-button-setup']")
     end
   end
 end
