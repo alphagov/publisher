cc6cfc4db38a56e848f20f26975786eb0314f56c
davidtrussler <mail@davidtrussler.net>
2025-10-16 13:00:53 +0100

    Add ga4-form-setup module - Taken as is from Whitehall

---
+++ app/assets/javascripts/modules/ga4-form-setup.js
@@ -0,0 +1,110 @@
+'use strict'
+window.GOVUK = window.GOVUK || {}
+window.GOVUK.analyticsGa4 = window.GOVUK.analyticsGa4 || {}
+window.GOVUK.analyticsGa4.analyticsModules =
+  window.GOVUK.analyticsGa4.analyticsModules || {}
+;(function (Modules) {
+  Modules.Ga4FormSetup = {
+    // Dunnno what this does - consider removing
+    trackedComponents: ['reorderable-list'],
+
+    init: function () {
+      console.log('Ga4FormSetup init!')
+
+      const $modules = document.querySelectorAll(
+        "[data-module~='ga4-form-setup']"
+      )
+
+      console.log('$modules: ', $modules)
+
+      const trackedComponentsSelector = Modules.Ga4FormSetup.trackedComponents
+        .map((trackedComponent) => `[data-module~="${trackedComponent}"]`)
+        .join(',')
+
+      $modules.forEach(($module) => {
+        const forms = $module.querySelectorAll(
+          "form:not([data-module~='ga4-finder-tracker']):not([data-module~='ga4-form-tracker'])"
+        )
+
+        forms.forEach(function (form) {
+          console.log('form: ', form)
+
+          if (!form.querySelector(trackedComponentsSelector)) {
+            form.setAttribute('data-ga4-form-change-tracking', '')
+          }
+
+          // Neither of these are currently present on Publisher
+          // Guess they need to be
+          const sectionContainer = form.closest('[data-ga4-section]')
+          const documentTypeContainer = form.closest('[data-ga4-document-type]')
+
+          let eventData = {
+            event_name: 'form_response',
+            action: 'Save'
+          }
+
+          if (sectionContainer) {
+            eventData.section =
+              sectionContainer.getAttribute('data-ga4-section')
+          }
+
+          if (documentTypeContainer) {
+            const [type, toolName] =
+              documentTypeContainer.dataset.ga4DocumentType.split('-')
+
+            const synonyms = {
+              create: 'new',
+              update: 'edit'
+            }
+
+            eventData = {
+              ...eventData,
+              type: synonyms[type] || type,
+              tool_name: toolName
+            }
+
+            console.log('eventData: ', eventData)
+          }
+
+          form.setAttribute('data-ga4-form', JSON.stringify(eventData))
+
+          if (
+            form.querySelectorAll(
+              'fieldset, input:not([type="checkbox"],[type="hidden"],[type="radio"],[type="search"]), select'
+            ).length > 1
+          ) {
+            // only record JSON if number of fields larger than 1
+            form.setAttribute('data-ga4-form-record-json', '')
+            form.setAttribute('data-ga4-form-split-response-text', '')
+          }
+
+          form.setAttribute('data-ga4-form-include-text', '')
+          form.setAttribute('data-ga4-form-use-text-count', '')
+          form.setAttribute('data-ga4-form-use-select-count', '')
+          new window.GOVUK.Modules.Ga4FormTracker(form).init()
+
+          form.addEventListener('submit', this.onSubmit)
+        }, this)
+      })
+    },
+
+    onSubmit: (event) => {
+      // on forms we have multiple submit buttons so need to "guess"
+      // the action from the focused element on submit
+
+      const form = event.target.closest('form')
+
+      const activeElement = document.activeElement
+
+      try {
+        const dataGa4Form = JSON.parse(form.getAttribute('data-ga4-form'))
+
+        dataGa4Form.action = activeElement.textContent
+
+        form.setAttribute('data-ga4-form', JSON.stringify(dataGa4Form))
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+})(window.GOVUK.analyticsGa4.analyticsModules)
