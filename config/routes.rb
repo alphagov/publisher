b48b4dacbd6ff641941ea8ef9991b2e0836b6819
“farahTW” <farah.mehmood@digital.cabinet-office.gov.uk>
2025-11-12 17:20:33 +0000

    Added edit chapter functionality

--- config/routes.rb
+++ config/routes.rb
@@ -80,12 +80,7 @@ Rails.application.routes.draw do
              }
       end
 
-      resources :guide_parts do
-        member do
-          get "new"
-          post "create"
-        end
-
+      resources :guide_parts, only: %i[new create show edit update] do
         collection do
           get "reorder"
           post "bulk_update_reorder"
