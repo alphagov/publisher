c13c2cf38e3733ea5ec232d9cf8726f86ada66ab
Helen Pickavance <helen.pickavance@digital.cabinet-office.gov.uk>
2025-11-13 15:05:10 +0000

    Refactor all_content_workflow_presenter_test to batch the processing of data
    - This is to reduce the amount of memory being used by the report
    - Since the migration to Postgres, this has caused issues and the report has been failing to run

--- test/unit/all_content_workflow_presenter_test.rb
+++ test/unit/all_content_workflow_presenter_test.rb
@@ -1,29 +1,69 @@
 require "test_helper"
 
 class AllContentWorkflowPresenterTest < ActiveSupport::TestCase
-  should "provide a CSV export of content workflow" do
+  setup do
+    @transaction_created_at_time = Time.zone.now - 2.days
     transaction_user = FactoryBot.create(:user, :govuk_editor, name: "Ray Khan")
+    transaction_edition = FactoryBot.create(:transaction_edition, title: "Register to vote (armed forces)",
+                                                                  slug: "register-to-vote-armed-forces", created_at: @transaction_created_at_time,
+                                                                  state: "published", assigned_to_id: transaction_user.id)
+    @guide_created_at_time = Time.zone.now - 1.year
     guide_user = FactoryBot.create(:user, :govuk_editor, name: "Constance Cerf")
-    transaction_edition = FactoryBot.create(:transaction_edition, title: "Register to vote (armed forces)", slug: "register-to-vote-armed-forces", state: "published", assigned_to_id: transaction_user.id, actions:)
-    guide_edition = FactoryBot.create(:guide_edition, title: "The Queen's Awards for Enterprise", slug: "queens-awards-for-enterprise", state: "published", assigned_to_id: guide_user.id, actions:)
+    guide_edition = FactoryBot.create(:guide_edition, title: "The Queen's Awards for Enterprise",
+                                                      slug: "queens-awards-for-enterprise", created_at: @guide_created_at_time,
+                                                      state: "published", assigned_to_id: guide_user.id)
 
-    editions = [transaction_edition, guide_edition]
+    action_types = [
+      Action::CREATE,
+      Action::REQUEST_REVIEW,
+      Action::SEND_FACT_CHECK,
+      Action::REQUEST_AMENDMENTS,
+      Action::SEND_FACT_CHECK,
+    ]
 
-    csv = AllContentWorkflowPresenter.new(editions).to_csv
+    action_time = @transaction_created_at_time
+    action_types.each do |action_type|
+      transaction_edition.actions.create!(
+        request_type: action_type,
+        created_at: action_time,
+        requester_id: transaction_user.id,
+        requester: transaction_user,
+      )
+      action_time += 1.hour
+    end
+
+    action_time = @guide_created_at_time
+    action_types.each do |action_type|
+      guide_edition.actions.create!(
+        request_type: action_type,
+        created_at: action_time,
+        requester_id: guide_user.id,
+        requester: guide_user,
+      )
+      action_time += 1.day
+    end
+  end
+
+  should "provide a CSV export of content workflow for the editions provided" do
+    csv = AllContentWorkflowPresenter.new(Edition.all.order(created_at: :desc)).to_csv
     data = CSV.parse(csv, headers: true)
 
     assert_equal 10, data.length
-    assert_equal "Register to vote (armed forces)", data.first["Content title"]
-    assert_equal "register-to-vote-armed-forces", data.first["Content slug"]
-    assert_equal "#{Plek.website_root}/register-to-vote-armed-forces", data.first["Content URL"]
-    assert_equal "published", data.first["Current status"]
-    assert_equal "create", data.first["Stage"]
-    assert_equal "Transaction", data.first["Format"]
-    assert_equal "Ray Khan", data.first["Current assignee"]
-    assert_equal "1", data.first["Version number"]
-    assert_equal "2016-01-07 17:41:57", data.first["Created at"]
-    assert_equal "2016-01-07", data.first["Date created"]
-    assert_equal "17:41", data.first["Time created"]
+    assert_equal "Register to vote (armed forces)", data[0]["Content title"]
+    assert_equal "register-to-vote-armed-forces", data[0]["Content slug"]
+    assert_equal "#{Plek.website_root}/register-to-vote-armed-forces", data[0]["Content URL"]
+    assert_equal "published", data[0]["Current status"]
+    assert_equal "create", data[0]["Stage"]
+    assert_equal "Transaction", data[0]["Format"]
+    assert_equal "Ray Khan", data[0]["Current assignee"]
+    assert_equal @transaction_created_at_time.to_fs(:db), data[0]["Created at"]
+    assert_equal "1", data[0]["Version number"]
+    assert_equal @transaction_created_at_time.to_date.to_s, data[0]["Date created"]
+    assert_equal @transaction_created_at_time.to_fs(:time), data[0]["Time created"]
+
+    assert_equal "request_review", data[1]["Stage"]
+    assert_equal "send_fact_check", data[2]["Stage"]
+    assert_equal "request_amendments", data[3]["Stage"]
 
     assert_equal "The Queen's Awards for Enterprise", data[7]["Content title"]
     assert_equal "queens-awards-for-enterprise", data[7]["Content slug"]
@@ -32,38 +72,11 @@ class AllContentWorkflowPresenterTest < ActiveSupport::TestCase
     assert_equal "send_fact_check", data[7]["Stage"]
     assert_equal "Guide", data[7]["Format"]
     assert_equal "Constance Cerf", data[7]["Current assignee"]
+
+    action_created_at_time = @guide_created_at_time + 2.days
+    assert_equal action_created_at_time.to_fs(:db), data[7]["Created at"]
     assert_equal "1", data[7]["Version number"]
-    assert_equal "2016-01-17 12:11:33", data[7]["Created at"]
-    assert_equal "2016-01-17", data[7]["Date created"]
-    assert_equal "12:11", data[7]["Time created"]
-  end
-
-  def actions
-    action1 = Action.new(
-      request_type: "create",
-      created_at: "2016-01-07 17:41:57",
-    )
-
-    action2 = Action.new(
-      request_type: "request_review",
-      created_at: "2016-01-11 10:21:00",
-    )
-
-    action3 = Action.new(
-      request_type: "send_fact_check",
-      created_at: "2016-01-17 12:11:33",
-    )
-
-    action4 = Action.new(
-      request_type: "request_amendments",
-      created_at: "2016-01-20 17:41:28",
-    )
-
-    action5 = Action.new(
-      request_type: "send_fact_check",
-      created_at: "2016-03-07 17:31:44",
-    )
-
-    [action1, action2, action3, action4, action5]
+    assert_equal action_created_at_time.to_date.to_s, data[7]["Date created"]
+    assert_equal action_created_at_time.to_fs(:time), data[7]["Time created"]
   end
 end
