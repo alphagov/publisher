eca5d187ae02606a1d2e39ee3d05ccb899722740
Rob Young <robert.young@digital.cabinet-office.gov.uk>
2025-12-05 15:58:11 +0000

    Update navigation items with my_content link

--- test/unit/helpers/navigation_items_helper_test.rb
+++ test/unit/helpers/navigation_items_helper_test.rb
@@ -4,7 +4,7 @@ class NavigationItemsHelperTest < ActionView::TestCase
   context "NavigationItemsHelper" do
     should "include editor-only links when user is an editor" do
       assert_equal [
-        { text: "Publications", href: root_path, active: false },
+        { text: "My content", href: my_content_path, active: false },
         { text: "Add artefact", href: new_artefact_path, active: false },
         { text: "Downtime", href: downtimes_path, active: false },
         { text: "Reports", href: reports_path, active: false },
@@ -15,7 +15,7 @@ class NavigationItemsHelperTest < ActionView::TestCase
 
     should "not include editor-only links when user is not an editor" do
       assert_equal [
-        { text: "Publications", href: root_path, active: false },
+        { text: "My content", href: my_content_path, active: false },
         { text: "Reports", href: reports_path, active: false },
         { text: "Search by user", href: user_search_path, active: false },
         { text: "Sign out", href: "/auth/gds/sign_out" },
@@ -24,7 +24,7 @@ class NavigationItemsHelperTest < ActionView::TestCase
 
     should "include user name link if user has name" do
       assert_equal [
-        { text: "Publications", href: root_path, active: false },
+        { text: "My content", href: my_content_path, active: false },
         { text: "Reports", href: reports_path, active: false },
         { text: "Search by user", href: user_search_path, active: false },
         { text: "Name", href: Plek.new.external_url_for("signon") },
@@ -34,7 +34,7 @@ class NavigationItemsHelperTest < ActionView::TestCase
 
     should "apply set link to active if page is current link" do
       paths = [
-        root_path,
+        my_content_path,
         new_artefact_path,
         downtimes_path,
         reports_path,
