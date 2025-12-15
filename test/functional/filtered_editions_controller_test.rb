7c81793e11bf31d750dc5448420fca5a520160dd
Rob Young <robert.young@digital.cabinet-office.gov.uk>
2025-12-15 14:33:58 +0000

    Add functional test for two_eye_queue action

--- test/functional/filtered_editions_controller_test.rb
+++ test/functional/filtered_editions_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class FilteredEditionsControllerTest < ActionController::TestCase
   setup do
     login_as_stub_user
+    @other_user = FactoryBot.create(:user, name: "Other User")
   end
 
   context "#my_content" do
@@ -36,8 +37,7 @@ class FilteredEditionsControllerTest < ActionController::TestCase
     end
 
     should "not display publications that are not assigned to the current user" do
-      other_user = FactoryBot.create(:user, name: "Other User")
-      FactoryBot.create(:edition, title: "Assigned to Other User", assigned_to: other_user)
+      FactoryBot.create(:edition, title: "Assigned to Other User", assigned_to: @other_user)
 
       get :my_content
 
@@ -100,6 +100,109 @@ class FilteredEditionsControllerTest < ActionController::TestCase
       assert_response :ok
       assert_select "th.govuk-table__header a", "Assigned to Stub User"
       assert_select "th.govuk-table__header .govuk-hint", "Sent 1 day ago"
+    end
+  end
+
+  context "#two_eye_queue" do
+    should "render two_eye_queue template" do
+      get :two_eye_queue
+
+      assert_response :ok
+      assert_template "filtered_editions/two_eye_queue"
+      assert_select "section.govuk-tabs__panel#english"
+      assert_select "section.govuk-tabs__panel#welsh"
+    end
+
+    should "render the English and Welsh tabs" do
+      get :two_eye_queue
+
+      assert_response :ok
+      assert_select "section.govuk-tabs__panel#english"
+      assert_select "section.govuk-tabs__panel#welsh"
+    end
+
+    should 'render publications that are in the "in_review" state' do
+      FactoryBot.create(:edition, :in_review, title: "English edition assigned to user", assigned_to: @user)
+      FactoryBot.create(:edition, :in_review, title: "English edition assigned to other user", assigned_to: @other_user)
+      FactoryBot.create(:edition, :in_review, :welsh, title: "Welsh edition assigned to user", assigned_to: @user)
+      FactoryBot.create(:edition, :in_review, :welsh, title: "Welsh edition assigned to other user", assigned_to: @other_user)
+
+      get :two_eye_queue
+
+      assert_response :ok
+      assert_select "section.govuk-tabs__panel#english" do
+        assert_select "th.govuk-table__header a", "English edition assigned to user"
+        assert_select "th.govuk-table__header a", "English edition assigned to other user"
+      end
+
+      assert_select "section.govuk-tabs__panel#welsh" do
+        assert_select "th.govuk-table__header a", "Welsh edition assigned to user"
+        assert_select "th.govuk-table__header a", "Welsh edition assigned to other user"
+      end
+    end
+
+    %i[draft amends_needed ready fact_check fact_check_received scheduled_for_publishing archived published].each do |state|
+      should "not render publications that are in the #{state} state" do
+        FactoryBot.create(:edition, state, title: state.to_s, assigned_to: @user)
+
+        get :two_eye_queue
+
+        assert_response :ok
+        assert_select "span.item-count", "0 items"
+        assert_not_select "tbody"
+        assert_not_select "th.govuk-table__header a", state.to_s
+      end
+    end
+
+    context "when the edition has no 2i reviewer" do
+      should "render the 'Claim 2i' button when the edition is not assigned to the user" do
+        FactoryBot.create(:edition, :in_review, title: "Unclaimed English edition", assigned_to: @other_user)
+        FactoryBot.create(:edition, :in_review, :welsh, title: "Unclaimed Welsh edition", assigned_to: @other_user)
+
+        get :two_eye_queue
+
+        assert_select "section.govuk-tabs__panel#english" do
+          assert_select "td.govuk-table__cell button", "Claim 2i"
+        end
+
+        assert_select "section.govuk-tabs__panel#welsh" do
+          assert_select "td.govuk-table__cell button", "Claim 2i"
+        end
+      end
+
+      should "not display the 'Claim 2i' button when the edition is assigned to the user" do
+        FactoryBot.create(:edition, :in_review, title: "Unclaimed English edition", assigned_to: @user)
+        FactoryBot.create(:edition, :in_review, :welsh, title: "Unclaimed Welsh edition", assigned_to: @user)
+
+        get :two_eye_queue
+
+        assert_select "section.govuk-tabs__panel#english" do
+          assert_not_select "td.govuk-table__cell button", "Claim 2i"
+          assert_select "td.govuk-table__cell", "Not yet claimed"
+        end
+
+        assert_select "section.govuk-tabs__panel#welsh" do
+          assert_not_select "td.govuk-table__cell button", "Claim 2i"
+          assert_select "td.govuk-table__cell", "Not yet claimed"
+        end
+      end
+    end
+
+    context "when the edition has a 2i reviewer" do
+      should "display the 2i reviewer name" do
+        FactoryBot.create(:edition, :in_review, title: "Unclaimed English edition", assigned_to: @user, reviewer: @other_user)
+        FactoryBot.create(:edition, :in_review, :welsh, title: "Unclaimed Welsh edition", assigned_to: @user, reviewer: @other_user)
+
+        get :two_eye_queue
+
+        assert_select "section.govuk-tabs__panel#english" do
+          assert_select "td.govuk-table__cell", "Other User"
+        end
+
+        assert_select "section.govuk-tabs__panel#welsh" do
+          assert_select "td.govuk-table__cell", "Other User"
+        end
+      end
     end
   end
 end
