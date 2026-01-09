a2c37ff793d00b4e4188c8cd9b9fcf810b0406e1
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2026-01-09 17:05:54 +0000

    Update legacy integration test for changing format
    - Rename the file to indicate that it is for testing the legacy UI.
    - Update the test class to inherit from `LegacyIntegrationTest` instead
      of `LegacyJavascriptIntegrationTest`, since it was only running tests
      within a `without_javascript` block.
    - Test conversion to all content types, even those that have been 'fully
      transitioned'.
    - Get rid of the unnecessary call to `reject` after calling
      `permutation`, since permutation already includes this behaviour.
    - Don't bother testing the `whole_body` since a) this is already covered
      by the `build_clone` unit tests; b) not all content types have a body
      or equivalent to check.

---
+++ test/integration/legacy_change_edition_type_test.rb
@@ -0,0 +1,56 @@
+require "legacy_integration_test_helper"
+
+class LegacyChangeEditionTypeTest < LegacyIntegrationTest
+  setup do
+    stub_linkables
+    FactoryBot.create(:user, :govuk_editor)
+    stub_holidays_used_by_fact_check
+    stub_events_for_all_content_ids
+    stub_users_from_signon_api
+    UpdateWorker.stubs(:perform_async)
+  end
+
+  teardown do
+    GDS::SSO.test_user = nil
+  end
+
+  fully_transitioned_types = %w[answer help_page place transaction completed_transaction local_transaction]
+
+  def select_target_edition(format)
+    select(format.to_s.humanize, from: "to")
+  end
+
+  def edition_parts(edition)
+    Set.new(edition.parts.map { |part| part.attributes.slice("title", "body", "slug") })
+  end
+
+  def create_artefact_of_kind(kind)
+    case kind
+    when "help_page"
+      FactoryBot.create(:artefact, slug: "help/foo", kind:)
+    when "completed_transaction"
+      FactoryBot.create(:artefact, slug: "done/foo", kind:)
+    else
+      FactoryBot.create(:artefact, kind:)
+    end
+  end
+
+  Edition.convertible_formats.permutation(2).each do |to, from|
+    next if fully_transitioned_types.include?(from)
+
+    should "be able to convert #{from} into #{to}" do
+      factory_name = "#{from}_edition".to_sym
+      artefact = create_artefact_of_kind(from)
+      edition = FactoryBot.create(factory_name, state: "published", panopticon_id: artefact.id)
+
+      visit "/editions/#{edition.to_param}/admin"
+
+      select_target_edition(to)
+
+      click_on "Change format"
+
+      assert_text edition.title
+      assert_text "New edition created"
+    end
+  end
+end
