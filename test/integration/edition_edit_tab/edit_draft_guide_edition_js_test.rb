20320ff32860c1685847ad9109f772ead1fee3fd
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2026-01-16 14:45:29 +0000

    Autofill a chapter's slug based on its title
    The Bootstrap UI auto-generates a slug value from the entered title when
    adding chapters/parts to a Guide.
    
    This commit replicates that functionality in the Design System UI
    (assuming that Javascript is enabled).
    
    The title to slug conversion process has been replicated, except that:
    - leading and trailing spaces are removed
    - underscores are converted to hyphens (since underscores are not valid
      in the slug)
    - hyphens in the title are preserved.
    
    The styling changes (flashing the slug field yellow) have not been
    replicated.

---
+++ test/integration/edition_edit_tab/edit_draft_guide_edition_js_test.rb
@@ -0,0 +1,52 @@
+require "integration_test_helper"
+
+class EditDraftGuideEditionJsTest < JavascriptIntegrationTest
+  context "auto-population of a chapter's slug from its title when JS is enabled" do
+    setup do
+      login_as_govuk_editor
+      @test_strategy = Flipflop::FeatureSet.current.test!
+      @test_strategy.switch!(:design_system_edit_phase_3a, true)
+    end
+
+    should "auto-populate the chapter's slug from the title" do
+      @draft_edition = FactoryBot.create(:guide_edition, :draft)
+      visit edition_path(@draft_edition)
+      click_on "Add a new chapter"
+
+      fill_in "part[title]", with: "Some Title"
+      find_field("part[title]").send_keys(:tab)
+
+      assert page.has_field?("part[slug]", with: "some-title")
+    end
+
+    context "when it's the first edition" do
+      should "overwrite the chapter's slug when changing the title" do
+        @draft_edition = FactoryBot.create(:guide_edition, :draft)
+        @part = @draft_edition.parts.create(title: "Original", slug: "original")
+        visit edit_edition_guide_part_path(@draft_edition, @part)
+
+        fill_in "part[title]", with: "Changed"
+        find_field("part[title]").send_keys(:tab)
+
+        slug_field = find_field("part[slug]")
+        assert_equal "changed", slug_field.value
+      end
+    end
+
+    context "when it's not the first edition" do
+      should "not overwrite the chapter's slug when changing the title" do
+        @published_edition = FactoryBot.create(:guide_edition, :published)
+        @draft_edition = @published_edition.build_clone
+        @draft_edition.save!
+        @part = @draft_edition.parts.create(title: "Original", slug: "original")
+        visit edit_edition_guide_part_path(@draft_edition, @part)
+
+        fill_in "part[title]", with: "Changed"
+        find_field("part[title]").send_keys(:tab)
+
+        slug_field = find_field("part[slug]")
+        assert_equal "original", slug_field.value
+      end
+    end
+  end
+end
