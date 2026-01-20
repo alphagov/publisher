8183a2f28ec5ed5650707c603fb879224503c0a3
Alex Sheppard <alexander.sheppard@digital.cabinet-office.gov.uk>
2026-01-20 10:58:52 +0000

    Refactor deliver_now call out of base_processor.rb and into event_notifier_service.rb

--- app/services/event_notifier_service.rb
+++ app/services/event_notifier_service.rb
@@ -2,23 +2,29 @@ class EventNotifierService
   def self.any_action(action, mailer: EventMailer)
     recipient_emails = (EMAIL_GROUPS[:citizen] + EMAIL_GROUPS[:business]).uniq
 
-    recipient_emails.map do |recipient_email|
+    emails = recipient_emails.map do |recipient_email|
       mailer.any_action(action, recipient_email)
     end
+
+    emails.map(&:deliver_now)
   end
 
   def self.skip_review(action, mailer: EventMailer)
     recipient_emails = EMAIL_GROUPS[:force_publish_alerts]
 
-    recipient_emails.map do |recipient_email|
+    emails = recipient_emails.map do |recipient_email|
       mailer.skip_review(action, recipient_email)
     end
+
+    emails.map(&:deliver_now)
   end
 
   def self.request_fact_check(action, mailer: EventMailer)
-    action.email_addresses.split(/,\s*/).map do |recipient_email|
+    emails = action.email_addresses.split(/,\s*/).map do |recipient_email|
       mailer.request_fact_check(action, recipient_email)
     end
+
+    emails.map(&:deliver_now)
   end
 
   def self.resend_fact_check(action)
@@ -28,7 +34,6 @@ class EventNotifierService
       request_fact_check(latest_status_action)
     else
       Rails.logger.info("Asked to resend fact check for #{edition.content_id}, but its most recent status action is not a fact check, it's a #{latest_status_action.request_type}")
-      EventMailer::NoMail.new
     end
   end
 end
