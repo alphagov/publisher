2d597ddde4bedc0c0d0a6d12e201d62bd688922b
davidtrussler <mail@davidtrussler.net>
2026-02-02 15:25:19 +0000

    Add required data-attributes and tracking/setup components - ga4-ecommerce-tracker component - ga4-search-results-setup module

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,10 @@
 
 // Analytics modules
 //= require govuk_publishing_components/analytics-ga4/ga4-form-change-tracker
+//= require govuk_publishing_components/analytics-ga4/ga4-ecommerce-tracker
 //= require analytics_modules/ga4-form-setup
 //= require analytics_modules/ga4-index-section-setup
+//= require analytics_modules/ga4-search-results-setup
 
 window.GOVUK.approveAllCookieTypes()
 window.GOVUK.cookie('cookies_preferences_set', 'true', { days: 365 })
