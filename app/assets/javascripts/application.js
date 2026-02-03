c49c05d74ccc0b8c98ce95ce6e850ea8c8bcb4e0
davidtrussler <mail@davidtrussler.net>
2026-02-03 10:09:02 +0000

    Add ga4-ecommerce-tracker component

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 //= require govuk_publishing_components/analytics-ga4/ga4-form-change-tracker
 //= require analytics_modules/ga4-form-setup
 //= require analytics_modules/ga4-index-section-setup
+//= require govuk_publishing_components/analytics-ga4/ga4-ecommerce-tracker
 
 window.GOVUK.approveAllCookieTypes()
 window.GOVUK.cookie('cookies_preferences_set', 'true', { days: 365 })
