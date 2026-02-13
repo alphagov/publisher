7251d7ab001300c2d2131f515081c19cc408e671
davidtrussler <mail@davidtrussler.net>
2026-02-13 12:39:50 +0000

    Reference `ga4-search-tracker` in `application.js`

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Analytics modules
 //= require govuk_publishing_components/analytics-ga4/ga4-ecommerce-tracker
 //= require govuk_publishing_components/analytics-ga4/ga4-form-change-tracker
+//= require govuk_publishing_components/analytics-ga4/ga4-search-tracker
 //= require analytics_modules/ga4-form-setup
 //= require analytics_modules/ga4-index-section-setup
 //= require analytics_modules/ga4-search-setup
