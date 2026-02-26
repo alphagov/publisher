65c119f4e62682a12ef475014ae49f66d0c0b263
davidtrussler <mail@davidtrussler.net>
2026-02-26 12:24:36 +0000

    Update ga4-search-results-setup for lack of search input

--- app/assets/javascripts/analytics_modules/ga4-search-results-setup.js
+++ app/assets/javascripts/analytics_modules/ga4-search-results-setup.js
@@ -12,10 +12,16 @@ window.GOVUK.analyticsGa4.analyticsModules = window.GOVUK.analyticsGa4.analytics
   }
 
   Ga4SearchResultsSetup.prototype.trackEcommerce = function () {
+    var searchTerm
     var searchTarget = document.querySelector('[data-ga4-ecommerce]')
-    var searchTerm = document.querySelector('input[name="search_text"]').value
 
-    searchTarget.dataset.ga4SearchQuery = searchTerm
+    if (document.querySelector('input[name="search_text"]')) {
+      searchTerm = document.querySelector('input[name="search_text"]').value
+    }
+
+    if (searchTerm) {
+      searchTarget.dataset.ga4SearchQuery = searchTerm
+    }
 
     if (searchTarget) {
       if (!searchTarget.hasAttribute('data-ga4-ecommerce-started')) {
