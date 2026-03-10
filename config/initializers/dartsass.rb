ee992586ecc6687b2b6793650ad3f1c6243e0dc0
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2026-03-10 11:15:48 +0000

    Fix sass compilation
    - fix dartsass config, only compile the component guide stylesheet as an extra (this configuration was changed a while back)
    - make changes to fit the breaking changes introduced in the components gem v65

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -3,6 +3,6 @@ APP_STYLESHEETS = {
   "legacy-application.scss" => "legacy-application.css",
 }.freeze
 
-all_stylesheets = APP_STYLESHEETS.merge(GovukPublishingComponents::Config.all_stylesheets)
+all_stylesheets = APP_STYLESHEETS.merge(GovukPublishingComponents::Config.component_guide_stylesheet)
 Rails.application.config.dartsass.builds = all_stylesheets
 Rails.application.config.dartsass.build_options << " --quiet-deps"
