7d69d3b7bbecaddde28ea24245b555faf2b9165f
davidtrussler <mail@davidtrussler.net>
2026-03-25 12:46:28 +0000

    Add new `ga4_tracking_unpublish_test` integration test

---
+++ test/integration/ga4_tracking_unpublish_test.rb
@@ -0,0 +1,40 @@
+require "integration_test_helper"
+require "support/ga4_test_helpers"
+
+class Ga4TrackingUnpublishTest < JavascriptIntegrationTest
+  include Ga4TestHelpers
+
+  setup do
+    setup_users
+    @edition = FactoryBot.create(:answer_edition, state: "published", title: "Answer edition")
+    @test_strategy.switch!(:ga4_form_tracking, true)
+  end
+
+  context "Unpublish tab" do
+    setup do
+      visit unpublish_edition_path(@edition)
+      disable_form_submit
+    end
+
+    should "add unpublish events to the dataLayer" do
+      fill_in "Redirect to URL", with: "https://www.gov.uk/redirect"
+      click_button "Continue"
+
+      event_data = get_event_data
+
+      assert_equal "select", event_data[0]["action"]
+      assert_equal "select_content", event_data[0]["event_name"]
+      assert_equal "Redirect to URL", event_data[0]["section"]
+      assert_equal "27", event_data[0]["text"]
+      assert_equal "1", event_data[0]["index"]["index_section"]
+      assert_equal "1", event_data[0]["index"]["index_section_count"]
+
+      assert_equal "Save", event_data[1]["action"]
+      assert_equal "form_response", event_data[1]["event_name"]
+      assert_equal "Unpublish", event_data[1]["section"]
+      assert_equal "{\"Redirect to URL\":\"27\"}", event_data[1]["text"]
+      assert_equal "Answer", event_data[1]["tool_name"]
+      assert_equal "edit", event_data[1]["type"]
+    end
+  end
+end
