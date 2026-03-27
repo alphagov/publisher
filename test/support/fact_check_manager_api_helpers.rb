58747bed3b5d65aadd19b952a1ca96d35e557a32
Helen Pickavance <helen.pickavance@digital.cabinet-office.gov.uk>
2026-03-27 14:45:44 +0000

    Call the `update_fact_check_content` method under the following conditions
    - when feature toggle for API is on
    - when edition is in `fact_check` state

--- test/support/fact_check_manager_api_helpers.rb
+++ test/support/fact_check_manager_api_helpers.rb
@@ -15,4 +15,20 @@ module FactCheckManagerApiHelpers
       Services.fact_check_manager_api.stubs(:post_resend_emails).raises(GdsApi::HTTPErrorResponse.new(code: 422), "Example error message")
     end
   end
+
+  def stub_patch_update_fact_check_content(success: true, source_id: "12345")
+    if success
+      mock_http_response = stub(
+        code: 200,
+        body: {
+          "source_app" => "publisher",
+          "source_id" => source_id,
+        }.to_json,
+      )
+
+      Services.fact_check_manager_api.stubs(:patch_update_content).returns(GdsApi::Response.new(mock_http_response))
+    else
+      Services.fact_check_manager_api.stubs(:patch_update_content).raises(GdsApi::HTTPErrorResponse.new(code: 422), "Example error message")
+    end
+  end
 end
