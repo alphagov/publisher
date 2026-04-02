68fa804a441e9fd8b2ab0b0e7c19309c0a0a875c
davidtrussler <mail@davidtrussler.net>
2026-04-02 15:37:36 +0100

    Update `ga4_tracking_add_artefact_test` to add test for "Create new content page two" (WIP)

--- test/integration/ga4_tracking_add_artefact_test.rb
+++ test/integration/ga4_tracking_add_artefact_test.rb
@@ -12,7 +12,7 @@ class Ga4TrackingAddArtefactTest < JavascriptIntegrationTest
     @test_strategy.switch!(:ga4_form_tracking, true)
   end
 
-  context "Create new content page" do
+  context "Create new content page one" do
     setup do
       visit new_artefact_path
     end
@@ -28,6 +28,48 @@ class Ga4TrackingAddArtefactTest < JavascriptIntegrationTest
       assert_equal "Select a content type", event_data[0]["text"]
       assert_equal "Kind", event_data[0]["section"]
       assert_equal "Publisher", event_data[0]["tool_name"]
+    end
+  end
+
+  context "Create new content page two" do
+    setup do
+      visit new_artefact_path
+      choose "Local transaction"
+      click_button "Continue"
+    end
+
+    should "push the correct values to the dataLayer when a form error is triggered" do
+      click_button "Create content"
+
+      event_data = get_event_data
+
+      assert_equal "error", event_data[0]["action"]
+      assert_equal "form_error", event_data[0]["event_name"]
+      assert_equal "Edit edition", event_data[0]["type"]
+      assert_equal "Enter a title", event_data[0]["text"]
+      assert_equal "Name", event_data[0]["section"]
+      assert_equal "Publisher", event_data[0]["tool_name"]
+
+      assert_equal "error", event_data[1]["action"]
+      assert_equal "form_error", event_data[1]["event_name"]
+      assert_equal "Edit edition", event_data[1]["type"]
+      assert_equal "Enter a slug", event_data[1]["text"]
+      assert_equal "Slug", event_data[1]["section"]
+      assert_equal "Publisher", event_data[1]["tool_name"]
+
+      assert_equal "error", event_data[2]["action"]
+      assert_equal "form_error", event_data[2]["event_name"]
+      assert_equal "Edit edition", event_data[2]["type"]
+      assert_equal "Enter a LGSL code", event_data[2]["text"]
+      assert_equal "Lgsl code", event_data[2]["section"]
+      assert_equal "Publisher", event_data[2]["tool_name"]
+
+      assert_equal "error", event_data[3]["action"]
+      assert_equal "form_error", event_data[3]["event_name"]
+      assert_equal "Edit edition", event_data[3]["type"]
+      assert_equal "Enter a LGIL code", event_data[3]["text"]
+      assert_equal "Lgil code", event_data[3]["section"]
+      assert_equal "Publisher", event_data[3]["tool_name"]
     end
   end
 end
