19b4d378097a2da26e4ed2a47eef9d10ad10e0b1
Rob Young <robert.young@digital.cabinet-office.gov.uk>
2026-05-08 12:15:08 +0100

    Allow empty strings for `comment` in FactCheckResponseContract
    Fact Check Manager responses send over an empty string for `comment`
    when there is no comment on the response (rather than omitting the key
    or sending `nil`). This change permits that behaviour rather than
    causing a validation error.

--- app/contracts/fact_check_response_contract.rb
+++ app/contracts/fact_check_response_contract.rb
@@ -3,11 +3,11 @@ class FactCheckResponseContract < Dry::Validation::Contract
     required(:edition_id).filled(:string)
     required(:responder_name).filled(:string)
     required(:accepted).filled(:bool)
-    optional(:comment).filled(:string)
+    optional(:comment).value(:string)
   end
 
   rule(:comment, :accepted) do
-    if !values[:accepted] && values[:comment].nil?
+    if !values[:accepted] && values[:comment].blank?
       key.failure("must be provided if the fact check is rejected")
     end
   end
