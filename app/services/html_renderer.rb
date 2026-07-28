59e556b1a2ba7351102fe6cf3da078880c0f5904
Ed Davey <edward.antoniodavey@dsit.gov.uk>
2026-07-28 18:22:14 +0100

    Obtain block from Pub API rather than Content Store
    [RFC-192: Restrict base_paths to a smaller character set][rfc-192] proposes
    restricting GOV.UK  base paths to `a-z 0-9 . /` (no underscores, no uppercase).
    Content blocks are affected because their `base_path` includes the `document_type`
    (e.g. content_block_time_period) which contains underscores, as in this base_url:
    
    ```
    /content-blocks/content_block_time_period/tax-year
    ```
    
    Before this change, when rendering content blocks to send for fact check,
    Publisher was getting the block `#details` from Content Store. This used
    `base_path` within the Content Block Tools gem via the call to
    `ContentBlockTools::ContentBlock.from_embed_code(code)` which constructs the
    (invalid) `base_path` to make the call to the Content Store:
    
    `ContentBlockTools::ContentBlockReference`:
    ```
    def content_store_identifier
      "/content-blocks/#{document_type}/#{identifier}"
    end
    ```
    
    With this change Publisher's HtmlRenderer instead of building a renderable
    block with a single call to the "tools" of
    `ContentBlockTools::ContentBlock.from_embed_code(code).render`, now:
    
    - obtains the block `details` from Publishing API
    - builds a renderable block using `ContentBlockTools::ContentBlock.new(...).render`
    
    [rfc-192]:
    https://github.com/alphagov/govuk-rfcs/pull/192/

--- app/services/html_renderer.rb
+++ app/services/html_renderer.rb
@@ -9,12 +9,32 @@ class HtmlRenderer
     return "" if document.blank?
 
     html_content = Govspeak::Document.new(document).to_html
+    references = ContentBlockTools::ContentBlockReference.find_all_in_document(html_content).uniq
 
-    ContentBlockTools::ContentBlockReference.find_all_in_document(html_content).uniq.each do |content_block|
-      code = content_block.embed_code
-      html_content.gsub!(code, ContentBlockTools::ContentBlock.from_embed_code(code).render)
+    references.each do |ref|
+      block = build_from_publishing_api(ref)
+      html_content.gsub!(ref.embed_code, block.render)
     end
 
     html_content.strip
+  end
+
+  def self.build_from_publishing_api(reference)
+    api_response = Services.publishing_api.get_content_items(
+      content_id_aliases: [reference.identifier],
+      document_type: reference.document_type,
+    ).parsed_content["results"].first
+
+    build_content_block(embed_code: reference.embed_code, api_response: api_response)
+  end
+
+  def self.build_content_block(embed_code:, api_response:)
+    ContentBlockTools::ContentBlock.new(
+      content_id: api_response["content_id"],
+      title: api_response["title"],
+      document_type: api_response["document_type"],
+      details: api_response["details"],
+      embed_code: embed_code,
+    )
   end
 end
