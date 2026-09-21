8a64f583ee5c2c8082e09df92a061046e4405ede
David Trussler <david.trussler@digital.cabinet-office.gov.uk>
2026-09-21 11:58:04 +0100

    Make necessary changes to render header - remove reference to `layout-header` js

--- app/assets/javascripts/es6-components.js
+++ app/assets/javascripts/es6-components.js
@@ -10,7 +10,6 @@
 //= require govuk_publishing_components/components/accordion
 //= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/error-summary
-//= require govuk_publishing_components/components/layout-header
 //= require govuk_publishing_components/components/radio
 //= require govuk_publishing_components/components/select-with-search
 //= require govuk_publishing_components/components/skip-link
